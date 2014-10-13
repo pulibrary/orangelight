cf847f12a2534a620188ac381eb040088faecf0a
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2014-10-13 16:59:12 -0400

    multiple locations test

--- spec/requests/resquest_spec.rb
+++ spec/requests/resquest_spec.rb
@@ -14,6 +14,16 @@ describe "blacklight tests" do
 	    r = JSON.parse(response.body)
 	    expect(r["response"]["docs"].select{|d| d["id"] == "4705304"}.length).to eq 1 
 	  end
+	end
+
+	describe "Multiple locations check" do
+		it "indicates an item has multiple holdings locations" do
+			get "/catalog.json?&search_field=all_fields&q="
+	    r = JSON.parse(response.body)
+	    expect(r["response"]["docs"].select{|d| d["id"] == "3"}[0]["location"].length).to eq 2
+	    get "/catalog?&search_field=all_fields&q=guida"
+	    expect(response.body.include?('<dd class="blacklight-location">Multiple Locations</dd>')).to eq true
+		end
 
 	end
 end
