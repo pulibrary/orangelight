ebb120940ba19148d29c52aae9066b93f91c0bbd
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2014-10-24 10:51:48 -0400

    link to facet search

--- app/controllers/orangelight/browsables_controller.rb
+++ app/controllers/orangelight/browsables_controller.rb
@@ -4,6 +4,9 @@ class Orangelight::BrowsablesController < ApplicationController
   # GET /orangelight/names
   # GET /orangelight/names.json
   def index
+    # if limit isn't specified default is 50
+    # previous/next page links need to pass
+    # manually set limit
     if params[:limit].nil?
       @limit = 50
       @page_link = "?"
@@ -16,17 +19,39 @@ class Orangelight::BrowsablesController < ApplicationController
     else
       @page = params[:page].to_i   
     end
+
+    # gets last page of table's results
+    last_page = params[:model].last.id/@limit+1
+    
+    # makes sure no next page link is shown for last page
+    @is_last = last_page <= @page
+    # makes sure valid page is displayed
+    if @is_last 
+      @page = last_page
+    end
+
+    # @id gets the id of the first entry to display on page
+    # specific ids are given based on search results
     if params[:id].nil?
+      # get id of first entry on given page
       @id = (@page-1)*@limit+1
     else
+      # default is id=1 if none specifed    
       @id = params[:id].to_i          
-    end     
+    end        
 
-    @is_last = (params[:model].last.id/@limit+1) <= @page
+
     @prev = @id/@limit
     @next = @id/@limit+2
     @orangelight_browsables = params[:model].where(id: @id..(@id+@limit-1))
     @model = @orangelight_browsables[0].class.name.demodulize.tableize
+
+    if @model == 'names'
+      @facet = 'author_s'
+    else @model == 'subject'
+      @facet = 'subject_topic_facet'
+    end
+        
     @list_name = @orangelight_browsables[0].class.name.demodulize.titleize
 
   end
