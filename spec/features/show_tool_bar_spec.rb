3d167e3aa283bdfbfe42c4e474ef11cbafff85e7
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2015-08-19 16:03:35 -0400

    update homepage text

--- spec/features/show_tool_bar_spec.rb
+++ spec/features/show_tool_bar_spec.rb
@@ -2,30 +2,32 @@ require 'rails_helper'
 
 describe "Tools links"  do
 
-    before(:each) do
-        visit  "/catalog?search_field=all_fields&q="
-        within ".documents-list" do
-          first(:link).click
-        end
+  before(:each) do
+    visit  "/catalog?search_field=all_fields&q="
+    within ".documents-list" do
+      first(:link).click
     end
+  end
 
-    ['SMS', 'Email', 'Librarian View', 'Cite', 'Classic Catalog'].each do |link_text|
-        it "#{link_text} appears for record view" do
-            find_link(link_text)
-        end
+  ['SMS', 'Email', 'Librarian View', 'Cite', I18n.t('blacklight.voyager')].each do |link_text|
+    it "#{link_text} appears for record view" do
+      within "#main-container" do
+        find_link(link_text)
+      end
     end
+  end
 
-    ['RefWorks', 'EndNote'].each do |link_text|
-        it "provides #{link_text} export options in dropdown" do
-            within "#previousNextDocument li.dropdown" do
-                find_link(link_text)
-            end
-        end
+  ['RefWorks', 'EndNote'].each do |link_text|
+    it "provides #{link_text} export options in dropdown" do
+      within "#previousNextDocument li.dropdown" do
+        find_link(link_text)
+      end
     end
+  end
 
-    ['Add to Folder', 'Send to'].each do |button_text|
-        it "has #{button_text} button" do
-            find_button(button_text)
-        end
+  ['Add to Folder', 'Send to'].each do |button_text|
+    it "has #{button_text} button" do
+      find_button(button_text)
     end
+  end
 end
