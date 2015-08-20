28b4505a3386eb7c0104d84d0b10538386c5fe1f
Axa Mei Liauw <aliauw@princeton.edu>
2015-08-20 10:38:05 -0400

    Vertically align remove button in facets to be in middle. Closes #210
    Get orangelight out of title bar and fix Guided search title. Closes #208
    
    Add x to pivot facet. Closes #213
    
    Add to Folder text to checkbox. Closes #207
    
    Unshame facets and add to folder in detail view on change

--- app/assets/javascripts/orangelight.js
+++ app/assets/javascripts/orangelight.js
@@ -1,6 +1,7 @@
 //= require 'jquery'
 
 $( document ).ready(function() {
+    //link highlighting of subject heirarchy
     $(".search-subject").hover(
         function() {
             $(this).prevAll().addClass("subject-heirarchy");
@@ -8,9 +9,16 @@ $( document ).ready(function() {
             $(this).prevAll().removeClass("subject-heirarchy");
         }
     );
+
+    //tooltip for subject heirarchy
     $(".document").tooltip({
         selector: "[data-toggle='tooltip']",
         placement: "bottom",
         container: "body"
+    });
+
+    //on change, submit form / add to folder
+    $('#folder_id').change(function() {
+        this.form.submit();
     });
 });
