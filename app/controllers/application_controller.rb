edea07337f27547a67745855711a35a61c6a7550
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2015-09-17 02:24:17 -0400

    authorized ips/hosts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
   # include Blacklight::Folders::ApplicationControllerBehavior
+  require 'resolv'
+  before_filter :protect
   # Please be sure to impelement current_user and user_session. Blacklight depends on
   # these methods in order to perform user specific actions.
 
@@ -10,4 +12,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  def protect
+    @ips = load_ip_whitelist
+    @hostnames = load_hostname_whitelist
+    unless @hostnames.include? Resolv.getname(request.remote_ip).downcase or @ips.include? request.remote_ip
+       redirect_to 'http://library.princeton.edu', status: 302
+    end
+  end
+
+  def load_ip_whitelist
+    YAML.load(File.open("./config/ip_whitelist.yml"))
+  end
+
+  def load_hostname_whitelist
+    YAML.load(File.open("./config/hostname_whitelist.yml"))
+  end
 end
