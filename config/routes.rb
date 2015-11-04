5e2a6c175d8357b30390293a909fe304846755ef
Jon Stroop <jpstroop@gmail.com>
2015-11-03 22:23:42 -0500

    moves help to its own page

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,8 @@ Rails.application.routes.draw do
   get '/catalog/issn/:id', to: 'catalog#issn'
 
   get '/notes' => 'high_voltage/pages#show', id: 'notes'
+  get '/help' => 'high_voltage/pages#show', id: 'help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
