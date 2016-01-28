679b663bc640b472847ecd36d97b6c3b6cd3ad2b
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2016-01-28 17:09:28 -0500

    improve email and sms templates

--- spec/models/solr_document_spec.rb
+++ spec/models/solr_document_spec.rb
@@ -51,4 +51,30 @@ RSpec.describe SolrDocument do
       end
     end
   end
+
+  describe "Blacklight::Document::Sms" do
+
+    it "should not include any text if call number not present" do
+      doc = described_class.new()
+      sms_text = doc.to_sms_text
+      expect(sms_text).to eq ''
+    end
+    it "should include call number in text" do
+      doc = described_class.new({call_number_display: ['AB 4209.3']})
+      sms_text = doc.to_sms_text
+      expect(sms_text).to match(/AB 4209.3/)
+    end
+    it "should include all call numbers if there are multiple holdings" do
+      doc = described_class.new({call_number_display: ['AB 4209.3', 'Electronic Resource']})
+      sms_text = doc.to_sms_text
+      expect(sms_text).to match(/AB 4209.3/)
+      expect(sms_text).to match(/Electronic Resource/)
+    end
+    it "should include all call numbers if there are multiple holdings" do
+      doc = SolrDocument.new({call_number_display: ['Electronic Resource', 'Electronic Resource', 'Electronic Resource']})
+      sms_text = doc.to_sms_text
+      expect(sms_text.scan(/Electronic Resource/).length).to eq 1
+    end
+  end
+
 end
