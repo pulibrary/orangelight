ee1470ea3cea2a800d2aa8fd1947221df5324365
Kevin Reiss <kevin.reiss@gmail.com>
2016-04-28 15:04:56 -0500

    Add 'live' request placeholder page. (#466)
    Sanitize param.
    
    Add some specs

---
+++ app/controllers/request_controller.rb
@@ -0,0 +1,12 @@
+class RequestController < ApplicationController
+  include ApplicationHelper
+
+  def show
+    @id = sanitize(params[:id])
+  end
+
+  private
+    def sanitize(str)
+      str.gsub(/[^A-Za-z0-9]/, '')
+    end
+end
