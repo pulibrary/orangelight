c094f1564c7f6a9527491db3fc35f14b88edc8cc
Axa Mei Liauw <aliauw@princeton.edu>
2016-05-31 11:51:14 -0400

    Add back-to-top button to easily scroll back to top on long pages. Closes #556

--- app/assets/javascripts/orangelight.js
+++ app/assets/javascripts/orangelight.js
@@ -80,4 +80,18 @@ $(document).ready(function() {
     $('#search_field').on('change', function(e){
         $('#q').attr('placeholder', $(this).find(':selected').data('placeholder'));
     });
+
+    // Back to top button appears on scroll
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn();
+        } else {
+            $('.back-to-top').fadeOut();
+        }
+    });
+
+    $('.back-to-top').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
 });
