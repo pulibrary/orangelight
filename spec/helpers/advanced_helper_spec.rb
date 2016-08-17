41ba4a14b368d776727379ae4eee22b9ac5cc379
Axa Mei Liauw <aliauw@princeton.edu>
2016-08-17 16:17:50 -0400

    Interface fixes (#671)
    * Adjust print css to put availability panel on first page. Closes #669
    
    * Fix availability label width. Closes #664

--- spec/helpers/advanced_helper_spec.rb
+++ spec/helpers/advanced_helper_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe AdvancedHelper do
     describe 'architecture library' do
       let(:architecture_hash) { subject['Architecture Library'] }
       it 'includes holding location code facet items' do
-        expect(architecture_hash['codes']).to match_array([uesla])
+        expect(architecture_hash['codes']).to match_array([ues, uesla, uesrf])
       end
       it 'includes recap location code facet items' do
         expect(architecture_hash['recap_codes'].include?(rcppw)).to be_truthy
