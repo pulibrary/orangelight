1ea42beb34bf672773a71d7bfca5536415861840
Eliot Jordan <eliotj@princeton.edu>
2016-08-17 21:58:22 -0500

    add controller level helpers

---
+++ spec/support/controller_level_helpers.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+module ControllerLevelHelpers
+  module ControllerViewHelpers
+    include Blacklight::Facet
+
+    def search_state
+      @search_state ||= Blacklight::SearchState.new(params, blacklight_config)
+    end
+
+    def blacklight_configuration_context
+      @blacklight_configuration_context ||= Blacklight::Configuration::Context.new(controller)
+    end
+  end
+
+  def initialize_controller_helpers(helper)
+    helper.extend ControllerViewHelpers
+  end
+end
