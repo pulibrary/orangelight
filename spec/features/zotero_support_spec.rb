d4e663706e443e0f5b69e7f6a062e1d1dbda10e6
Kevin Reiss <kevin.reiss@gmail.com>
2016-09-15 23:35:51 -0500

    Remove skipped zotero spec.

--- spec/features/zotero_support_spec.rb
+++ spec/features/zotero_support_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 describe 'Zotero Support via Context Objects' do
-  xit 'is available on the search results page' do
-    visit '/?f%5Baccess_facet%5D%5B%5D=At+the+Library'
-    expect(page.all('.Z3988').length).to eq 20
-  end
-
   it 'is available on the individual record page' do
     visit '/catalog/9031545'
     expect(page.all('.Z3988').length).to eq 1
