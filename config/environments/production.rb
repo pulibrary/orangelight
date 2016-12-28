1e937344e629592d7122dc2933cb70d4bace1d94
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2016-12-28 12:44:33 -0500

    disables action_view logging in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_view.logger = nil
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx,
