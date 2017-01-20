9eb1a3e8a37bce43b62dc3a13f3e09f5d0af1550
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2017-01-20 12:48:34 -0500

    improve accession number normalization

--- lib/orangelight/string_functions.rb
+++ lib/orangelight/string_functions.rb
@@ -1,11 +1,23 @@
 module StringFunctions
   class << self
     def cn_normalize(str)
-      Lcsort.normalize(str.gsub(/x([A-Z])/, '\1')) || str.upcase
+      if /^CD \d+$/ =~ str # the normalizer thinks "CD 104" is valid LC
+        accession_number(str)
+      else
+        Lcsort.normalize(str.gsub(/x([A-Z])/, '\1')) || accession_number(str)
+      end
     end
 
     def oclc_normalize(oclc, _opts = { prefix: false })
       oclc.gsub(/\D/, '').to_i.to_s
+    end
+
+    private
+
+    def accession_number(str)
+      norm = str.upcase
+      norm = norm.gsub('CD-', 'CD') # cds should file together regardless of dash
+      norm.gsub(/(\d+)$/) { |c| format('%07d', c) } # normalize number to 7-digits
     end
   end
 end
--- spec/lib/orangelight/string_functions_spec.rb
+++ spec/lib/orangelight/string_functions_spec.rb
@@ -4,19 +4,28 @@ RSpec.describe StringFunctions do
   describe '#cn_normalize' do
     describe 'LC call numbers' do
       it 'LC call numbers with a lowercase x normalize the same as cns without it' do
-        expect(StringFunctions.cn_normalize('BP190.5.W35 xF3')).to eq StringFunctions.cn_normalize('BP190.5.W35 F3')
+        expect(described_class.cn_normalize('BP190.5.W35 xF3')).to eq described_class.cn_normalize('BP190.5.W35 F3')
       end
       it 'lowercase x remains when it does not immediately precede a cutter letter' do
-        expect(StringFunctions.cn_normalize('BP190.5.W35 F3x')).to include('X')
+        expect(described_class.cn_normalize('BP190.5.W35 F3x')).to include('X')
+      end
+      it 'valid LC call numbers starting with LC are normalized as such' do
+        expect(described_class.cn_normalize('CD102 .D575 2008')).to eq Lcsort.normalize('CD102 .D575 2008')
       end
     end
 
     describe 'accession numbers' do
       it 'alphabetic characters are made upper case' do
-        microfiche = StringFunctions.cn_normalize('MICROFICHE')
-        microfilm = StringFunctions.cn_normalize('microfilm')
-        microfilm_1 = StringFunctions.cn_normalize('MICROFILM 1')
+        microfiche = described_class.cn_normalize('MICROFICHE')
+        microfilm = described_class.cn_normalize('microfilm')
+        microfilm_1 = described_class.cn_normalize('MICROFILM 1')
         expect(microfiche..microfilm_1).to cover microfilm
+      end
+      it 'CD and CD- file the same' do
+        expect(described_class.cn_normalize('CD 4032')).to eq described_class.cn_normalize('CD- 4032')
+      end
+      it 'the number is sorted as an integer' do
+        expect(described_class.cn_normalize('18th-25')).to be < described_class.cn_normalize('18th-24000')
       end
     end
   end
