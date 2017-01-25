3dc0c3c03c0c8e2e7b771fe52d77c41e79d90c50
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2017-01-25 07:53:15 -0800

    improve normalizing function for accession numbers

--- lib/orangelight/browse_lists.rb
+++ lib/orangelight/browse_lists.rb
@@ -127,8 +127,8 @@ module BrowseLists
     private
 
     # determines if there are multiple locations for the same call number and same bib
-    def multiple_locations? holdings
-      locations = holdings.select { |_k,h| h['library'] != 'Online' }.map { |_k,h| h['location']}.uniq
+    def multiple_locations?(holdings)
+      locations = holdings.select { |_k, h| h['library'] != 'Online' }.map { |_k, h| h['location'] }.uniq
       locations.length > 1
     end
   end
