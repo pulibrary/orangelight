c0fd8a7bf054b4596a218eb8969204d767e72063
Kevin Reiss <kevin.reiss@gmail.com>
2017-06-06 16:13:28 -0500

    Up BD timeout.

--- config/initializers/borrow_direct.rb
+++ config/initializers/borrow_direct.rb
@@ -7,3 +7,4 @@ BorrowDirect::Defaults.library_symbol = 'PRINCETON'
 BorrowDirect::Defaults.api_base = BorrowDirect::Defaults::PRODUCTION_API_BASE
 BorrowDirect::Defaults.api_key = ENV['BD_AUTH_KEY']
 BorrowDirect::Defaults.find_item_patron_barcode = ENV['BD_FIND_BARCODE']
+BorrowDirect::Defaults.timeout = 60
