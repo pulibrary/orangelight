16625101196cdc1f9037ce284b3960e54de34f6e
Kevin Reiss <kevin.reiss@gmail.com>
2017-06-08 09:22:44 -0500

    WIP - Adjust borrow direct timeout, smoother error handling for borrow direct requests, latest requests master. (#889)
    * Adjust borrow direct timeout, latest requests master.
    
    BD frozen form fix.
    
    * Add recap deployement role, various https fixes.

--- config/initializers/borrow_direct.rb
+++ config/initializers/borrow_direct.rb
@@ -9,3 +9,4 @@ BorrowDirect::Defaults.api_key = ENV['BD_AUTH_KEY']
 
 BorrowDirect::Defaults.api_base = BorrowDirect::Defaults::PRODUCTION_API_BASE
 BorrowDirect::Defaults.find_item_patron_barcode = ENV['BD_FIND_BARCODE']
+BorrowDirect::Defaults.timeout = 60
