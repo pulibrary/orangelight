fb67fc7db93e1ce6940a5ebe3cc9ee38162932ce
James R. Griffin III <jrgriffiniii@gmail.com>
2017-12-18 17:05:35 -0500

    Ensuring the availability for the results page is not accessed for bots; Ensuring that the call number browse availability is not accessed for bots

--- app/helpers/orangelight/browsables_helper.rb
+++ app/helpers/orangelight/browsables_helper.rb
@@ -1,15 +1,19 @@
 module Orangelight
   module BrowsablesHelper
-    def should_check_availability?(bib_id)
-      if /^SCSB-\d+/ =~ bib_id
-        false
-      else
-        bib_for_availability(bib_id) == bib_id
-      end
+    def recap_id?(bib_id)
+      /^SCSB-\d+/ =~ bib_id
     end
 
     def bib_for_availability(bib_id)
       bib_id.to_i.to_s
+    end
+
+    def valid_bib_id?(bib_id)
+      bib_for_availability(bib_id) == bib_id
+    end
+
+    def should_check_availability?(bib_id)
+      !recap_id?(bib_id) && valid_bib_id?(bib_id) && render_availability?
     end
   end
 end
