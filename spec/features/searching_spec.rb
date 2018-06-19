a6a70b15f5b1c7fb7be6e59086d23f392ee5fad2
christinach <actspatial@gmail.com>
2018-06-18 22:39:26 -0400

    removes selected value in chosen select-advanced search form when user removes it

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -28,4 +28,16 @@ describe 'searching' do
       expect(page).to have_selector '.icon-request-reading-room[aria-hidden="true"]'
     end
   end
+
+  context 'chosen selected values' do
+    it 'removes a chosen selected value' do
+      visit '/catalog?utf8=%E2%9C%93&f1=all_fields&q3=&f_inclusive%5Bformat%5D%5B%5D=Journal&search_field=advanced&commit=Search'
+      expect(page).to have_link 'Edit search'
+      page.find(:xpath, '//*[@id="editSearchLink"]').click
+      expect(current_url).to include 'f_inclusive%5Bformat%5D%5B%5D=Journal'
+      page.find(:xpath, '//select[@id="format"]/option[4]').unselect_option
+      page.find(:xpath, '//*[@id="advanced-search-submit"]').click
+      expect(current_url).not_to include 'f_inclusive%5Bformat%5D%5B%5D=Journal'
+    end
+  end
 end
