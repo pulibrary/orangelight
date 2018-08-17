725714df64fea87d91d70cf02b6eb9456355ca0c
GriffinJ <jrgriffiniii@gmail.com>
2018-08-17 09:49:09 -0400

    Handling cases where BlacklightAdvancedSearch::ParsingNestingParser#process_query encounters a nil advanced Blacklight configuration (instead of an OpenStructWithHashAccess)

---
+++ spec/helpers/blacklight_advanced_search/parsing_nesting_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BlacklightAdvancedSearch::ParsingNestingParser do
+  describe '#process_query' do
+    subject(:queries) { process_query(_params, config) }
+
+    let(:_params) { instance_double(ActiveSupport::HashWithIndifferentAccess) }
+    # instance_double cannot be used to stub #advanced_search for Blacklight::Configuration
+    let(:config) { double('Blacklight::Configuration') }
+    let(:advanced_search) { instance_double(Blacklight::OpenStructWithHashAccess) }
+    let(:keyword_queries) do
+      {
+        'title' => 'anything',
+        'all_fields' => '(something) OR (anything)'
+      }
+    end
+
+    before do
+      allow(config).to receive(:advanced_search).and_return(advanced_search)
+      allow(self).to receive(:local_param_hash).with('all_fields', config).and_return({})
+      allow(self).to receive(:local_param_hash).with('title', config).and_return("spellcheck.dictionary": 'title', qf: '$title_qf', pf: '$title_pf')
+      allow(self).to receive(:keyword_op).and_return([])
+      allow(self).to receive(:keyword_queries).and_return(keyword_queries)
+    end
+
+    it 'appends the operators to the queries' do
+      allow(advanced_search).to receive(:[]).with(:query_parser).and_return('edismax')
+      expect(queries).to include('_query_:"{!edismax spellcheck.dictionary=title qf=$title_qf pf=$title_pf}anything"  _query_:"{!dismax mm=1}something anything"')
+    end
+
+    context 'when the query parser is not specified in the advanced search' do
+      let(:advanced_search) { nil }
+
+      before do
+        allow(Blacklight.logger).to receive(:error)
+      end
+
+      it 'logs an error and returns an empty query' do
+        expect(queries).to be_empty
+        expect(Blacklight.logger).to have_received(:error).with(/Failed to parse the advanced search, advanced search config\. settings are not accessible for\:/)
+      end
+    end
+  end
+end
