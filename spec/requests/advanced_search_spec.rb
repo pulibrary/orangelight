bf596e5692aa8cbec92b67b69c741bc2274bb673
GriffinJ <jrgriffiniii@gmail.com>
2018-11-05 16:22:53 -0500

    Providing redirection for cases where clients attempt to query advanced#index with catalog queries

---
+++ spec/requests/advanced_search_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Orangelight advanced search', type: :request do
+  it 'redirects search requests to the catalog search' do
+    get '/advanced?f[subject_facet][]=United+Nations-Decision+making&id=3681146&page=1&per_page=20'
+    expect(response.status).to eq(302)
+    expect(response).to redirect_to('/catalog')
+  end
+  it 'renders the advanced search form' do
+    get '/advanced?f[subject_facet][]=United+Nations-Decision+making'
+    expect(response.status).to eq(200)
+  end
+end
