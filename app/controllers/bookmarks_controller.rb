4834489352d1eac91db841c3cf43141774e6787a
Esmé Cowles <escowles@ticklefish.org>
2018-12-03 15:18:10 -0500

    Adding CSV download to bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 class BookmarksController < CatalogController
   include Blacklight::Bookmarks
   configure_blacklight do |_config|
@@ -12,11 +14,52 @@ class BookmarksController < CatalogController
   end
 
   def print
-    bookmarks = token_or_current_or_guest_user.bookmarks
-    bookmark_ids = bookmarks.collect { |b| b.document_id.to_s }
-
-    _, @documents = fetch(bookmark_ids)
+    fetch_bookmarked_documents
     @url_gen_params = {}
     render 'record_mailer/email_record.text.erb', template: false, content_type: 'text/plain'
   end
+
+  def csv
+    fetch_bookmarked_documents
+    send_data csv_output, type: 'text/csv', filename: "bookmarks-#{Time.zone.today}.csv"
+  end
+
+  private
+
+    def fetch_bookmarked_documents
+      bookmarks = token_or_current_or_guest_user.bookmarks
+      bookmark_ids = bookmarks.collect { |b| b.document_id.to_s }
+      _, @documents = fetch(bookmark_ids)
+    end
+
+    # byte-order-mark declaring our output as UTF-8 (required for non-ASCII to be handled by Excel)
+    def csv_bom
+      %w[EF BB BF].map { |a| a.hex.chr }.join
+    end
+
+    def csv_fields
+      {
+        id: 'ID',
+        title_display: 'Title',
+        author_citation_display: 'Author',
+        format: 'Format',
+        language_facet: 'Language',
+        pub_citation_display: 'Published/Created',
+        pub_date_display: 'Date',
+        description_display: 'Description',
+        series_display: 'Series',
+        location_display: 'Location',
+        call_number_display: 'Call Number',
+        notes_display: 'Notes'
+      }
+    end
+
+    def csv_output
+      CSV.generate(csv_bom, headers: true) do |csv|
+        csv << csv_fields.values
+        @documents.each do |doc|
+          csv << csv_fields.keys.map { |field| Array(doc[field]).join('; ') }
+        end
+      end
+    end
 end
--- spec/controllers/bookmarks_controller_spec.rb
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -14,4 +14,21 @@ RSpec.describe BookmarksController do
       expect(response).to render_template 'record_mailer/email_record.text.erb'
     end
   end
+
+  describe '#csv' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:headers) { 'ID,Title,Author,Format,Language,Published/Created,Date,Description,Series,Location,Call Number,Notes' }
+    let(:data) { ['9741216', 'Adriaen van de Velde : Dutch master of landscape / Bart Cornelis, Marijn Schapelhouman.', 'Cornelis, Bart; Velde, Adriaen van de; Schapelhouman, Marijn; Rijksmuseum (Netherlands); Dulwich Picture Gallery', 'Book', 'English', 'London: Paul Holberton Publishing', '2016', '224 pages : illustrations (some color) ; 29 cm', 'Marquand Library', 'ND653.V414 A4 2016', 'Published to accompany the exhibition held at the Rijksmuseum, Amsterdam, 24 June - 15 September 2016 and Dulwich Picture Gallery, London, 12 October 2016 - 15 January 2017.'] }
+
+    it 'renders a CSV list of metadata' do
+      sign_in user
+      user.bookmarks.create!([{ document_id: '9741216', document_type: 'SolrDocument' }])
+      get :csv
+      expect(assigns(:documents).length).to eq 1
+      expect(response.body).to include(headers)
+      data.each do |value|
+        expect(response.body).to include(value)
+      end
+    end
+  end
 end
