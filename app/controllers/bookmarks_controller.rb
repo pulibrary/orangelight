f28605f1a9caff0f513b8018c301eb6b2694f659
Esmé Cowles <escowles@ticklefish.org>
2018-12-06 13:22:18 -0500

    Adding CSV download to bookmarks (#1491)
    * Adding CSV download to bookmarks
    
    * Changing author/title fields
    
    * Retrieve all bookmarks for print/CSV
    
    * Separating Roman and original script authors/titles into separate fields

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 class BookmarksController < CatalogController
   include Blacklight::Bookmarks
   configure_blacklight do |_config|
@@ -12,11 +14,62 @@ class BookmarksController < CatalogController
   end
 
   def print
-    bookmarks = token_or_current_or_guest_user.bookmarks
-    bookmark_ids = bookmarks.collect { |b| b.document_id.to_s }
-
-    _, @documents = fetch(bookmark_ids)
+    fetch_bookmarked_documents
     @url_gen_params = {}
     render 'record_mailer/email_record.text.erb', template: false, content_type: 'text/plain'
   end
+
+  def csv
+    fetch_bookmarked_documents
+    send_data csv_output, type: 'text/csv', filename: "bookmarks-#{Time.zone.today}.csv"
+  end
+
+  private
+
+    def fetch_bookmarked_documents
+      bookmarks = token_or_current_or_guest_user.bookmarks
+      bookmark_ids = bookmarks.collect { |b| b.document_id.to_s }
+      _, @documents = fetch(bookmark_ids, rows: bookmark_ids.length)
+    end
+
+    # byte-order-mark declaring our output as UTF-8 (required for non-ASCII to be handled by Excel)
+    def csv_bom
+      %w[EF BB BF].map { |a| a.hex.chr }.join
+    end
+
+    def csv_fields
+      {
+        id: 'ID',
+        title_citation_display: ['Title', 'Title (Original Script)'],
+        author_display: ['Author', 'Author (Original Script)'],
+        format: 'Format',
+        language_facet: 'Language',
+        pub_citation_display: 'Published/Created',
+        pub_date_display: 'Date',
+        description_display: 'Description',
+        series_display: 'Series',
+        location_display: 'Location',
+        call_number_display: 'Call Number',
+        notes_display: 'Notes'
+      }
+    end
+
+    def csv_output
+      CSV.generate(csv_bom, headers: true) do |csv|
+        csv << csv_fields.values.flatten
+        @documents.each do |doc|
+          csv << csv_fields.keys.map { |field| csv_values(doc, field) }.flatten
+        end
+      end
+    end
+
+    def csv_values(doc, field)
+      csv_fields[field].is_a?(Array) ? two_values(doc[field]) : Array(doc[field]).join('; ')
+    end
+
+    def two_values(arr)
+      values = arr || ['', '']
+      values << '' if values.length == 1
+      values.map { |v| v.chomp(' /') }
+    end
 end
