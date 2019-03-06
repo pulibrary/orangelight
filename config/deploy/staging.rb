3518f66ccc3f1b7934f9b6fa98f9b95e887deeea
Eliot Jordan <eliotj@princeton.edu>
2019-03-06 17:35:27 -0600

    Add capistrano tasks to set environment variables on staging server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,51 +1,46 @@
 # frozen_string_literal: true
 
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}
-# role :web, %w{deploy@example.com}
-# role :db,  %w{deploy@example.com}
-
 set :rvm_ruby_string, :local # use the same ruby as used locally for deployment
-
 set :rails_env, 'staging'
-
 set :branch, ENV['BRANCH'] || 'master'
-
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
 
 server 'catalog-staging1', user: 'deploy', roles: %i[web app db]
 
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# And/or per server (overrides global)
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+namespace :env do
+  desc 'Set an Orangelight environment variable'
+  task :set do |_task, args|
+    on roles(:app) do
+      abort "Environment variables and values must be specified. `env:set['ENV_VAR=value']`" if args.extras.empty?
+      config_file = '/home/deploy/app_configs/orangelight'
+      args.extras.each do |arg|
+        variable, value = arg.split('=', 2)
+        abort "Environment variable and value must be specified. `env:set['ENV_VAR=value']`" if value.nil?
+        within release_path do
+          execute("sed -i -e 's/#{variable}=.*/#{variable}=#{value.gsub('/', '\/')}/' #{config_file}")
+        end
+      end
+
+      # Print out app_config file
+      within release_path do
+        execute :cat, config_file
+      end
+
+      # Restart passenger
+      invoke 'deploy:restart'
+    end
+  end
+
+  desc 'Set Orangelight to use figgy production'
+  task :figgy_production do
+    on roles(:app) do
+      Rake::Task['env:set'].invoke('GRAPHQL_API_URL=https://figgy.princeton.edu/graphql', 'FIGGY_URL=https://figgy.princeton.edu')
+    end
+  end
+
+  desc 'Set Orangelight to use figgy staging'
+  task :figgy_staging do
+    on roles(:app) do
+      Rake::Task['env:set'].invoke('GRAPHQL_API_URL=https://figgy-staging.princeton.edu/graphql', 'FIGGY_URL=https://figgy-staging.princeton.edu')
+    end
+  end
+end
