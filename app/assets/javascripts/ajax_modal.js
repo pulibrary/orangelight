fc1b438d9bfaf73772af5dbbebcd97756f8389d2
christinach <actspatial@gmail.com>
2019-07-09 12:11:36 -0400

    update ajax-modal name - remove ajax

--- app/assets/javascripts/ajax_modal.js
+++ app/assets/javascripts/ajax_modal.js
@@ -2,8 +2,8 @@
 
 // A function used as an event handler on loaded.blacklight.ajax-modal
 // to catch contained data-ajax-modal=closed directions
-Blacklight.ajaxModal.check_close_ajax_modal = function(event) {
-  if ($(event.target).find(Blacklight.ajaxModal.modalCloseSelector).length) {
+Blacklight.modal.check_close_ajax_modal = function(event) {
+  if ($(event.target).find(Blacklight.modal.modalCloseSelector).length) {
     modal_flashes = $(this).find('.flash_messages');
 
     window.setTimeout(function(){
