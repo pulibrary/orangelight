25b6a113d1535ed68eb84671ffa8ce1dee3416cf
Eliot Jordan <eliotj@princeton.edu>
2019-07-15 18:54:28 -0500

    Fix issue with datadog middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,5 +103,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.middleware.insert_after Datadog::Contrib::Rack::TraceMiddleware, Orangelight::Middleware::InvalidParameterHandler
 end
