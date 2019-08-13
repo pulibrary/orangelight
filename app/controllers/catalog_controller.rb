0a709be412d102115e7fb3e46455eb2e00195ea2
Eliot Jordan <eliotj@princeton.edu>
2019-08-13 14:11:56 -0500

    Fix show tools actions

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -63,6 +63,11 @@ class CatalogController < ApplicationController
     #   :q => query
     # }
 
+    config.add_show_tools_partial(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
+    config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
+    config.add_show_tools_partial(:sms, if: :render_sms_action?, callback: :sms_action, validator: :validate_sms_params)
+    config.add_show_tools_partial(:citation)
+
     config.navbar.partials.delete(:search_history)
     config.add_nav_action(:reserves, partial: 'course_reserves/nav')
 
