4f3162490ae58fe37e4aa3cd3f3dd648ac83fd2a
Eliot Jordan <eliotj@princeton.edu>
2019-08-20 13:07:35 -0500

    Refactor header links spec for better performance

--- spec/features/header_links_spec.rb
+++ spec/features/header_links_spec.rb
@@ -7,69 +7,46 @@ describe 'Tools links' do
 
   context 'With MARC-based records' do
     before do
-      visit  '/catalog?search_field=all_fields&q='
-      within '.documents-list' do
-        first(:link).click
-      end
+      visit '/catalog/10585552'
     end
 
-    [I18n.t('blacklight.header_links.course_reserves'), I18n.t('blacklight.header_links.bookmarks')].each do |link_text|
-      it "#{link_text} appears for navbar" do
-        find('.navbar-item * a', text: link_text)
-      end
-    end
+    it 'displays links in the navbar, account dropdown, and record view' do
+      expect(page).to have_selector('.navbar-item * a', text: 'Course Reserves')
+      expect(page).to have_selector('.navbar-item * a', text: 'Bookmark')
+      expect(page).to have_button('Send to')
 
-    [I18n.t('blacklight.header_links.login'), I18n.t('blacklight.header_links.search_history')].each do |link_text|
-      it "#{link_text} appears for your account dropdown" do
-        within '.menu--level-1' do
-          find_link(link_text)
-        end
+      within '.menu--level-1' do
+        expect(page).to have_link('Login')
+        expect(page).to have_link('Search History')
       end
-    end
 
-    ['SMS', 'Email', I18n.t('blacklight.tools.librarian_view'), 'Cite'].each do |link_text|
-      it "#{link_text} appears for record view" do
-        within '#main-container' do
-          find_link(link_text)
-        end
+      within '#main-container' do
+        expect(page).to have_link('SMS')
+        expect(page).to have_link('Email')
+        expect(page).to have_link('Staff view')
+        expect(page).to have_link('Cite')
       end
-    end
 
-    %w[RefWorks EndNote].each do |link_text|
-      it "provides #{link_text} export options in dropdown" do
-        within '.search-widgets li.dropdown' do
-          find_link(link_text)
-        end
-      end
-    end
-
-    # ['Add to Folder', 'Send to'].each do |button_text|
-    ['Send to'].each do |button_text|
-      it "has #{button_text} button" do
-        find_button(button_text)
+      within '.search-widgets li.dropdown' do
+        expect(page).to have_link('RefWorks')
+        expect(page).to have_link('EndNote')
       end
     end
   end
 
   context 'With non-MARC-based records' do
     before do
-      visit  '/catalog?search_field=all_fields&q=dsp01ft848s955'
-      within '.documents-list' do
-        first(:link).click
-      end
+      visit  '/catalog/dsp01ft848s955'
     end
 
-    it 'does not have a cite link' do
+    it 'does not have cite, RefWorks, or EndNote links' do
       within '#main-container' do
         expect(page).not_to have_link('Cite')
       end
-    end
 
-    %w[RefWorks EndNote].each do |link_text|
-      it "provides #{link_text} export options in dropdown" do
-        within '.search-widgets li.dropdown' do
-          expect(page).not_to have_link(link_text)
-        end
+      within '.search-widgets li.dropdown' do
+        expect(page).not_to have_link('RefWorks')
+        expect(page).not_to have_link('EndNote')
       end
     end
   end
