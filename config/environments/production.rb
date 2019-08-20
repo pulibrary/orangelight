c784a9145b9978c6e350fa52a8fcdb4061f0cadb
Eliot Jordan <eliotj@princeton.edu>
2019-08-19 19:18:18 -0500

    Fix searching spec

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.middleware.use Orangelight::Middleware::InvalidParameterHandler
 end
