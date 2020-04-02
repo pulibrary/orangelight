82e8d3a39413c0d40c0da12d0d3e511f42f92818
Carolyn Cole <cac9@princeton.edu>
2020-04-02 14:22:21 -0400

    changing to grep the file instead of looking it up in the database. It is slower, but is working

--- app/services/hathi_compare.rb
+++ app/services/hathi_compare.rb
@@ -19,32 +19,43 @@ class HathiCompare
     end
   end
 
-  def process_solr_query(max: 5435394, rows: 200)
+  def process_solr_query(max: 5435394, rows: 200, hathi_file: "/Users/colec/Downloads/overlap_20200316_princeton.tsv")
     unmatched_results = []
     matched_results = []
     start = 0
     while(start< max) do
-      umatch, match = process_data(start: start)
+      umatch, match = process_data(start: start, rows: rows)
       unmatched_results << umatch
       matched_results << match
-      start +=20
+      start +=rows
       unmatched_results= unmatched_results.flatten
       matched_results= matched_results.flatten
       puts "processed #{start} results unmatched: #{unmatched_results.count} matched: #{matched_results.count} ratio matched #{matched_results.count/start.to_f} ratio unmatched #{unmatched_results.count/start.to_f}"
+      puts "**** Unatched ***\n#{umatch}"
+      puts "**** Matched ***\n#{match}"
     end
     [unmatched_results, matched_results]
   end
 
   private
-    def process_data(start: 0)
+    def process_data(start: , rows: )
       unmatched_results = []
       matched_results = []
-      solr_data = query_solr(start: start)
+      solr_data = query_solr(start: start, rows: rows)
+      puts "doc count = #{solr_data["response"]["docs"].count}"
       solr_data["response"]["docs"].each do |doc|
         oclc = doc["oclc_s"].first
-        url = HathiUrl.new(oclc_id: oclc, lccn: nil, isbn: nil)
+        id = doc["id"]
+        url = nil
+        begin
+          url = HathiUrl.new(oclc_id: oclc, lccn: nil, isbn: nil)
+        rescue JSON::ParserError
+          sleep 30
+          retry
+        end
         if (url.present?)
-          match = HathiPulMatch.find_by(oclc: oclc)
+          match = `grep '#{oclc+"\t"+id}' /Users/colec/Downloads/overlap_20200316_princeton.tsv`
+          # match = HathiPulMatch.find_by(oclc: oclc)
           if match.blank?
             unmatched_results << oclc 
           else
@@ -55,7 +66,7 @@ class HathiCompare
       [unmatched_results, matched_results]
     end
 
-    def query_solr(start: 0, rows: 2000)
+    def query_solr(start:, rows:)
       uri = URI("#{solr_base_url}&start=#{start}&format=json&rows=#{rows}")
       json_data = Net::HTTP.get(uri)
       JSON.parse(json_data)
