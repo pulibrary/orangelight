4ea22c693f49e066cc31b4c66b1c598650b2cbf8
Carolyn Cole <cac9@princeton.edu>
2020-04-03 10:05:07 -0400

    Adding a catch and retry for the JSON parse error fixes #1998

--- spec/services/hathi_url_spec.rb
+++ spec/services/hathi_url_spec.rb
@@ -16,6 +16,32 @@ RSpec.describe HathiUrl do
     end
   end
 
+  context 'with a valid oclc number, but multiple exception' do
+    let(:oclc_id) { '42579288' }
+    it 'returns nil' do
+      allow(JSON).to receive(:parse).and_raise JSON::ParserError
+      hathi_url = described_class.new(oclc_id: oclc_id, maximum_retries: 1, sleep_duration: 0.5)
+      expect(hathi_url.url).to be_nil
+    end
+  end
+
+  context 'with a valid oclc number, but an exception' do
+    let(:oclc_id) { '42579288' }
+    it 'returns hathi item url' do
+      first = true
+      allow(JSON).to receive(:parse).and_wrap_original do |m, *args|
+        if first
+          first = false
+          raise JSON::ParserError
+        else
+          m.call(*args)
+        end
+      end
+      hathi_url = described_class.new(oclc_id: oclc_id, maximum_retries: 1, sleep_duration: 0.5)
+      expect(hathi_url.url).to eq("#{hathi_base_url}mdp.39015047450062")
+    end
+  end
+
   context 'with an invalid oclc number' do
     let(:oclc_id) { '42579288x' }
     it 'returns nil' do
