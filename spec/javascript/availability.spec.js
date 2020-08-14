1d4541852d89bd27df26a0b28a481d8cd98b1284
Anna Headley <anna.headley@gmail.com>
2020-08-14 11:10:11 -0400

    Add availability test for record_ids from a search results page

--- spec/javascript/availability.spec.js
+++ spec/javascript/availability.spec.js
@@ -5,7 +5,7 @@ describe('AvailabilityUpdater', function() {
     expect(updater).not.toBe(undefined)
   })
 
-  test('record_ids()', () => {
+  test('record_ids() on a show page', () => {
     document.body.innerHTML =
       '<div>' +
       '<li data-availability-record="true" data-record-id="8938641" data-holding-id="8856502" data-aeon="false">' +
@@ -15,4 +15,39 @@ describe('AvailabilityUpdater', function() {
     let u = new updater
     expect(u.record_ids()).toEqual(['8938641'])
   })
+
+  test('record_ids() on a search results page', () => {
+    document.body.innerHTML =
+      '<div id="documents" class="documents-list">' +
+        '<article>' +
+          '<div class="row">' +
+            '<div class="record-wrapper">' +
+              '<ul class="document-metadata dl-horizontal dl-invert">' +
+                '<li class="blacklight-holdings">' +
+                  '<ul>' +
+                    '<li data-availability-record="true" data-record-id="10585552" data-holding-id="10329434" data-aeon="false"></li>' +
+                  '</ul>' +
+                '</li>' +
+              '</ul>' +
+            '</div>' +
+          '</div>' +
+        '</article>' +
+        '<article>' +
+          '<div class="row">' +
+            '<div class="record-wrapper">' +
+              '<ul class="document-metadata dl-horizontal dl-invert">' +
+                '<li class="blacklight-holdings">' +
+                  '<ul>' +
+                    '<li data-availability-record="true" data-record-id="7058493" data-holding-id="6938508" data-aeon="false">' +
+                  '</ul>' +
+                '</li>' +
+              '</ul>' +
+            '</div>' +
+          '</div>' +
+        '</article>' +
+      '</div>'
+    let u = new updater
+    expect(u.record_ids()).toEqual(['10585552', '7058493'])
+  })
+
 })
