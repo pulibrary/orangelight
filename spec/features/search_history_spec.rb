8c0f515f6a9a1ec364625b38446063b49cc82765
Anna Headley <anna.headley@gmail.com>
2021-05-11 16:14:10 -0400

    Add login button to search history page

---
+++ spec/features/search_history_spec.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'search history' do
+  context 'when using Voyager' do
+    describe 'login link' do
+      it 'brings user to account page on login' do
+        valid_patron_response = fixture('/bibdata_patron_response.json')
+        voyager_account_response = fixture('/generic_voyager_account_response.xml')
+        valid_voyager_patron = JSON.parse('{"patron_id": "77777"}').with_indifferent_access
+        valid_patron_record_uri = "#{ENV['voyager_api_base']}/vxws/MyAccountService?patronId=#{valid_voyager_patron[:patron_id]}&patronHomeUbId=1@DB"
+        # I'm not sure why current_user gets created with this id but it seems
+        # to happen reliably
+        stub_request(:get, "#{ENV['bibdata_base']}/patron/1234")
+          .to_return(status: 200, body: valid_patron_response, headers: {})
+        stub_request(:get, valid_patron_record_uri)
+          .to_return(status: 200, body: voyager_account_response, headers: {})
+        visit '/search_history'
+        click_button "Your Account"
+        click_link "Login"
+        click_link "Princeton faculty, staff, and students log in with NetID"
+        expect(current_path).to eq account_path
+      end
+    end
+  end
+
+  describe 'action buttons' do
+    context 'when using Voyager' do
+      context 'and no searches have been performed' do
+        it 'does not have a login button' do
+          visit '/search_history'
+          within('#content') do
+            expect(page).not_to have_link("Login")
+          end
+        end
+      end
+
+      context 'and a search has been performed' do
+        it 'has a clear search history button and no login button' do
+          stub_holding_locations
+          visit '/'
+          find_button('search').click
+          visit '/search_history'
+          within('#content') do
+            expect(page).to have_link("Clear search history")
+            expect(page).not_to have_link("Login")
+          end
+        end
+      end
+    end
+
+    context 'when using Alma, not logged in' do
+      before do
+        allow(Rails.configuration).to receive(:use_alma).and_return(true)
+      end
+      context 'and no searches have been performed' do
+        it 'has a login button' do
+          visit '/search_history'
+          within('#content') do
+            expect(page).to have_link("Login", class: "btn-primary")
+          end
+        end
+
+        it 'brings user back to search history page on login' do
+          visit '/search_history'
+          click_link "Login"
+          click_link "Princeton faculty, staff, and students log in with NetID"
+          expect(current_path).to eq blacklight.search_history_path
+        end
+      end
+
+      context 'and searches have been performed' do
+        it 'has a login button' do
+          stub_holding_locations
+          visit '/'
+          find_button('search').click
+          visit '/search_history'
+          within('#content') do
+            expect(page).to have_link("Login", class: "btn-primary")
+          end
+        end
+      end
+    end
+
+    context 'when using Alma, logged in' do
+      let(:user) { FactoryBot.create(:user) }
+      before do
+        allow(Rails.configuration).to receive(:use_alma).and_return(true)
+        login_as user
+      end
+
+      it 'does not have a login button' do
+        visit '/search_history'
+        within('#content') do
+          expect(page).not_to have_link("Login", class: "btn-primary")
+        end
+      end
+    end
+  end
+end
