3d5dbaa6eb0891f6827372c193b29d2ad2016f37
Carolyn Cole <cac9@princeton.edu>
2021-05-21 15:06:42 -0400

    Updating requests to the alma branch so we can deploy the latest version to a server

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,7 @@ module Orangelight
     config.robots = OpenStruct.new(config_for(:robots))
 
     config.use_alma = ActiveModel::Type::Boolean.new.cast(ENV['USE_ALMA'])
+
+    config.alma = config_for(:alma).with_indifferent_access
   end
 end
