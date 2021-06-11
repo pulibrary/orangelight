e62f8f131b3855385868bf88e94652263fbec501
jrgriffiniii <jrgriffiniii@gmail.com>
2021-06-11 10:26:58 -0400

    Fixing the system/catalog_show_spec to use 4609321 for the catalog ID in the second test case

--- spec/system/catalog_show_spec.rb
+++ spec/system/catalog_show_spec.rb
@@ -3,36 +3,20 @@
 require 'rails_helper'
 
 describe 'Viewing Catalog Documents', type: :system, js: true do
-  let(:availability_fixture_path) do
-    File.join(fixture_path, 'bibdata', 'availability.json')
-  end
-  let(:availability_fixture) do
-    File.read(availability_fixture_path)
-  end
+  let(:availability_fixture_path) { File.join(fixture_path, 'bibdata', 'availability.json') }
+  let(:availability_fixture) { File.read(availability_fixture_path) }
 
   before do
     stub_holding_locations
   end
 
   context 'when the Document references a Figgy Resource' do
-    let(:solr_url) do
-      Blacklight.connection_config[:url]
-    end
-    let(:solr) do
-      RSolr.connect(url: solr_url)
-    end
-    let(:document_id) do
-      '9946093213506421'
-    end
-    let(:document_fixture_path) do
-      Rails.root.join('spec', 'fixtures', 'alma', "#{document_id}.json")
-    end
-    let(:document_fixture_content) do
-      File.read(document_fixture_path)
-    end
-    let(:document_fixture) do
-      JSON.parse(document_fixture_content)
-    end
+    let(:solr_url) { Blacklight.connection_config[:url] }
+    let(:solr) { RSolr.connect(url: solr_url) }
+    let(:document_id) { '9946093213506421' }
+    let(:document_fixture_path) { Rails.root.join('spec', 'fixtures', 'alma', "#{document_id}.json") }
+    let(:document_fixture_content) { File.read(document_fixture_path) }
+    let(:document_fixture) { JSON.parse(document_fixture_content) }
 
     before do
       solr.add(document_fixture)
@@ -49,9 +33,7 @@ describe 'Viewing Catalog Documents', type: :system, js: true do
     end
 
     context 'when the Figgy GraphQL responds with a different bib. ID' do
-      let(:document_id) do
-        '9947652213506421'
-      end
+      let(:document_id) { '4609321' }
 
       it 'renders the thumbnail using the IIIF Manifest' do
         visit "catalog/#{document_id}"
