1c772360abc6fa690fef291f616e027ceb046a0e
Eliot Jordan <eliotj@princeton.edu>
2021-06-11 11:43:12 -0500

    Update ready only mode environment variable names

--- config/initializers/read_only_mode.rb
+++ config/initializers/read_only_mode.rb
@@ -2,12 +2,12 @@
 
 module Orangelight
   def read_only_mode
-    @read_only_mode ||= ENV.fetch("ORANGELIGHT_READ_ONLY_MODE", false) == "true"
+    @read_only_mode ||= ENV.fetch("OL_READ_ONLY_MODE", false) == "true"
   end
 
   def read_only_message
     default_msg = "Catalog is in read-only mode."
-    @read_only_message ||= ENV.fetch("ORANGELIGHT_READ_ONLY_MESSAGE", default_msg)
+    @read_only_message ||= ENV.fetch("OL_READ_ONLY_MESSAGE", default_msg)
   end
 
   module_function :read_only_mode, :read_only_message
