c14d962056a2078b8256f55ac62c5aa615b5eb75
jrgriffiniii <jrgriffiniii@gmail.com>
2021-06-14 13:35:07 -0400

    Correcting the RSpec test suite titles AccountController and OmniauthCallbacksController test suites; Updating the error message for 429 API errors for authenticating users

--- spec/controllers/account_controller_spec.rb
+++ spec/controllers/account_controller_spec.rb
@@ -123,7 +123,7 @@ RSpec.describe AccountController do
     let(:invalid_user) { FactoryBot.create(:invalid_princeton_patron) }
     let(:unauthorized_user) { FactoryBot.create(:unauthorized_princeton_patron) }
 
-    it 'Returns Princeton Patron Account Data using a NetID' do
+    it 'returns Princeton Patron Account Data using a persisted User Model' do
       valid_patron_record_uri = "#{Requests.config['bibdata_base']}/patron/#{valid_user.uid}"
       stub_request(:get, valid_patron_record_uri)
         .to_return(status: 200, body: valid_patron_response, headers: {})
@@ -132,7 +132,7 @@ RSpec.describe AccountController do
       expect(patron).to be_truthy
     end
 
-    it "Returns false when an ID doesn't exist" do
+    it "returns a nil value when a user ID cannot be resolved to a persisted User Model" do
       invalid_patron_record_uri = "#{Requests.config['bibdata_base']}/patron/#{invalid_user.uid}"
       stub_request(:get, invalid_patron_record_uri)
         .to_return(status: 404, body: '<html><title>Not Here</title><body></body></html>', headers: {})
@@ -140,7 +140,7 @@ RSpec.describe AccountController do
       expect(patron).to be nil
     end
 
-    it "Returns false when the application isn't authorized to access patron data" do
+    it "returns a nil value when the application isn't authorized to access patron data" do
       unauthorized_patron_record_uri = "#{Requests.config['bibdata_base']}/patron/#{unauthorized_user.uid}"
       stub_request(:get, unauthorized_patron_record_uri)
         .to_return(status: 403, body: '<html><title>Not Authorized</title><body></body></html>', headers: {})
@@ -148,7 +148,7 @@ RSpec.describe AccountController do
       expect(patron).to be nil
     end
 
-    it 'Returns false when the http response throws 500' do
+    it 'returns a nil value when the HTTP response to the API request has a 500 status code' do
       valid_patron_record_uri = "#{Requests.config['bibdata_base']}/patron/#{valid_user.uid}"
       stub_request(:get, valid_patron_record_uri)
         .to_return(status: 500, body: 'Error', headers: {})
