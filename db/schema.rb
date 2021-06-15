a72b9eaf67da4473851b737b488633d7afe6875a
Trey Pendragon <tpendragon@princeton.edu>
2021-06-15 14:39:15 -0700

    Use special browse tables for Alma.
    If use_alma is true the system will use these special tables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_202403) do
+ActiveRecord::Schema.define(version: 2021_06_15_212034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "alma_orangelight_call_numbers", id: :serial, force: :cascade do |t|
+    t.text "label"
+    t.string "dir"
+    t.text "scheme"
+    t.text "sort"
+    t.text "title"
+    t.text "author"
+    t.text "date"
+    t.text "bibid"
+    t.string "holding_id"
+    t.string "location"
+    t.index ["label"], name: "index_alma_orangelight_call_numbers_on_label"
+    t.index ["sort"], name: "index_alma_orangelight_call_numbers_on_sort"
+  end
+
+  create_table "alma_orangelight_name_titles", id: :serial, force: :cascade do |t|
+    t.text "label"
+    t.integer "count"
+    t.text "sort"
+    t.string "dir"
+    t.index ["sort"], name: "index_alma_orangelight_name_titles_on_sort"
+  end
+
+  create_table "alma_orangelight_names", id: :serial, force: :cascade do |t|
+    t.text "label"
+    t.integer "count"
+    t.text "sort"
+    t.string "dir"
+    t.index ["sort"], name: "index_alma_orangelight_names_on_sort"
+  end
+
+  create_table "alma_orangelight_subjects", id: :serial, force: :cascade do |t|
+    t.text "label"
+    t.integer "count"
+    t.text "sort"
+    t.string "dir"
+    t.index ["sort"], name: "index_alma_orangelight_subjects_on_sort"
+  end
 
   create_table "bookmarks", id: :serial, force: :cascade do |t|
     t.integer "user_id", null: false
