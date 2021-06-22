5c2322decc5b14755dfb2ce76c005953785d8c40
Anna Headley <anna.headley@gmail.com>
2021-06-22 16:52:35 -0400

    Use array join instead of string interpolation with chomp
    Also add test coverage for locations display logic in
    holding_reqeusts_adapter
    
    Co-authored-by: Hector Correa <hector@hectorcorrea.com>

--- spec/support/webmock_stubs.rb
+++ spec/support/webmock_stubs.rb
@@ -6,6 +6,12 @@ def stub_holding_locations
                body: File.read(File.join(fixture_path, 'bibdata', 'holding_locations.json')))
 end
 
+def stub_alma_holding_locations
+  stub_request(:get, "#{Requests.config['bibdata_base']}/locations/holding_locations.json")
+    .to_return(status: 200,
+               body: File.read(File.join(fixture_path, 'bibdata', 'alma', 'holding_locations.json')))
+end
+
 def stub_test_document
   stub_request(:get, "#{Requests.config['bibdata_base']}/bibliographic/test-id")
     .to_return(status: 200, body: '')
