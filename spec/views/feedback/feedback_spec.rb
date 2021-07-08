96cdc02c37a9221c6f1e85431e86ff87aaaffbef
Eliot Jordan <eliotj@princeton.edu>
2021-07-08 15:04:47 -0500

    Refactor account functionality for Alma

--- spec/views/feedback/feedback_spec.rb
+++ spec/views/feedback/feedback_spec.rb
@@ -45,15 +45,10 @@ describe 'Feedback Form', type: :feature do
   context 'Princeton Community User has signed in' do
     let(:user) { FactoryBot.create(:valid_princeton_patron) }
     let(:valid_patron_response) { fixture('/bibdata_patron_response.json') }
-    let(:voyager_account_response) { fixture('/generic_voyager_account_response.xml') }
-    let(:valid_voyager_patron) { JSON.parse('{"patron_id": "77777"}').with_indifferent_access }
 
     it 'Populates Email Field' do
       stub_request(:get, "#{Requests.config['bibdata_base']}/patron/#{user.uid}")
         .to_return(status: 200, body: valid_patron_response, headers: {})
-      valid_patron_record_uri = "#{ENV['voyager_api_base']}/vxws/MyAccountService?patronId=#{valid_voyager_patron[:patron_id]}&patronHomeUbId=1@DB"
-      stub_request(:get, valid_patron_record_uri)
-        .to_return(status: 200, body: voyager_account_response, headers: {})
       sign_in user
       visit('/catalog/4747577')
       click_link('Feedback')
