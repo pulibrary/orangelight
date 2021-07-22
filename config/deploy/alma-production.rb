19a370c39f22fbb097c79ba1612530de91859716
Anna Headley <anna.headley@gmail.com>
2021-07-22 11:59:32 -0400

    Set all the prod boxes to be workers
    So that sneakers will restart
    
    Co-authored-by: Trey Pendragon <tpendragon@princeton.edu>

--- config/deploy/alma-production.rb
+++ config/deploy/alma-production.rb
@@ -13,11 +13,11 @@ set :branch, ENV['BRANCH'] || 'main'
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'catalog1', user: 'deploy', roles: %i[web app db]
-server 'catalog2', user: 'deploy', roles: %i[web app db sitemap]
-server 'catalog-indexer1', user: 'deploy', roles: %i[cron_prod1 cron_db]
-server 'catalog-indexer2', user: 'deploy', roles: %i[cron_prod2]
-server 'catalog-indexer3', user: 'deploy', roles: %i[cron_prod3]
+server 'catalog1', user: 'deploy', roles: %i[web app db worker]
+server 'catalog2', user: 'deploy', roles: %i[web app db sitemap worker]
+server 'catalog-indexer1', user: 'deploy', roles: %i[cron_prod1 cron_db worker]
+server 'catalog-indexer2', user: 'deploy', roles: %i[cron_prod2 worker]
+server 'catalog-indexer3', user: 'deploy', roles: %i[cron_prod3 worker]
 
 set :deploy_to, '/opt/orangelight'
 set :log_level, :info
