bc81cc25db0b8e9f782beed3785415d6b21abe70
Anna Headley <anna.headley@gmail.com>
2021-07-22 16:29:00 -0400

    Append readonly message to existing message
    Instead of showing just one or the other
    Co-authored-by: Eliot Jordan <eliot.jordan@gmail.com>

--- spec/views/shared/_announcement.html.erb_spec.rb
+++ spec/views/shared/_announcement.html.erb_spec.rb
@@ -13,15 +13,7 @@ RSpec.describe 'shared/_announcement' do
       allow(Orangelight).to receive(:read_only_mode).and_return(true)
       allow(Orangelight).to receive(:read_only_message).and_return("test message")
       render
-      expect(rendered).to match(/test message/)
-    end
-  end
-
-  context 'in Alma mode' do
-    it 'renders an Alma message' do
-      allow(Rails.configuration).to receive(:use_alma).and_return(true)
-      render
-      expect(rendered).to match(/Catalog Alma QA site/)
+      expect(rendered).to match(/System upgrade.* test message/)
     end
   end
 end
