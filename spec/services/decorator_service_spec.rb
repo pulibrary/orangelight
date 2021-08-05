2a38af35ed46170370b05d3d4c01d5b236ad3205
Hector Correa <hector_correa@princeton.edu>
2021-08-05 11:10:16 -0400

    Make author_display an array to better mimic real data

--- spec/services/decorator_service_spec.rb
+++ spec/services/decorator_service_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe DecoratorService::SolrDocumentDecorator do
   it "handles records without a title" do
-    solr_doc = SolrDocument.new(id: "99123923123506421", author_display: "Wosk, Yosef")
+    solr_doc = SolrDocument.new(id: "99123923123506421", author_display: ["Wosk, Yosef"])
     service = described_class.new(document: solr_doc)
     expect(service.title).to be nil
   end
