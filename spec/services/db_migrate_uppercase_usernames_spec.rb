d063513d105b41c1a895f4b6bd08373a7c03281c
zaymasood <zmasood@princeton.edu>
2021-08-13 12:40:29 -0400

    create tests for self.run instead of individual methods

--- spec/services/db_migrate_uppercase_usernames_spec.rb
+++ spec/services/db_migrate_uppercase_usernames_spec.rb
@@ -5,38 +5,39 @@ require 'rails_helper'
 RSpec.describe DBMigrateUppercaseUsernames do
   let(:username) { "testUSER123" }
   let!(:uppercase_user) { FactoryBot.create(:user, username: username) }
-  let!(:lowercase_user) { FactoryBot.create(:user, username: username.downcase) }
-  let(:migrator) { described_class.new }
+  let(:lowercase_user) { FactoryBot.create(:user, username: username.downcase) }
 
-  describe ".find_uppercase_users" do
-    it "finds users with uppercase letters in their usernames" do
-      expect(migrator.find_uppercase_users).to eq([uppercase_user])
+  describe "self.run" do
+    context "when there is an equivalent lowercase user" do
+      it "merges bookmarks into existing user" do
+        lowercase_user
+        uppercase_user.bookmarks.create([{ document_id: '9741216', document_type: 'SolrDocument' }])
+        expect { described_class.run }.to change(lowercase_user.bookmarks, :count).by(1)
+      end
+
+      it "merges searches into existing user" do
+        lowercase_user
+        uppercase_user.searches.create([{ query_params: 'history' }])
+        expect { described_class.run }.to change(lowercase_user.searches, :count).by(1)
+      end
     end
-  end
 
-  describe ".find_lowercase_user" do
-    it "finds user with lowercase equivalent of uppercase username" do
-      expect(migrator.find_lowercase_user(uppercase_user)).to eq(lowercase_user)
-    end
-  end
+    context "when there is not an equivalent lowercase user" do
+      it "creates a new lowercase user" do
+        expect { described_class.run }.to change(User, :count).by(1)
+      end
 
-  describe ".merge_bookmarks" do
-    it "transfer bookmarks to user with lowercase username" do
-      uppercase_user.bookmarks.create!([{ document_id: '9741216', document_type: 'SolrDocument' }])
-      expect { migrator.merge_bookmarks(uppercase_user, lowercase_user) }.to change(lowercase_user.bookmarks, :count).by(1)
-    end
-  end
+      it "merges bookmarks into new user" do
+        uppercase_user.bookmarks.create([{ document_id: '9741216', document_type: 'SolrDocument' }])
+        described_class.run
+        expect(User.last.bookmarks.count).to eq(1)
+      end
 
-  describe ".merge_searches" do
-    it "merge searches" do
-      uppercase_user.searches.create!([{ query_params: 'history' }])
-      expect { migrator.merge_searches(uppercase_user, lowercase_user) }.to change(lowercase_user.searches, :count).by(1)
-    end
-  end
-
-  describe ".delete_uppercase_user" do
-    it "deletes users with uppercase letters in their usernames" do
-      expect { migrator.delete_uppercase_user(uppercase_user) }.to change(User, :count).by(-1)
+      it "merges searches into new user" do
+        uppercase_user.searches.create([{ query_params: 'history' }])
+        described_class.run
+        expect(User.last.searches.count).to eq(1)
+      end
     end
   end
 end
