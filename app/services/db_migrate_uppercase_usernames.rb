9243db64c7d7c7f54c64d5d1c2b6174700cf77d6
zaymasood <zmasood@princeton.edu>
2021-08-13 09:14:16 -0400

    add lowercase user code to separate class

--- app/services/db_migrate_uppercase_usernames.rb
+++ app/services/db_migrate_uppercase_usernames.rb
@@ -2,13 +2,11 @@
 
 class DBMigrateUppercaseUsernames
   def self.run
-    find_uppercase_users.map { |uppercase_user|
-      find_lowercase_user(uppercase_user).map { |lowercase_user|
-        merge_bookmarks(uppercase_user, lowercase_user)
-        merge_searches(uppercase_user, lowercase_user)
-        delete_uppercase_user(uppercase_user)
-      }
-    }
+    new.run
+  end
+
+  def run
+    find_uppercase_users.map { |uppercase_user| UserLowercaser.new(uppercase_user).convert }
   end
 
   # find all users with uppercase letters in their username
@@ -16,27 +14,36 @@ class DBMigrateUppercaseUsernames
     User.where("uid ~ ?", "[A-Z]").to_a
   end
 
-  # find lowercase version of the user with uppercase letters in their username
-  def find_lowercase_user(uppercase_user)
-    User.find_by(username: uppercase_user.uid.downcase)
-  end
+  class UserLowercaser
+    attr_reader :uppercase_user
 
-  # merge uppercase user's bookmarks into lowercase user's bookmarks
-  def merge_bookmarks(uppercase_user, lowercase_user)
-    uppercase_user_bookmarks = Bookmark.where(user_id: uppercase_user.id)
-    lowercase_user_bookmarks = Bookmark.where(user_id: lowercase_user.id)
-    lowercase_user.bookmarks = uppercase_user_bookmarks | lowercase_user_bookmarks
-  end
+    def initialize(uppercase_user)
+      @uppercase_user = uppercase_user
+    end
 
-  # merge uppercase user's searches into lowercase user's searches
-  def merge_searches(uppercase_user, lowercase_user)
-    uppercase_user_searches = Search.where(user_id: uppercase_user.id)
-    lowercase_user_searches = Search.where(user_id: lowercase_user.id)
-    lowercase_user.searches = uppercase_user_searches | lowercase_user_searches
-  end
+    def convert
+      lowercase_user = find_lowercase_user
+      merge_bookmarks(lowercase_user)
+      merge_searches(lowercase_user)
+    end
 
-  # delete the duplicate account
-  def delete_uppercase_user(uppercase_user)
-    uppercase_user.destroy
+    # find lowercase version of the user with uppercase letters in their username
+    def find_lowercase_user
+      User.find_by(username: uppercase_user.uid.downcase)
+    end
+
+    # merge uppercase user's bookmarks into lowercase user's bookmarks
+    def merge_bookmarks(lowercase_user)
+      uppercase_user_bookmarks = Bookmark.where(user_id: uppercase_user.id)
+      lowercase_user_bookmarks = Bookmark.where(user_id: lowercase_user.id)
+      lowercase_user.bookmarks = uppercase_user_bookmarks | lowercase_user_bookmarks
+    end
+
+    # merge uppercase user's searches into lowercase user's searches
+    def merge_searches(lowercase_user)
+      uppercase_user_searches = Search.where(user_id: uppercase_user.id)
+      lowercase_user_searches = Search.where(user_id: lowercase_user.id)
+      lowercase_user.searches = uppercase_user_searches | lowercase_user_searches
+    end
   end
 end
