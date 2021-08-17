18b83a55c0197c50e7a51d593c6c99af6cf49412
christinach <actspatial@gmail.com>
2021-08-17 14:27:11 -0400

    Move to pending holding_locations_sort_order_spec until #2660

--- spec/features/holding_locations_sort_order_spec.rb
+++ spec/features/holding_locations_sort_order_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 # See issue https://github.com/pulibrary/orangelight/issues/2660
 # Do we still need this sort when there are recap holdings?
 context 'viewing record with multiple holdings' do
-  it 'shows east asian holding before recap holding' do
+  xit 'shows east asian holding before recap holding' do
     stub_holding_locations
     visit '/catalog/9938615393506421'
     locations = all('.location-text').map(&:text)
