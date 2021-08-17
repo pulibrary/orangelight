d09eceeaa44bc3405622f2451d84ba32a527cda9
christinach <actspatial@gmail.com>
2021-08-17 11:25:06 -0400

    Update citation_spec.rb and header_links_spec.rb

--- spec/features/citation_spec.rb
+++ spec/features/citation_spec.rb
@@ -3,12 +3,18 @@
 require 'rails_helper'
 
 describe 'citation' do
-  let(:bibid) { '9979948663506421' }
-
+  before do
+    stub_request(:get, "#{Requests.config['bibdata_base']}/bibliographic/9979948663506421")
+      .with(
+        headers: {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'Faraday v0.17.4'
+        }
+      )
+      .to_return(status: 200, body: "", headers: {})
+  end
   it 'will render successfully even if there is not a subfield a' do
-    stub_request(:get, "#{Requests.config['bibdata_base']}/bibliographic/#{bibid}")
-      .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent' => 'Faraday v0.15.3' })
-      .to_return(status: 200, body: '', headers: {})
     visit '/catalog/9979948663506421/citation'
     expect(current_url).to include '/catalog/9979948663506421/citation'
     expect(page.status_code).to eq 200
