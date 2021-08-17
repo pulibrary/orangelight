fe799a45ee0e24a19f7e0f7e9d31236b23212942
christinach <actspatial@gmail.com>
2021-08-17 11:34:45 -0400

    Update holding_locations_sort_order follow up issue #2660

--- spec/features/holding_locations_sort_order_spec.rb
+++ spec/features/holding_locations_sort_order_spec.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-
+# See issue https://github.com/pulibrary/orangelight/issues/2660
+# Do we still need this sort when there are recap holdings?
 context 'viewing record with multiple holdings' do
   it 'shows east asian holding before recap holding' do
     stub_holding_locations
     visit '/catalog/9938615393506421'
     locations = all('.location-text').map(&:text)
-    expect(locations).to eq ['East Asian Library - Reference', 'ReCAP']
+    expect(locations).to eq ["ReCAP - Remote Storage", "East Asian Library - Reference"]
   end
 end
