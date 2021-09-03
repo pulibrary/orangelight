d91d3c8ba58bd20524d29afc88aca8d7b83a473d
Hector Correa <hector_correa@princeton.edu>
2021-09-03 09:21:46 -0400

    Made test more descriptive, remove deprecated word dummy too

--- spec/models/concerns/blacklight/solr/document/marc_export_override_spec.rb
+++ spec/models/concerns/blacklight/solr/document/marc_export_override_spec.rb
@@ -2,15 +2,17 @@
 require 'rails_helper'
 
 RSpec.describe Blacklight::Solr::Document::MarcExportOverride do
-  # See https://mixandgo.com/learn/how-to-test-ruby-modules-with-rspec for information on
-  # testing methods defined in modules.
-  let(:dummy_class) do
-    Class.new do
-      extend Blacklight::Solr::Document::MarcExportOverride
+  describe "#clean_end_punctuation" do
+    # See https://mixandgo.com/learn/how-to-test-ruby-modules-with-rspec for information on
+    # testing methods defined in modules.
+    let(:sample_class) do
+      Class.new do
+        include Blacklight::Solr::Document::MarcExportOverride
+      end
     end
-  end
 
-  it 'handles nil values' do
-    expect(dummy_class.clean_end_punctuation(nil)).to eq ""
+    it 'handles nil values' do
+      expect(sample_class.new.clean_end_punctuation(nil)).to eq ""
+    end
   end
 end
