d25997266a86df3d263b8515890a2ed7288dcdeb
Hector Correa <hector_correa@princeton.edu>
2021-10-05 15:53:17 -0400

    Add to the allow list our new redirect-to-alma origin path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
       request.flash.keep('notice')
       params[:origin].chomp('/email')
     elsif !request.env['omniauth.origin'].nil? &&
-          /request|borrow-direct|email|bookmarks|search_history/.match(request.env['omniauth.origin'])
+          /request|borrow-direct|email|bookmarks|search_history|redirect-to-alma/.match(request.env['omniauth.origin'])
       request.env['omniauth.origin']
     else
       account_path
