e995ec0ac6f90779c2ef985deaac4f13b34a3ea9
Hector Correa <hector_correa@princeton.edu>
2021-10-06 11:00:22 -0400

    Adjust the tests to the new URL

--- spec/features/login_account_spec.rb
+++ spec/features/login_account_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 
 describe 'Account login' do
   let(:user) { FactoryBot.create(:user) }
-  let(:alma_account_url) { "https://princeton.alma.exlibrisgroup.com/discovery/account?vid=01PRI_INST:Services&lang=EN&section=overview" }
+  let(:login_and_redirect_to_alma_url) { "/users/sign_in?origin=%2Fredirect-to-alma" }
 
   describe 'Your Account menu', js: true do
-    xit "lists correct options when not logged in" do
+    it "lists correct options when not logged in" do
       visit "/"
       click_button("Your Account")
       within('li.show') do
         link = find_link("Library Account")
-        expect(link[:href]).to eq alma_account_url
+        expect(link[:href]).to include login_and_redirect_to_alma_url
         expect(link[:target]).to eq("_blank")
         expect(has_css?('i.fa-external-link', count: 1)).to eq true
         expect(page).to have_link("Digitization Requests", href: digitization_requests_path)
@@ -21,13 +21,13 @@ describe 'Account login' do
       end
     end
 
-    xit "lists correct options when logged in" do
+    it "lists correct options when logged in" do
       login_as user
       visit "/"
       click_button(user.username)
       within('li.show') do
         link = find_link("Library Account")
-        expect(link[:href]).to eq alma_account_url
+        expect(link[:href]).to include login_and_redirect_to_alma_url
         expect(link[:target]).to eq("_blank")
         expect(has_css?('i.fa-external-link', count: 1)).to eq true
         expect(page).to have_link("Digitization Requests", href: digitization_requests_path)
