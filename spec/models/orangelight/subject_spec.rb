25cae974512faf893cc077ca0d94b000d5ed36c9
Christina Chortaria <actspatial@gmail.com>
2021-10-07 10:33:20 -0400

    Add indigenous studies fixture generated from https://github.com/pulibrary/bibdata/pull/1676 (#2756)
    Update browselists subject table
    Test that table is been created

---
+++ spec/models/orangelight/subject_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Orangelight::Subject do
+  it "initializes to the alma tables" do
+    described_class.reset_table_name
+
+    expect { described_class.new }.not_to raise_error
+    expect(described_class.table_name).to eq "alma_orangelight_subjects"
+  end
+end
