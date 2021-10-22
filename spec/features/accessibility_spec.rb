a28b304d22cea780cd469bab5aebca2675344128
christinach <actspatial@gmail.com>
2021-10-22 17:12:24 -0400

    add test for record page test for tags wcag2aa,wcag2a, wcag21a, ,wcag21aa

--- spec/features/accessibility_spec.rb
+++ spec/features/accessibility_spec.rb
@@ -2,11 +2,30 @@
 require "rails_helper"
 
 describe "accessibility", type: :feature, js: true do
+  before do
+    stub_alma_holding_locations
+  end
   context "home page" do
-    it "complies with ..." do
+    before do
       visit "/"
+    end
 
+    it "complies with wcag" do
       expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa)
+        .skipping("duplicate-id-active") # Issue https://github.com/pulibrary/orangelight/issues/2796
+    end
+  end
+
+  context "record page" do
+    before do
+      visit "catalog/9931488793506421"
+    end
+    it "complies with wcag2aa wcag21a" do
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa)
+        .skipping("duplicate-id-active") # Issue https://github.com/pulibrary/orangelight/issues/2796
+        .skipping("link-name") # Issue https://github.com/pulibrary/orangelight/issues/2799
     end
   end
 end
