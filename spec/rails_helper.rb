9b65dbb2c38e7b0f72492ce2a340ef5f6d05961d
Christina Chortaria <actspatial@gmail.com>
2021-12-15 10:07:33 -0500

    Auto accessibility audit (#2824)
    * Auto audit home page
    
    * add test for record page
    test for tags wcag2aa,wcag2a, wcag21a, ,wcag21aa

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'devise'
+require "axe-rspec"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
