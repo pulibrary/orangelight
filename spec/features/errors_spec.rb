557009a4bb99f352bfb1ade1460a209bdb51991f
Christina Chortaria <actspatial@gmail.com>
2022-03-31 14:53:15 -0400

    Replace email for 500 error and 404 missing page, (#2907)
    also replaced public files for consistency. They are not used

--- spec/features/errors_spec.rb
+++ spec/features/errors_spec.rb
@@ -9,6 +9,16 @@ describe 'errors' do
       visit '/404'
       expect(page.status_code).to eq 404
       expect(page).to have_content("The page you were looking for doesn't exist")
+      expect(page).to have_selector(:css, 'a[href="mailto:catalog-feedback@princeton.libanswers.com"]')
+    end
+  end
+
+  describe '500 page' do
+    it "when there is an Internal Server Error" do
+      visit '/500'
+      expect(page.status_code).to eq 500
+      expect(page).to have_content("We're sorry, but there was a problem processing the page you requested")
+      expect(page).to have_selector(:css, 'a[href="mailto:catalog-feedback@princeton.libanswers.com"]')
     end
   end
 
@@ -16,6 +26,7 @@ describe 'errors' do
     it 'redirects to the 404 page' do
       visit '/catalog/3861539wrong'
       expect(page.status_code).to eq 404
+      expect(page).to have_selector(:css, 'a[href="mailto:catalog-feedback@princeton.libanswers.com"]')
     end
   end
 end
