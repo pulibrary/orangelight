e26fbc7376ae64594205fd4d7dcaf744d2fea054
Max Kadel <mkadel@princeton.edu>
2022-07-28 16:17:26 -0400

    Add almost all values to jsonld

--- app/models/concerns/blacklight/document/json_ld.rb
+++ app/models/concerns/blacklight/document/json_ld.rb
@@ -16,7 +16,7 @@ module Blacklight::Document::JsonLd
 
   def data
     metadata = {}
-    metadata["@context"] = "http://bibdata.princeton.edu/context.json"
+    metadata["@context"] = "#{default_host}/context.json"
     metadata["@id"] = Rails.application.routes.url_helpers.solr_document_url(id: self['id'], host: default_host)
     metadata["title"] = title
     metadata["language"] = title_language
@@ -25,6 +25,15 @@ module Blacklight::Document::JsonLd
       values = values.first if values.size == 1
       metadata[metadata_key] = values unless values.empty?
     end
+    metadata.merge!(contributors)
+    metadata.merge! creator
+    metadata['created'] = date(true) if date(true)
+    metadata['date'] = date if date
+    metadata['abstract'] = abstract if abstract
+    metadata['identifier'] = identifier if identifier
+    # local_identifier?
+    metadata['location'] = location if location
+    metadata['electronic_locations'] = electronic_links if electronic_links.present?
 
     metadata
   end
@@ -86,6 +95,87 @@ module Blacklight::Document::JsonLd
 
   def title_language
     self['language_code_s'].first
+  end
+
+  def contributors
+    related_name_display = self['related_name_json_1display']
+    return {} unless related_name_display
+
+    contributors = {}
+    JSON.parse(related_name_display).each do |role, names|
+      contributors[check_role(role)] = names
+    end
+    contributors
+  end
+
+  def creator
+    role = check_role((self['marc_relator_display'] || []).first)
+    return { role => self['author_display'].first } unless role == 'contributor'
+    {}
+  end
+
+  def check_role(label)
+    role = (label || '').parameterize(separator: '_').singularize
+    RELATORS.include?(role) ? role : 'contributor'
+  end
+
+  def date(expanded = false)
+    return self['compiled_created_display'].first if expanded == false && self['compiled_created_display']
+    return unless self['pub_date_start_sort']
+    date = self['pub_date_start_sort'].to_s
+    date += "-01-01T00:00:00Z" if expanded
+    end_date = self['pub_date_end_sort'].to_s || ''
+    unless end_date.empty?
+      date += expanded ? "/" + end_date + "-12-31T23:59:59Z" : "-" + end_date
+    end
+
+    date
+  end
+
+  def abstract
+    (self['summary_note_display'] || []).first
+  end
+
+  # I'm not as confident about this one - the version in bibdata uses a bunch of local methods to build this field
+  # but it appears to only display ark links, which appear to only be iiif manifest links? Or at least that's what it looks like
+  # from the tests?
+  def identifier
+    return unless electronic_locations
+
+    return unless electronic_locations['iiif_manifest_paths']
+
+    electronic_locations['iiif_manifest_paths'].keys.first
+  end
+
+  def electronic_locations
+    return unless self['electronic_access_1display']
+
+    JSON.parse(self['electronic_access_1display'])
+  end
+
+  def location
+    return unless self['location'] && self['call_number_display']
+
+    values = []
+    self['location'].each do |location_code|
+      self['call_number_display'].each do |call_number|
+        values << "#{location_code} #{call_number}"
+      end
+    end
+    values
+  end
+
+  def electronic_links
+    @electronic_links ||= begin
+      return unless electronic_locations
+
+      electronic_locations.map do |uri, label|
+        {
+          "@id" => uri,
+          "label" => label
+        }
+      end
+    end
   end
 
   def default_host
--- spec/models/json_ld_spec.rb
+++ spec/models/json_ld_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe Blacklight::Document::JsonLd do
   let(:solr_doc) { SolrDocument.new(properties) }
 
   describe '#export_as_jsonld' do
-    context 'For a standard MARC Record' do
+    context 'with a standard MARC Record' do
       let(:id) { '9996180723506421' }
 
       it 'creates a jsonld document' do
-        expect(doc['@context']).to eq('http://bibdata.princeton.edu/context.json')
+        expect(doc['@context']).to eq('http://localhost:3000/context.json')
         expect(doc['@id']).to eq('http://localhost:3000/catalog/9996180723506421')
         expect(doc['title']).to be_instance_of Array
         expect(doc['title'].first).to be_instance_of Hash
@@ -32,24 +32,72 @@ RSpec.describe Blacklight::Document::JsonLd do
                                              '@language' => "kor-Latn"
                                            })
         expect(doc['language']).to eq('kor')
+        expect(doc['editor']).to eq(['Pak, Chong-guk, 1935-', '박 종국, 1935-'])
+        expect(doc['created']).to eq("2008-01-01T00:00:00Z")
+        expect(doc['date']).to eq('2008')
+        expect(doc['identifier']).to be nil
+        expect(doc['location']).to eq(['ReCAP BQ2043.K6 T757 2008'])
       end
     end
-    context 'For an English-language journal' do
+    context 'with an English-language journal' do
       let(:id) { '995597013506421' }
 
       it 'creates a jsonld document' do
-        expect(doc['@context']).to eq('http://bibdata.princeton.edu/context.json')
+        expect(doc['@context']).to eq('http://localhost:3000/context.json')
         expect(doc['@id']).to eq('http://localhost:3000/catalog/995597013506421')
         expect(doc['title'].size).to eq 1
         expect(doc['title'].first['@language']).to eq('eng')
         expect(doc['language']).to eq('eng')
+        expect(doc['contributor']).to eq(["New York Botanical Garden", "Mycological Society of America"])
+        expect(doc['created']).to eq("1909-01-01T00:00:00Z/9999-12-31T23:59:59Z")
+        expect(doc['date']).to eq('1909-9999')
       end
     end
-    context 'For an artwork' do
+    context 'with an artwork' do
       let(:id) { '99106471643506421' }
 
       it 'creates a jsonld document' do
         expect(doc['creator']).to eq("Avery, Eric, born 1948")
+        expect(doc['created']).to eq("1984-01-01T00:00:00Z")
+        expect(doc['date']).to eq('1984')
+      end
+    end
+
+    context 'with a monograph with an author' do
+      let(:id) { '9956200533506421' }
+
+      it 'creates a jsonld document' do
+        expect(doc['creator']).to eq("Dagen, Philippe")
+        expect(doc['author']).to eq("Dagen, Philippe")
+        expect(doc['created']).to eq("2008-01-01T00:00:00Z")
+        expect(doc['date']).to eq('2008')
+      end
+    end
+
+    context 'with a special collections codex' do
+      let(:id) { '9952615993506421' }
+      it 'creates a jsonld document' do
+        expect(doc['abstract']).to include('an abridgment of part 3 of Miftāḥ al-ʻulūm')
+      end
+    end
+
+    context 'with a digitized manuscript' do
+      let(:id) { '9946093213506421' }
+
+      it 'creates a jsonld document' do
+        expect(doc['identifier']).to eq("http://arks.princeton.edu/ark:/88435/7d278t10z")
+        expect(doc['electronic_locations']).to eq([{
+                                                    "@id" => "https://catalog.princeton.edu/catalog/4609321#view",
+                                                    "label" => ["Digital content"]
+                                                  },
+                                                   {
+                                                     "@id" => "https://drive.google.com/open?id=0B3HwfRG3YqiNVVR4bXNvRzNwaGs",
+                                                     "label" => ["drive.google.com", "Curatorial documentation"]
+                                                   },
+                                                   {
+                                                     "@id" => "iiif_manifest_paths",
+                                                     "label" => { "http://arks.princeton.edu/ark:/88435/7d278t10z" => "https://figgy.princeton.edu/concern/scanned_resources/d446107a-bdfd-4a5d-803c-f315b7905bf4/manifest" }
+                                                   }])
       end
     end
   end
