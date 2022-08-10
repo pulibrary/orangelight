f4bd5311fda1bb77b431c509e6daa1afcf4c0139
Max Kadel <mkadel@princeton.edu>
2022-08-10 11:03:44 -0400

    Encode username for authorization against alma

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -70,6 +70,46 @@ RSpec.describe Users::OmniauthCallbacksController do
         expect(user.uid).to eq '22101002369676'
       end
     end
+    context 'valid alma user' do
+      let(:user) { FactoryBot.create(:alma_patron) }
+      let(:omniauth_response) { OmniAuth::AuthHash.new(provider: 'alma', uid: user.uid) }
+      let(:expected_url) { 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/Student?op=auth&password=whatever' }
+
+      before do
+        stub_request(:post, expected_url)
+          .to_return(status: 204)
+        controller.request.env['omniauth.auth'] = omniauth_response
+      end
+      it 'redirects to account path' do
+        get :alma, params: { username: user.username, password: 'whatever' }
+
+        expect(WebMock).to have_requested(:post, expected_url)
+        expect(response).to redirect_to(account_path)
+      end
+      context "with added spaces in the middle of the username from form" do
+        let(:user) { FactoryBot.create(:alma_patron, username: "Juan Hernandez") }
+        let(:expected_url) { 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/Juan%20Hernandez?op=auth&password=whatever' }
+
+        it 'redirects to account path' do
+          get :alma, params: { username: user.username, password: 'whatever' }
+
+          expect(WebMock).to have_requested(:post, expected_url)
+          expect(response).to redirect_to(account_path)
+        end
+      end
+      context "with an added space at the begginning of the username" do
+        let(:user) { FactoryBot.create(:alma_patron, username: "BC001111111") }
+        let(:expected_url) { 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/BC001111111?op=auth&password=whatever' }
+
+        it 'redirects to account path' do
+          get :alma, params: { username: user.username.to_s, password: 'whatever' }
+
+          expect(WebMock).to have_requested(:post, expected_url)
+          expect(response).to redirect_to(account_path)
+        end
+      end
+    end
+
     it 'invalid patron redirects to login page' do
       allow(User).to receive(:from_barcode) { valid_barcode_user }
       allow(Bibdata).to receive(:get_patron) { {} }
