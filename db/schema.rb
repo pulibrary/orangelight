9ae606d8ac7d3942b3ce2e038eeff30623d41cfa
Max Kadel <mkadel@princeton.edu>
2022-08-15 15:28:30 -0400

    Add admin to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_15_122519) do
+ActiveRecord::Schema.define(version: 2022_08_15_145521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2022_08_15_122519) do
     t.string "uid"
     t.string "username"
     t.boolean "guest", default: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username"
