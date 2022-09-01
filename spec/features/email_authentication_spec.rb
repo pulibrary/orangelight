420e5c83f6d63f8ebeb99e9464a612ad6b79c7a1
Max Kadel <mkadel@princeton.edu>
2022-09-01 13:37:24 -0400

    Put needed stubs back

--- spec/features/email_authentication_spec.rb
+++ spec/features/email_authentication_spec.rb
@@ -5,6 +5,12 @@ require 'rails_helper'
 describe 'email form' do
   let(:bibid) { '9979948663506421' }
   let(:user) { FactoryBot.create(:valid_princeton_patron) }
+  let(:valid_patron_response) { fixture('/bibdata_patron_response.json') }
+
+  before do
+    current_illiad_user_uri = "#{Requests::Config[:illiad_api_base]}/ILLiadWebPlatform/Users/jstudent"
+    stub_request(:get, current_illiad_user_uri).to_return(status: 404, body: '{"Message":"User jstudent was not found."}')
+  end
 
   it 'requires user to sign in' do
     visit "/catalog/#{bibid}/email"
@@ -12,6 +18,8 @@ describe 'email form' do
   end
 
   it 'shows send button for authenticated users' do
+    stub_request(:get, "#{Requests.config['bibdata_base']}/patron/#{user.uid}")
+      .to_return(status: 200, body: valid_patron_response, headers: {})
     sign_in user
     visit "/catalog/#{bibid}/email"
     expect(page).to have_button('Send')
