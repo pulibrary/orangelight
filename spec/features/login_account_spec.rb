711632f65c4db21566a30f952d991d2c265c1d96
Max Kadel <mkadel@princeton.edu>
2022-10-05 10:21:51 -0400

    Test a person trying to hack the request url

--- spec/features/login_account_spec.rb
+++ spec/features/login_account_spec.rb
@@ -71,12 +71,12 @@ describe 'Account login' do
       end
     end
   end
-  describe 'Alma account login from requests page' do
-    let(:alma_user) { FactoryBot.create(:valid_alma_patron) }
-    let(:valid_patron_response) { File.open(fixture_path + '/bibdata_patron_response.json') }
-    let(:valid_patron_record_uri) { "#{Requests.config['bibdata_base']}/patron/#{alma_user.uid}" }
-    let(:expected_login_url) { 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/Alma%20Patron?op=auth&password=foobarfoo' }
+  describe 'Account login from requests page' do
     before do
+      stub_request(:post, 'https://scsb.recaplib.org:9093/sharedCollection/bibAvailabilityStatus')
+        .to_return(status: 200, body: [{ itemBarcode: 'CU71562478', itemAvailabilityStatus: "Available" }].to_json)
+      stub_request(:get, "#{Requests::Config[:bibdata_base]}/patron/1234?ldap=true")
+        .to_return(status: 200, body: patron_response, headers: {})
       stub_holding_locations
       stub_delivery_locations
       stub_request(:get, "#{Requests::Config[:pulsearch_base]}/catalog/SCSB-2143785/raw")
@@ -84,73 +84,102 @@ describe 'Account login' do
       stub_request(:get, "#{Requests::Config[:bibdata_base]}/locations/holding_locations/scsbcul.json")
         .to_return(status: 200, body: fixture('/bibdata/scsbcul_holding_locations.json'))
       stub_request(:get, "#{Requests::Config[:bibdata_base]}/bibliographic/SCSB-2143785/holdings/2110046/availability.json")
-        .to_return(status: 400)
-      stub_request(:post, expected_login_url)
-        .to_return(status: 204)
-      stub_request(:get, valid_patron_record_uri)
-        .to_return(status: 200, body: valid_patron_response, headers: {})
+        .to_return(status: 200)
     end
 
-    it 'logs the user in', js: true do
-      visit "/catalog/SCSB-2143785"
-      click_link('Request')
-      expect(page.body).to include('Log in with Alma Account (affiliates)')
-      click_link('Log in with Alma Account (affiliates)')
-      fill_in(id: 'username', with: alma_user.username)
-      fill_in(id: 'password', with: alma_user.password)
-      click_button('Log in')
-      expect(WebMock).to have_requested(:post, expected_login_url)
-      expect(page.body).to include('Successfully authenticated with alma account. Please log out to protect your privacy when using a shared computer')
-      expect(page.body).to include('Library Material Request')
+    context 'with a CAS account' do
+      let(:user) { FactoryBot.create(:user) }
+      let(:patron_response) { File.open(fixture_path + '/bibdata_patron_response_barcode.json') }
+
+      it 'logs the user in', js: true do
+        visit "/catalog/SCSB-2143785"
+        click_link('Request')
+        expect(page).to have_link('Log in with netID')
+        click_link('Log in with netID')
+        expect(page.body).to include('Successfully authenticated from Princeton Central Authentication Service.')
+        expect(page.body).to include('Library Material Request')
+        expect(page.current_url).to include('/requests/SCSB-2143785?aeon=false')
+        expect(page).to have_selector('#request_3270290')
+      end
+
+      context 'without logging in but trying to hack the url' do
+        it 'does not display options to select' do
+          visit '/requests/SCSB-2143785?aeon=true'
+          expect(page.body).to include('Library Material Request')
+          expect(page).not_to have_selector('#request_3270290')
+        end
+      end
     end
+    context 'with an Alma account' do
+      let(:user) { FactoryBot.create(:valid_alma_patron) }
+      let(:patron_response) { File.open(fixture_path + '/alma_login_response.json') }
+      let(:expected_login_url) { 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/Alma%20Patron?op=auth&password=foobarfoo' }
 
-    context 'an aeon item' do
-      describe 'requesting a coin' do
-        before do
-          stub_request(:get, "#{Requests::Config[:pulsearch_base]}/catalog/coin-1167/raw")
-            .to_return(status: 200, body: fixture('/numismatics/coin-1167.json'), headers: {})
-          stub_request(:get, "#{Requests::Config[:bibdata_base]}/locations/holding_locations/rare$num.json")
-            .to_return(status: 200, body: fixture('/bibdata/numismatics_holding_locations.json'))
-        end
+      before do
+        stub_request(:post, expected_login_url)
+          .to_return(status: 204)
+      end
+      it 'logs the user in', js: true do
+        visit "/catalog/SCSB-2143785"
+        click_link('Request')
+        expect(page.body).to include('Log in with Alma Account (affiliates)')
+        click_link('Log in with Alma Account (affiliates)')
+        fill_in(id: 'username', with: user.username)
+        fill_in(id: 'password', with: user.password)
+        click_button('Log in')
+        expect(WebMock).to have_requested(:post, expected_login_url)
+        expect(page.body).to include('Successfully authenticated with alma account. Please log out to protect your privacy when using a shared computer')
+        expect(page.body).to include('Library Material Request')
+      end
 
-        it 'does not require authentication', js: true do
-          visit "/catalog/coin-1167"
-          expect(page).to have_link('Reading Room Request', href: '/requests/coin-1167?aeon=true&mfhd=numismatics')
-          click_link('Reading Room Request')
-          expect(page.current_url).to include(Requests::Config[:aeon_base])
+      context 'an aeon item' do
+        describe 'requesting a coin' do
+          before do
+            stub_request(:get, "#{Requests::Config[:pulsearch_base]}/catalog/coin-1167/raw")
+              .to_return(status: 200, body: fixture('/numismatics/coin-1167.json'), headers: {})
+            stub_request(:get, "#{Requests::Config[:bibdata_base]}/locations/holding_locations/rare$num.json")
+              .to_return(status: 200, body: fixture('/bibdata/numismatics_holding_locations.json'))
+          end
+
+          it 'does not require authentication', js: true do
+            visit "/catalog/coin-1167"
+            expect(page).to have_link('Reading Room Request', href: '/requests/coin-1167?aeon=true&mfhd=numismatics')
+            click_link('Reading Room Request')
+            expect(page.current_url).to include(Requests::Config[:aeon_base])
+          end
         end
-      end
-      describe 'requesting a thesis' do
-        before do
-          stub_request(:get, "#{Requests::Config[:pulsearch_base]}/catalog/dsp01tq57ns24j/raw")
-            .to_return(status: 200, body: fixture('/theses_and_dissertations/dsp01tq57ns24j.json'), headers: {})
-          stub_request(:get, "#{Requests::Config[:bibdata_base]}/locations/holding_locations/mudd$stacks.json")
-            .to_return(status: 200, body: fixture('/bibdata/mudd_stacks_holding_locations.json'))
+        describe 'requesting a thesis' do
+          before do
+            stub_request(:get, "#{Requests::Config[:pulsearch_base]}/catalog/dsp01tq57ns24j/raw")
+              .to_return(status: 200, body: fixture('/theses_and_dissertations/dsp01tq57ns24j.json'), headers: {})
+            stub_request(:get, "#{Requests::Config[:bibdata_base]}/locations/holding_locations/mudd$stacks.json")
+              .to_return(status: 200, body: fixture('/bibdata/mudd_stacks_holding_locations.json'))
+          end
+          it 'does not require authentication', js: true do
+            visit "/catalog/dsp01tq57ns24j"
+            expect(page).to have_link('Reading Room Request', href: '/requests/dsp01tq57ns24j?aeon=true&mfhd=thesis')
+            click_link('Reading Room Request')
+            expect(page.current_url).to include(Requests::Config[:aeon_base])
+          end
         end
-        it 'does not require authentication', js: true do
-          visit "/catalog/dsp01tq57ns24j"
-          expect(page).to have_link('Reading Room Request', href: '/requests/dsp01tq57ns24j?aeon=true&mfhd=thesis')
-          click_link('Reading Room Request')
-          expect(page.current_url).to include(Requests::Config[:aeon_base])
-        end
-      end
-      describe 'requesting a special collections holding with a single item' do
-        let(:availability) do
-          '[{"barcode":"32101070796881","id":"23745123320006421","holding_id":"22745123330006421","copy_number":"1","status":"Available","status_label":"Item in place","status_source":"base_status","process_type":null,"on_reserve":"N","item_type":"Closed","pickup_location_id":"rare","pickup_location_code":"rare","location":"rare$map","label":"Special Collections - Rare Books Historic Map Collection","description":"","enum_display":"","chron_display":"","in_temp_library":false}]'
-        end
-        before do
-          stub_request(:get, "#{Requests::Config[:pulsearch_base]}/catalog/99496133506421/raw")
-            .to_return(status: 200, body: fixture('/alma/99496133506421.json'), headers: {})
-          stub_request(:get, "#{Requests::Config[:bibdata_base]}/locations/holding_locations/rare$map.json")
-            .to_return(status: 200, body: fixture('/bibdata/rare_map_holding_locations.json'))
-          stub_request(:get, "#{Requests::Config[:bibdata_base]}/bibliographic/99496133506421/holdings/22745123330006421/availability.json")
-            .to_return(status: 200, body: availability)
-        end
-        it 'does not require authentication', js: true do
-          visit "/catalog/99496133506421"
-          expect(page).to have_link('Reading Room Request', href: '/requests/99496133506421?aeon=true&mfhd=22745123330006421')
-          click_link('Reading Room Request', href: '/requests/99496133506421?aeon=true&mfhd=22745123330006421')
-          expect(page.current_url).to include(Requests::Config[:aeon_base])
+        describe 'requesting a special collections holding with a single item' do
+          let(:availability) do
+            '[{"barcode":"32101070796881","id":"23745123320006421","holding_id":"22745123330006421","copy_number":"1","status":"Available","status_label":"Item in place","status_source":"base_status","process_type":null,"on_reserve":"N","item_type":"Closed","pickup_location_id":"rare","pickup_location_code":"rare","location":"rare$map","label":"Special Collections - Rare Books Historic Map Collection","description":"","enum_display":"","chron_display":"","in_temp_library":false}]'
+          end
+          before do
+            stub_request(:get, "#{Requests::Config[:pulsearch_base]}/catalog/99496133506421/raw")
+              .to_return(status: 200, body: fixture('/alma/99496133506421.json'), headers: {})
+            stub_request(:get, "#{Requests::Config[:bibdata_base]}/locations/holding_locations/rare$map.json")
+              .to_return(status: 200, body: fixture('/bibdata/rare_map_holding_locations.json'))
+            stub_request(:get, "#{Requests::Config[:bibdata_base]}/bibliographic/99496133506421/holdings/22745123330006421/availability.json")
+              .to_return(status: 200, body: availability)
+          end
+          it 'does not require authentication', js: true do
+            visit "/catalog/99496133506421"
+            expect(page).to have_link('Reading Room Request', href: '/requests/99496133506421?aeon=true&mfhd=22745123330006421')
+            click_link('Reading Room Request', href: '/requests/99496133506421?aeon=true&mfhd=22745123330006421')
+            expect(page.current_url).to include(Requests::Config[:aeon_base])
+          end
         end
       end
     end
