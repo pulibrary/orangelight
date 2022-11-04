9de4f0913d3fb3ea87a994f742c2a6b41cf25d23
Shaun Ellis <shaun@sdellis.com>
2022-11-04 12:55:05 -0400

    adds test for checking that null notice does not break

--- spec/javascript/orangelight/figgy_manifest_manager.spec.js
+++ spec/javascript/orangelight/figgy_manifest_manager.spec.js
@@ -47,4 +47,34 @@ describe('RelatedRecords', function() {
       expect(availableOnlineElement[0].getElementsByTagName('li')).toHaveLength(2);
       expect(viewerWrapperElement.getElementsByTagName('iframe')).toHaveLength(0);
     })
+
+    test('does not error when graphql notice is null', async() => {
+      const graphqlResponseNullNotice = {
+                                "resourcesByOrangelightId": [
+                                  {
+                                    "id": "9e42a4eb-42dc-43b6-b476-660d7e489c62",
+                                    "__typename": "ScannedResource",
+                                    "label": "Qadhafi's thesis",
+                                    "embed": {
+                                      "type": "",
+                                      "content": "",
+                                      "status": "unauthorized"
+                                    }
+                                  }]
+                              };
+
+      document.body.innerHTML =
+      '<div class="availability--online"><h3>Available Online</h3><ul><li></li></ul></div>' +
+      '<div id="view" class="document-viewers" data-bib-id="dsp01wd3760321"></div>';
+
+      const queryFunction2 = jest.fn((_bibIds) => { return graphqlResponseNullNotice })
+
+      const availableOnlineElement = document.getElementsByClassName('availability--online');
+      const viewerWrapperElement = document.getElementById('view');
+      const viewerSet = new FiggyViewerSet(viewerWrapperElement, queryFunction2, 'dsp01wd3760321', [], null)
+      await viewerSet.render()
+      expect(queryFunction2).toHaveBeenCalledTimes(1);
+      expect(availableOnlineElement[0].getElementsByTagName('li')).toHaveLength(1);
+      expect(viewerWrapperElement.getElementsByTagName('iframe')).toHaveLength(1);
+    })
 });
