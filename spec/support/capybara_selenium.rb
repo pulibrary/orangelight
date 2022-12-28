ee754944be5734ce56caee12167b097965c06dfe
Christina Chortaria <actspatial@gmail.com>
2022-12-28 09:10:47 -0500

    update capybara/selenium configuration

--- spec/support/capybara_selenium.rb
+++ spec/support/capybara_selenium.rb
@@ -4,31 +4,11 @@ require 'capybara/rails'
 require 'capybara/rspec'
 require "selenium-webdriver"
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
 
 Capybara.register_driver(:selenium) do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    'goog:chromeOptions': { args: %w[disable-gpu disable-setuid-sandbox window-size=7680,4320] }
-  )
-
-  browser_options = ::Selenium::WebDriver::Chrome::Options.new
-  browser_options.args << "--disable-gpu"
-
-  http_client = Selenium::WebDriver::Remote::Http::Default.new
-  http_client.read_timeout = 120
-  http_client.open_timeout = 120
-
-  Capybara::Selenium::Driver.new(app,
-                                 browser: :chrome,
-                                 capabilities:,
-                                 http_client:,
-                                 options: browser_options)
-end
-
-# This was needed for my local workstation, perhaps :selenium is overridden elsewhere?
-Capybara.register_driver(:selenium_headless) do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     'goog:chromeOptions': { args: %w[headless disable-gpu disable-setuid-sandbox window-size=7680,4320] }
   )
@@ -63,5 +43,5 @@ Capybara.register_driver :iphone do |app|
 end
 
 Capybara.server = :webrick
-Capybara.javascript_driver = :selenium_headless
+Capybara.javascript_driver = :selenium
 Capybara.default_max_wait_time = 15
