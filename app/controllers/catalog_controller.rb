5af940c303ed1c4f196b5d14a63f5610ae7b175f
Jane Sandberg <js7389@princeton.edu>
2023-01-03 14:01:22 -0800

    Explicitly allow params related to advanced search and numismatics

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -661,6 +661,11 @@ class CatalogController < ApplicationController
       'ris' => { content_type: 'application/x-research-info-systems' }
     }
     config.filter_search_state_fields = true
+    config.search_state_fields = config.search_state_fields + [
+      :advanced_type, :f1, :f2, :f3,
+      :op1, :op2, :op3,
+      :q1, :q2, :q3
+    ]
   end
 
   def render_search_results_as_json
