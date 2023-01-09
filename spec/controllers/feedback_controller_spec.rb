87b833ca53ee0f7190c5db17690967bdc5156075
Max Kadel <mkadel@princeton.edu>
2023-01-09 08:59:08 -0500

    Get test passing

--- spec/controllers/feedback_controller_spec.rb
+++ spec/controllers/feedback_controller_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 RSpec.describe FeedbackController, type: :controller do
   describe "#ask_a_question" do
     it "routes to the Ask A Question form" do
-      get :ask_a_question
+      get :ask_a_question, params: { id: '123' }
 
       expect(response).to be_successful
     end
