4e80a1ad7b0f73d99a43b992d8b15d0a398762d7
Max Kadel <mkadel@princeton.edu>
2023-02-08 12:33:10 -0500

    Rubocop

--- spec/services/physical_holdings_markup_builder_spec.rb
+++ spec/services/physical_holdings_markup_builder_spec.rb
@@ -195,7 +195,7 @@ RSpec.describe PhysicalHoldingsMarkupBuilder do
       expect(holding_location_markup).to include '<span class="location-text"'
       expect(holding_location_markup).to include 'Firestone Library'
       expect(holding_location_markup).to include 'data-holding-id="3668455"'
-      expect(holding_location_markup).to include "data-map-location=\"#{holding.first[1]["location_code"]}"
+      expect(holding_location_markup).to include "data-map-location=\"#{holding.first[1]['location_code']}"
     end
   end
 
@@ -727,7 +727,7 @@ RSpec.describe PhysicalHoldingsMarkupBuilder do
       expect(holding_location_markup).to include '<span class="location-text"'
       expect(holding_location_markup).to include 'Mendel Music Library: Reserve'
       expect(holding_location_markup).to include 'data-holding-id="22270490550006421"'
-      expect(holding_location_markup).to include "data-map-location=\"#{holding.first[1]["location_code"]}"
+      expect(holding_location_markup).to include "data-map-location=\"#{holding.first[1]['location_code']}"
     end
   end
 
@@ -786,7 +786,7 @@ RSpec.describe PhysicalHoldingsMarkupBuilder do
       expect(holding_location_markup).to include '<span class="location-text"'
       expect(holding_location_markup).to include 'Remote Storage (ReCAP): Manuscripts. Special Collections Use Only'
       expect(holding_location_markup).to include 'data-holding-id="22939015790006421"'
-      expect(holding_location_markup).to include "data-map-location=\"#{holding.first[1]["location_code"]}"
+      expect(holding_location_markup).to include "data-map-location=\"#{holding.first[1]['location_code']}"
     end
 
     it 'generates a "service-conditional" class' do
