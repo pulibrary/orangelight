faf0cada157b538b8d7ce4b16cd14db0ab979ad4
Jane Sandberg <js7389@princeton.edu>
2023-02-22 15:21:51 -0800

    Add some facet selects back into the advanced search form

--- spec/components/orangelight/facet_field_checkboxes_component_spec.rb
+++ spec/components/orangelight/facet_field_checkboxes_component_spec.rb
@@ -3,13 +3,32 @@
 require "rails_helper"
 
 RSpec.describe Orangelight::FacetFieldCheckboxesComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
-
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  let(:paginator) do
+    instance_double(Blacklight::FacetPaginator, items: [
+                      double(label: 'a', hits: 10, value: 'a'),
+                      double(label: 'b', hits: 33, value: 'b'),
+                      double(label: 'c', hits: 3, value: 'c')
+                    ])
+  end
+  let(:search_state) { Blacklight::SearchState.new({}.with_indifferent_access, Blacklight::Configuration.new) }
+  let(:facet_field) do
+    instance_double(
+      Blacklight::FacetFieldPresenter,
+      facet_field: Blacklight::Configuration::NullField.new(key: 'field', item_component: Blacklight::FacetItemComponent, item_presenter: Blacklight::FacetItemPresenter),
+      paginator:,
+      key: 'field',
+      label: 'Field',
+      active?: false,
+      collapsed?: false,
+      modal_path: nil,
+      search_state: search_state
+    )
+  end
+  it 'renders a visible facet label' do
+    expect(
+      render_inline(described_class.new(facet_field:)).to_s
+    ).to include(
+      "Field"
+    )
+  end
 end
