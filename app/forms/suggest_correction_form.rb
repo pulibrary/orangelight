3c3bb40171549d5b8dbcb107b5b4618e1c0c5504
Jane Sandberg <js7389@princeton.edu>
2023-02-27 11:48:01 -0800

    Add honeypot field to new forms
    * AskAQuestionForm, ReportHarmfulLanguageForm, and SuggestCorrectionForm.

--- app/forms/suggest_correction_form.rb
+++ app/forms/suggest_correction_form.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
-class SuggestCorrectionForm
+class SuggestCorrectionForm < MailForm::Base
   include ActiveModel::Model
   attr_accessor :name, :email, :message, :context, :title
 
   validates :name, :email, :message, :context, presence: true
   validates :email, email: true
+  attribute :feedback_desc, captcha: true
 
   def email_subject
     "[Catalog] #{title}"
   end
 
   def submit
-    ContactMailer.with(form: self).suggestion.deliver
+    ContactMailer.with(form: self).suggestion.deliver unless spam?
     @submitted = true
     @name = ""
     @email = ""
