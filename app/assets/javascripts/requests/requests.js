58b5e38c1a9defa322ee4dbcd56524f6e045b8a1
Christina Chortaria <actspatial@gmail.com>
2023-04-11 20:04:10 -0400

    Replace var with let Run eslint on app/assets/javascripts/requests/requests.js

--- app/assets/javascripts/requests/requests.js
+++ app/assets/javascripts/requests/requests.js
@@ -3,135 +3,135 @@
 $(document).ready(function() {
 
     function isEmail(email) {
-      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-      return regex.test(email);
+        const regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
     }
 
-    $( "form#logins" ).submit(function( event ) {
-      if ( !isEmail($( "#request_email" ).val()) ) {
-        event.preventDefault();
-        $( "#request_email" ).css("background-color","#f2dede");
-        $( "span.error-email").css("color", "red");
-        $( "span.error-email").text("Please supply a valid email address.");
-      } else {
-        $( "#request_email" ).css("background-color","#ffffff");
-        $( "span.error-email").text("");
-      }
-      if ($.trim($('#request_user_name').val()) == "")
-      {
-        event.preventDefault();
-        $( "#request_user_name" ).css("background-color","#f2dede");
-        $( "span.error-user_name").css("color", "red");
-        $( "span.error-user_name").text("Please supply your full name.");
-      } else {
-        $( "#request_user_name" ).css("background-color","#ffffff");
-        $( "span.error-user_name").text("");
-      }
+    $("form#logins").submit(function(event) {
+        if (!isEmail($("#request_email").val())) {
+            event.preventDefault();
+            $("#request_email").css("background-color","#f2dede");
+            $("span.error-email").css("color", "red");
+            $("span.error-email").text("Please supply a valid email address.");
+        } else {
+            $("#request_email").css("background-color","#ffffff");
+            $("span.error-email").text("");
+        }
+        if ($.trim($('#request_user_name').val()) == "")
+        {
+            event.preventDefault();
+            $("#request_user_name").css("background-color","#f2dede");
+            $("span.error-user_name").css("color", "red");
+            $("span.error-user_name").text("Please supply your full name.");
+        } else {
+            $("#request_user_name").css("background-color","#ffffff");
+            $("span.error-user_name").text("");
+        }
     });
 
-    $( "#no_netid").click(function( event ) {
-      event.preventDefault();
-      $( "#no_netid").hide();
-      $( "#other_user_account_info").show();
+    $("#no_netid").click(function(event) {
+        event.preventDefault();
+        $("#no_netid").hide();
+        $("#other_user_account_info").show();
     });
 
     $('#no_netid').keydown(function (e) {
-      var keyCode = e.keyCode || e.which;
+        const keyCode = e.keyCode || e.which;
 
-      if (keyCode == 13) {
-        $( "#no_netid" ).trigger( "click" );
-        return false;
-      }
+        if (keyCode == 13) {
+            $("#no_netid").trigger("click");
+            return false;
+        }
     });
 
-    $( "#go_back").click(function( event ) {
-      event.preventDefault();
-      $( "#no_netid").show();
-      $( "#other_user_account_info").hide();
+    $("#go_back").click(function(event) {
+        event.preventDefault();
+        $("#no_netid").show();
+        $("#other_user_account_info").hide();
     });
 
     // Enhance the Bootstrap collapse utility to toggle hide/show for other options
     $('input[type=radio][name^="requestable[][delivery_mode"]').on('change', function() {
         // collapse others
-        $("input[name='" + this.name + "']").each(function( index ) {
-          var target = $(this).attr('data-target');
-          $(target).collapse('hide');
+        $("input[name='" + this.name + "']").each(function(index) {
+            const target = $(this).attr('data-target');
+            $(target).collapse('hide');
         });
         // open target
-        var target = $(this).attr('data-target');
+        const target = $(this).attr('data-target');
         $(target).collapse('show');
 
         checkAllRequestable();
     });
 
     $('input[type=checkbox][id^="requestable_selected"').on('change', function() {
-      checkAllRequestable();
+        checkAllRequestable();
     });
 
     $('input[type=text][id^="requestable_user_supplied_enum_"').on('input', function() {
-      checkAllRequestable();
+        checkAllRequestable();
     });
 
     $('select[name^="requestable[][pick_up"]').on('change', function() {
-      checkAllRequestable();
+        checkAllRequestable();
     });
 
     function requestable(changed) {
-      var parent = $(changed).closest('[id^="request_"]');
-      var selected = parent.find('input[type=checkbox][id^="requestable_selected"').is(':checked');
-      var delivery_mode = false;
-      var radios = parent.find('input[type=radio][name^="requestable[][delivery_mode"]');
-      if (radios.length === 0) {
-        delivery_mode = true;
-      } else {
-        radios.each(function() {
-          if ($(this).is(':checked')) {
+        const parent = $(changed).closest('[id^="request_"]');
+        const selected = parent.find('input[type=checkbox][id^="requestable_selected"').is(':checked');
+        let delivery_mode = false;
+        const radios = parent.find('input[type=radio][name^="requestable[][delivery_mode"]');
+        if (radios.length === 0) {
             delivery_mode = true;
-          }
-        });
-      }
-      var volume_text = parent.find('input[type=text][id^="requestable_user_supplied_enum_"');
-      var user_supplied = true;
-      if (volume_text.length > 0 && volume_text.val().length === 0) {
-        user_supplied = false;
-      }
-      var requestable_pickups = parent.find('select[name^="requestable[][pick_up"] option');
-      var delivery_location = false;
-      if (requestable_pickups.length === 0) {
-        delivery_location = true;
-      } else {
-        requestable_pickups.each(function() {
-          if ($(this).is(':selected') && $(this).val() !== '') {
+        } else {
+            radios.each(function() {
+                if ($(this).is(':checked')) {
+                    delivery_mode = true;
+                }
+            });
+        }
+        const volume_text = parent.find('input[type=text][id^="requestable_user_supplied_enum_"');
+        let user_supplied = true;
+        if (volume_text.length > 0 && volume_text.val().length === 0) {
+            user_supplied = false;
+        }
+        const requestable_pickups = parent.find('select[name^="requestable[][pick_up"] option');
+        let delivery_location = false;
+        if (requestable_pickups.length === 0) {
             delivery_location = true;
-          }
-        });
-      }
-      if (selected && delivery_mode && user_supplied && delivery_location) {
-        $('#request-submit-button').prop('disabled', false);
-      }
-    };
+        } else {
+            requestable_pickups.each(function() {
+                if ($(this).is(':selected') && $(this).val() !== '') {
+                    delivery_location = true;
+                }
+            });
+        }
+        if (selected && delivery_mode && user_supplied && delivery_location) {
+            $('#request-submit-button').prop('disabled', false);
+        }
+    }
 
     function checkAllRequestable() {
-      $('#request-submit-button').prop('disabled', true);
-      $(".delivery--options").each(function() {
-        requestable(this);
-      });
-    };
+        $('#request-submit-button').prop('disabled', true);
+        $(".delivery--options").each(function() {
+            requestable(this);
+        });
+    }
 
     checkAllRequestable();
 
     jQuery(function() {
-      return $(".tablesorter").DataTable({
-        language: {
-          search: "Search by Enumeration"
-        },
-        ordering: false
-      });
+        return $(".tablesorter").DataTable({
+            language: {
+                search: "Search by Enumeration"
+            },
+            ordering: false
+        });
     });
 
     $('.table input[type=checkbox]').on('change', function() {
-      $(this).closest('tr').toggleClass('selected', $(this).is(':checked'));
-      requestable(this);
+        $(this).closest('tr').toggleClass('selected', $(this).is(':checked'));
+        requestable(this);
     });
   
 
