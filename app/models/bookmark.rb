a3b19ae3b7cfa762d426c14534a4bc1d533138da
Max Kadel <mkadel@princeton.edu>
2023-04-26 15:18:19 -0400

    Deletes bookmarks with missing Alma ids

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -12,4 +12,35 @@ class Bookmark < ApplicationRecord
   def document_type
     SolrDocument
   end
+
+  def self.destroy_without_solr_documents
+    Bookmark.find_in_batches do |bookmarks|
+      bookmark_doc_ids = bookmark_doc_ids(bookmarks)
+      doc_ids_without_solr_doc = bookmark_doc_ids - doc_ids_in_solr(bookmark_doc_ids)
+
+      if doc_ids_without_solr_doc.present?
+        doc_ids_without_solr_doc.each do |doc_id|
+          Bookmark.where(document_id: doc_id)&.destroy_all
+        end
+      end
+    end
+  end
+
+  def self.bookmark_doc_ids(bookmarks)
+    ids = bookmarks.map(&:document_id).to_set
+    ids.map { |id| ensure_voyager_to_alma_id(id) }.to_set
+  end
+
+  def self.doc_ids_in_solr(bookmark_doc_ids)
+    solr = Blacklight.default_index.connection
+    response = solr.get 'select', params: { fq: "{!terms f=id}#{bookmark_doc_ids.join(',')}", fl: 'id' }
+    response["response"]["docs"].map { |doc| doc["id"] }
+  end
+
+  # This method duplicates logic in the SolrDocument, but it did not seem well-suited for
+  # re-use directly here (needing to instantiate a SolrDocument for each ID before being able to call it)
+  def self.ensure_voyager_to_alma_id(id)
+    return id if id.length > 7 && id.start_with?("99")
+    "99#{id}3506421"
+  end
 end
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -7,4 +7,64 @@ RSpec.describe Bookmark do
     bookmark1 = Bookmark.new user_id: -1, document_id: 123
     expect { bookmark1.save }.to raise_error(ActiveRecord::InvalidForeignKey)
   end
+
+  describe 'removing bookmarks with deleted solr records' do
+    let(:in_solr_alma_id_one) { '994956003506421' }
+    let(:in_solr_alma_id_two) { '99118600973506421' }
+    let(:in_solr_voyager_id) { '8908514' }
+    let(:not_in_solr_alma_id) { '991234567806421' }
+    let(:not_in_solr_voyager_id) { '1234567' }
+    let!(:bookmark_one) { FactoryBot.create(:bookmark, document_id: document_ids[0]) }
+    let!(:bookmark_two) { FactoryBot.create(:bookmark, document_id: document_ids[1]) }
+
+    context 'with existing alma ids' do
+      let(:document_ids) { [in_solr_alma_id_one, in_solr_alma_id_two] }
+
+      it 'does not delete bookmarks with solr documents' do
+        expect do
+          Bookmark.destroy_without_solr_documents
+        end.not_to change { Bookmark.count }
+      end
+    end
+
+    context 'with existing and not existing alma ids' do
+      let(:document_ids) { [in_solr_alma_id_one, not_in_solr_alma_id] }
+
+      it 'deletes bookmarks without solr documents' do
+        expect do
+          Bookmark.destroy_without_solr_documents
+        end.to change { Bookmark.count }.by(-1)
+      end
+    end
+
+    context 'with more than one bookmark with the same alma id not in solr' do
+      let(:document_ids) { [not_in_solr_alma_id, not_in_solr_alma_id] }
+
+      it 'deletes bookmarks without solr documents' do
+        expect do
+          Bookmark.destroy_without_solr_documents
+        end.to change { Bookmark.count }.by(-2)
+      end
+    end
+
+    context 'with a voyager id that is in solr' do
+      let(:document_ids) { [in_solr_alma_id_one, in_solr_voyager_id] }
+
+      it 'does not delete the voyager bookmark' do
+        expect do
+          Bookmark.destroy_without_solr_documents
+        end.not_to change { Bookmark.count }
+      end
+    end
+
+    context 'with a voyager id that is not in solr' do
+      let(:document_ids) { [in_solr_voyager_id, not_in_solr_voyager_id] }
+
+      xit 'deletes the voyager bookmark that is not in solr' do
+        expect do
+          Bookmark.destroy_without_solr_documents
+        end.to change { Bookmark.count }.by(-1)
+      end
+    end
+  end
 end
