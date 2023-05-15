99055c3a28a68aa686f611b5f657b9858693e77b
Jane Sandberg <js7389@princeton.edu>
2023-05-15 15:15:12 -0700

    Remove unneeded param

--- app/components/request_button_component.rb
+++ app/components/request_button_component.rb
@@ -2,11 +2,10 @@
 
 # ViewComponent that displays a request button on the show page
 class RequestButtonComponent < ViewComponent::Base
-  def initialize(location:, doc_id:, holding_id: nil, force_aeon: false)
+  def initialize(location:, doc_id:, holding_id: nil)
     @location = location
     @doc_id = doc_id
     @holding_id = holding_id
-    @force_aeon = force_aeon
   end
 
   def label
@@ -27,6 +26,6 @@ class RequestButtonComponent < ViewComponent::Base
   private
 
     def aeon?
-      @aeon ||= @force_aeon || @location&.dig(:aeon_location)
+      @aeon ||= @location&.dig(:aeon_location)
     end
 end
--- spec/features/login_account_spec.rb
+++ spec/features/login_account_spec.rb
@@ -182,7 +182,7 @@ describe 'Account login' do
           end
           it 'does not require authentication', js: true do
             visit "/catalog/#{bib_id}"
-            expect(page).to have_link('Reading Room Request', href: "/requests/#{bib_id}?aeon=true&mfhd=22745123330006421")
+            expect(page).to have_link('Reading Room Request', href: Regexp.new('https://lib-aeon\.princeton\.edu/aeon/aeon\.dll/OpenURL.*dsp01tq57ns24j'))
             click_link('Reading Room Request', href: "/requests/#{bib_id}?aeon=true&mfhd=22745123330006421")
             expect(page.current_url).to include(Requests::Config[:aeon_base])
           end
