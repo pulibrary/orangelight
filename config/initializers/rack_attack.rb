df30e79ff8878f4994279a8151fe6e2c286ddc12
Ryan Laddusaw <rladdusaw@princeton.edu>
2023-05-31 09:10:49 -0500

    Limiting all sitemap requests

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,6 +2,6 @@
 
 class Rack::Attack
   Rack::Attack.throttle('limit sitemap requests by IP', limit: 1, period: 10) do |request|
-    { 'ip' => request.ip, 'path' => request.path } if /sitemap/.match(request.path) && request.get?
+    request.ip if /sitemap/.match(request.path) && request.get?
   end
 end
