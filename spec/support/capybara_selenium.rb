7f0dc0b4b14c277e494b4697e83cddd67eb6f04c
Jane Sandberg <js7389@princeton.edu>
2023-07-28 11:13:26 -0700

    Upgrade selenium-webdrivers gem, remove webdrivers gem

--- spec/support/capybara_selenium.rb
+++ spec/support/capybara_selenium.rb
@@ -5,13 +5,10 @@ require 'capybara/rspec'
 require "selenium-webdriver"
 
 Capybara.register_driver(:selenium) do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    'goog:chromeOptions': { args: %w[headless disable-gpu disable-setuid-sandbox window-size=7680,4320] }
-  )
-
   browser_options = ::Selenium::WebDriver::Chrome::Options.new
   browser_options.args << "--headless"
   browser_options.args << "--disable-gpu"
+  browser_options.args << "--disable-setuid-sandbox"
   browser_options.args << "--window-size=1920,1200"
 
   http_client = Selenium::WebDriver::Remote::Http::Default.new
@@ -20,7 +17,6 @@ Capybara.register_driver(:selenium) do |app|
 
   Capybara::Selenium::Driver.new(app,
                                  browser: :chrome,
-                                 capabilities:,
                                  http_client:,
                                  options: browser_options)
 end
