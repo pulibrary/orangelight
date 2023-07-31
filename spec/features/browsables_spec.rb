1d17b1d1b925dd719d67b758626dff1985c05b92
Max Kadel <mk8066@princeton.edu>
2023-07-31 16:56:26 -0400

    I3192 oversize cd browse (#3654)
    * Normalize oversize CDs correctly
    
    - Fix small typo
    - Update seed call numbers based on latest code
    
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    
    * Only look for up to seven digits
    
    ---------
    
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- spec/features/browsables_spec.rb
+++ spec/features/browsables_spec.rb
@@ -4,17 +4,33 @@ require 'rails_helper'
 
 describe 'Browsables' do
   describe 'Browse by Call Number' do
-    before do
-      visit '/browse/call_numbers?q=PN842+.S539+2006&rpp=10'
+    context 'with an LC call number' do
+      before do
+        visit '/browse/call_numbers?q=PN842+.S539+2006&rpp=10'
+      end
+      it 'displays two browse entries before exact match' do
+        expect(page.all('tr')[3][:class]).to eq('alert alert-info clickable-row')
+      end
+      it 'has a call number link' do
+        expect(page.all('tr')[3]).to have_xpath("//a", text: "PN842 .S539 2006")
+      end
+      it 'has the library name if it exists' do
+        expect(page).to have_xpath('//*[@id="content"]/table/tbody/tr[3]//td[2]', text: 'Firestone Library - Near East Collections')
+      end
     end
-    it 'displays two browse entries before exact match' do
-      expect(page.all('tr')[3][:class]).to eq('alert alert-info clickable-row')
-    end
-    it 'has a call number link' do
-      expect(page.all('tr')[3]).to have_xpath("//a", text: "PN842 .S539 2006")
-    end
-    it 'has the library name if it exists' do
-      expect(page).to have_xpath('//*[@id="content"]/table/tbody/tr[3]//td[2]', text: 'Firestone Library - Near East Collections')
+    context 'with oversize CDs' do
+      before do
+        visit '/browse/call_numbers?search_field=browse_cn&q=CD-+40056q'
+      end
+
+      it 'sorts Oversize CDs with other CDs' do
+        within(:xpath, '//*[@id="content"]/table/tbody') do
+          expect(page.find('tr[2]/td[1]/a').text).to eq("CD- 40055")
+          expect(page.find('tr[3]')).to match_css('.alert.alert-info.clickable-row')
+          expect(page.find('tr[3]//td[1]').text).to eq("CD- 40056q Oversize")
+          expect(page.find('tr[4]/td[1]/a').text).to eq("CD- 40057")
+        end
+      end
     end
   end
 
