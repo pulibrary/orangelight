ac9b654f6df19a90fe9b2a1d94b5d5b708173ea2
Max Kadel <mkadel@princeton.edu>
2023-08-16 17:20:03 -0400

    Address remaining deprecation warnings
    - Already on the latest 7.X release of Blacklight
    
    Closes #3678

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -39,7 +39,7 @@ class BookmarksController < CatalogController
   def print
     fetch_bookmarked_documents
     @url_gen_params = {}
-    render 'record_mailer/email_record.text.erb', template: false, content_type: 'text/plain'
+    render('record_mailer/email_record', formats: [:text])
   end
 
   def csv
