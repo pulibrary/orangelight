c4b7aecc74741d281b6352c3aac958eced5022c2
Max Kadel <mkadel@princeton.edu>
2023-08-21 17:08:09 -0400

    WIP
    - Search form is searching correct fields
    - Needs more testing
    - Booleans are not actually hooked up correctly - not sure if this is possible within json syntax
    - Locally there are some failing tests when I turn the feature on for everything, mostly in browse specs - don't know if this is related to local solr config change

--- spec/components/orangelight/advanced_search_form_component_spec.rb
+++ spec/components/orangelight/advanced_search_form_component_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe Orangelight::AdvancedSearchFormComponent, type: :component do
+RSpec.describe Orangelight::AdvancedSearchFormComponent, type: :component, advanced_search: true do
   subject(:render) do
     component.render_in(view_context)
   end
@@ -42,5 +42,12 @@ RSpec.describe Orangelight::AdvancedSearchFormComponent, type: :component do
     ]
     options = rendered.all('select')[1].all('option').map(&:text)
     expect(options).to eq(expected_options)
+  end
+
+  it 'has text fields for each search field' do
+    expect(rendered).to have_selector '.advanced-search-field', count: 1
+    expect(rendered).to have_field 'clause_0_field', with: 'all_fields'
+    expect(rendered).to have_field 'clause_1_field', with: 'author'
+    expect(rendered).to have_field 'clause_2_field', with: 'title'
   end
 end
