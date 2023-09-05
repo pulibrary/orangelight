b638fc0bd30da9cf422bb212106e69a9d12eff44
Max Kadel <mkadel@princeton.edu>
2023-09-05 11:36:57 -0400

    Remove advanced search controller
    - Per @kevinreiss, this behavior is no longer needed, either with or without the BlacklightAdvancedSearch gem

--- spec/requests/advanced_search_spec.rb
+++ spec/requests/advanced_search_spec.rb
@@ -2,16 +2,11 @@
 
 require 'rails_helper'
 
-describe 'Orangelight advanced search', type: :request do
+describe 'Orangelight advanced search', type: :request, advanced_search: true do
   before do
     stub_holding_locations
   end
 
-  it 'redirects search requests to the catalog search' do
-    get '/advanced?f[subject_facet][]=United+Nations-Decision+making&id=3681146&page=1&per_page=20'
-    expect(response.status).to eq(302)
-    expect(response).to redirect_to('/')
-  end
   it 'renders the advanced search form' do
     get '/advanced?f[subject_facet][]=United+Nations-Decision+making'
     expect(response.status).to eq(200)
