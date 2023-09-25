8524e80ac59b4e3894d3432c8c74e88946a89dbf
Jane Sandberg <js7389@princeton.edu>
2023-09-25 09:38:09 -0700

    Fix bug identified in review

--- app/helpers/advanced_helper.rb
+++ app/helpers/advanced_helper.rb
@@ -13,6 +13,8 @@ module AdvancedHelper
       params[key]
     elsif params['search_field'] == key || guided_context(key)
       params['q']
+    else
+      param_for_field key
     end
   end
 
@@ -26,8 +28,8 @@ module AdvancedHelper
 
   # carries over original search field and original guided search fields if user switches to guided search from regular search
   def guided_field(field_num, default_val)
-    return advanced_search_fields[params[:search_field]].key || default_val if field_num == :f1 && params[:f1].nil? && params[:f2].nil? && params[:f3].nil? && params[:search_field] && advanced_search_fields[params[:search_field]]
-    params[field_num] || default_val
+    return advanced_search_fields[params[:search_field]].key || default_val if first_search_field_selector?(field_num) && no_advanced_search_fields_specified? && params[:search_field] && advanced_search_fields[params[:search_field]]
+    params[field_num] || param_for_field(field_num) || default_val
   end
 
   # carries over original search query if user switches to guided search from regular search
@@ -96,6 +98,23 @@ module AdvancedHelper
 
     def advanced_search_fields
       blacklight_config.search_fields.select { |_k, v| v.include_in_advanced_search || v.include_in_advanced_search.nil? }
+    end
+
+    def first_search_field_selector?(key)
+      [:f1, :clause_0_field].include? key
+    end
+
+    def no_advanced_search_fields_specified?
+      [:f1, :f2, :f3, :clause_0_field, :clause_1_field, :clause_2_field].none? do |key|
+        params[key].present?
+      end
+    end
+
+    def param_for_field(field_identifier)
+      if field_identifier.to_s.start_with? 'clause'
+        components = field_identifier.to_s.split('_')
+        params.dig(*components)
+      end
     end
 
     def first_search_field?(key)
--- spec/helpers/advanced_helper_spec.rb
+++ spec/helpers/advanced_helper_spec.rb
@@ -36,6 +36,15 @@ RSpec.describe AdvancedHelper do
     end
   end
 
+  describe '#guided_field' do
+    context 'when field_num is :clause_0_field' do
+      it 'can get the field name from params clause[0][field]' do
+        params[:clause] = { '0' => { 'field' => 'title' } }
+        expect(guided_field(:clause_0_field, 'all_fields')).to eq('title')
+      end
+    end
+  end
+
   describe '#label_tag_default_for' do
     context 'when key is :q1' do
       it 'takes search term from q param' do
@@ -57,6 +66,14 @@ RSpec.describe AdvancedHelper do
         end
         allow_any_instance_of(described_class).to receive(:blacklight_config).and_return(blacklight_config)
         expect(label_tag_default_for(:clause_0_query)).to eq('cats')
+      end
+      it 'takes search term from q param' do
+        params[:clause] = { '0' => { 'field' => 'all_fields', 'query' => 'beasts' } }
+        blacklight_config = Blacklight::Configuration.new do |config|
+          config.add_search_field 'all_fields'
+        end
+        allow_any_instance_of(described_class).to receive(:blacklight_config).and_return(blacklight_config)
+        expect(label_tag_default_for(:clause_0_query)).to eq('beasts')
       end
     end
   end
