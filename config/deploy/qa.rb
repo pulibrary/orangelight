ac8916b4f4695211db72e976da1320e455b3d191
Liz Garcia <lizgarova16@gmail.com>
2023-10-16 15:58:39 -0400

    Remove cron_prod1 from staging and qa
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -6,7 +6,7 @@ set :branch, ENV['BRANCH'] || 'main'
 
 # server 'catalog-qa1.princeton.edu', user: 'deploy', roles: %i[web app db worker mailcatcher cron_db]
 server 'catalog-qa2.princeton.edu', user: 'deploy', roles: %i[web app db worker mailcatcher cron_db]
-server 'catalog-indexer-qa2', user: 'deploy', roles: %i[cron_prod1 cron_db worker]
+server 'catalog-indexer-qa2', user: 'deploy', roles: %i[cron_db worker]
 
 namespace :env do
   desc 'Set an Orangelight environment variable'
