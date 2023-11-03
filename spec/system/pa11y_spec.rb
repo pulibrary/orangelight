532740f097b830761e89db1fb5b7adf4b49e4a54
Christina Chortaria <actspatial@gmail.com>
2023-11-03 12:39:03 -0400

    Add random port number. Run rails server and detach for yarn pa11y
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- spec/system/pa11y_spec.rb
+++ spec/system/pa11y_spec.rb
@@ -2,12 +2,23 @@
 require 'rails_helper'
 
 RSpec.describe 'pa11y' do
+  let(:bibs) { ["994264203506421", "994916543506421", "9928379683506421", "9961023373506421", "9976174773506421"] }
+  before do
+    $port = rand(9000..9999)
+    stub_alma_holding_locations
+    $rails_server = spawn("bundle exec rails s -p #{$port}")
+    Process.detach($rails_server)
+  end
+
+  after do
+    Process.kill('TERM', $rails_server)
+    `rm tmp/pids/server.pid`
+  end
+
   it 'passes pa11y' do
-    # Capybara.app_host = "http://localhost:3000"
-    # stub_alma_holding_locations
-    visit '/catalog/99122643653506421'
-    results = `yarn pa11y https://catalog-staging.princeton.edu/catalog/99122643653506421`
-    # byebug
-    expect(results).to include('No issues found!')
+    bibs.each do |bib|
+      results = `yarn pa11y http://localhost:#{$port}/catalog/#{bib}`
+      expect(results).to include('No issues found!'), bib
+    end
   end
 end
