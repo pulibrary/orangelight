102ef38668a7470d46666c11a733e6fe468933ff
leefaisonr <rl3667@princeton.edu>
2023-11-16 11:59:53 -0500

    Adjust routes for form submission
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Shaun Ellis <sdellis@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -104,8 +104,9 @@ Rails.application.routes.draw do
   post "/contact/report_harmful_language", to: "contact#report_harmful_language"
 
   # For "Reporting Biased Search Results" form
+  # TODO: change get route to '/report_biased_results'
   get '/feedback/biased_results', to: 'feedback#report_biased_results', as: 'feedback_biased_results'
-  post '/feedback/biased_results', to: 'feedback#report_biased_results_submit', as: 'feedback_biased_results_submit'
+  post '/contact/report_biased_results', to: 'contact#report_biased_results', as: 'feedback_biased_results_submit'
 
   get '/thumbnail/:id', to: 'thumbnail#show'
 
--- spec/features/feedback_biased_results_spec.rb
+++ spec/features/feedback_biased_results_spec.rb
@@ -5,10 +5,13 @@ require 'rails_helper'
 describe 'submitting biased results', js: true do
   before do
     visit '/feedback/biased_results?report_biased_results_form[q]=cats'
+  end
+
+  it 'submits the message' do
     fill_in(id: 'name', with: 'John Smith')
     fill_in(id: 'email', with: 'jsmith@localhost.localdomain')
     fill_in(id: 'message', with: 'Lorem ipsum dolor sit amet, consectetur...')
-    # click_on('Send')
+    click_on('Send')
   end
 
   # it 'renders an accessible icon for returning' do
