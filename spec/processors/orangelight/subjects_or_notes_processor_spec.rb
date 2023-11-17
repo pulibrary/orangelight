b073a3982a3b008a7440837f8d9632f07f72ff3f
rladdusaw <rladdusaw@gmail.com>
2023-11-17 15:29:42 -0600

    Added more tests to cover all cases (#3844)
    Co-authored-by: Ryan Laddusaw <rladdusaw@princeton.edu>

--- spec/processors/orangelight/subjects_or_notes_processor_spec.rb
+++ spec/processors/orangelight/subjects_or_notes_processor_spec.rb
@@ -2,9 +2,6 @@
 require 'rails_helper'
 
 RSpec.describe Orangelight::SubjectsOrNotesProcessor do
-  before do
-    allow(Flipflop).to receive(:highlighting?).and_return(true)
-  end
   let(:document) { SolrDocument.new }
   let(:options) do
     { context: 'show' }
@@ -14,22 +11,75 @@ RSpec.describe Orangelight::SubjectsOrNotesProcessor do
   let(:rendered) { processor.render }
   let(:request_context) { double('View context') }
 
-  context "when notes_display and subject_display are present" do
+  context "when highlights are turned off" do
     before do
-      allow(document).to receive(:highlight_field).with('lc_subject_display').and_return(values)
-      allow(request_context).to receive(:action_name).and_return('index')
+      allow(Flipflop).to receive(:highlighting?).and_return(false)
     end
-    let(:values) { ['The lives of <em>Black</em> and Latino <em>teenagers</em> in a low-income :'] }
-    context "notes_display field" do
-      let(:config) { Blacklight::Configuration::Field.new(field: 'notes_display') }
-      it "will not render" do
-        expect(rendered).to be_empty
+
+    context "when notes_display and subject_display are present" do
+      before do
+        allow(document).to receive(:highlight_field).with('lc_subject_display').and_return(values)
+        allow(request_context).to receive(:action_name).and_return('index')
+      end
+      let(:values) { ['The lives of <em>Black</em> and Latino <em>teenagers</em> in a low-income :'] }
+      context "notes_display field" do
+        let(:config) { Blacklight::Configuration::Field.new(field: 'notes_display') }
+        it "will not render" do
+          expect(rendered).to be_empty
+        end
+      end
+      context "lc_subject_display field" do
+        let(:config) { Blacklight::Configuration::Field.new(field: 'lc_subject_display') }
+        it "will not render" do
+          expect(rendered).to be_empty
+        end
       end
     end
-    context "lc_subject_display field" do
-      let(:config) { Blacklight::Configuration::Field.new(field: 'lc_subject_display') }
-      it "will render" do
-        expect(rendered).not_to be_empty
+  end
+
+  context "when highlights are turned on" do
+    before do
+      allow(Flipflop).to receive(:highlighting?).and_return(true)
+    end
+
+    context "when notes_display and subject_display are present" do
+      before do
+        allow(document).to receive(:highlight_field).with('lc_subject_display').and_return(values)
+        allow(request_context).to receive(:action_name).and_return('index')
+      end
+      let(:values) { ['The lives of <em>Black</em> and Latino <em>teenagers</em> in a low-income :'] }
+      context "notes_display field" do
+        let(:config) { Blacklight::Configuration::Field.new(field: 'notes_display') }
+        it "will not render" do
+          expect(rendered).to be_empty
+        end
+      end
+      context "lc_subject_display field" do
+        let(:config) { Blacklight::Configuration::Field.new(field: 'lc_subject_display') }
+        it "will render" do
+          expect(rendered).not_to be_empty
+        end
+      end
+    end
+
+    context "when notes_display is present and subject_display is not" do
+      before do
+        allow(document).to receive(:highlight_field).with('lc_subject_display').and_return([])
+        allow(document).to receive(:highlight_field).with('notes_display').and_return(values)
+        allow(request_context).to receive(:action_name).and_return('index')
+      end
+      let(:values) { ['The lives of <em>Black</em> and Latino <em>teenagers</em> in a low-income :'] }
+      context "notes_display field" do
+        let(:config) { Blacklight::Configuration::Field.new(field: 'notes_display') }
+        it "will render" do
+          expect(rendered).not_to be_empty
+        end
+      end
+      context "lc_subject_display field" do
+        let(:config) { Blacklight::Configuration::Field.new(field: 'lc_subject_display') }
+        it "will not render" do
+          expect(rendered).to be_empty
+        end
       end
     end
   end
