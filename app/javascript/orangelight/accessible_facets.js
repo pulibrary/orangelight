29864e9d0326b33e9d26af8c38a2aeae8710d268
Christina Chortaria <actspatial@gmail.com>
2023-11-28 17:45:10 -0500

    Update to const variables that are never reassigned

--- app/javascript/orangelight/accessible_facets.js
+++ app/javascript/orangelight/accessible_facets.js
@@ -8,35 +8,42 @@
 // };
 
 const handleBtnKeyDown = () => {
-  const cardFacets = document.querySelectorAll('#facet-panel-collapse div.card-header.collapse-toggle.facet-field-heading > a')
-  cardFacets.forEach(function(card) {
+  const cardFacets = document.querySelectorAll(
+    '#facet-panel-collapse div.card-header.collapse-toggle.facet-field-heading > a'
+  )
+  cardFacets.forEach(function (card) {
     card.addEventListener('keydown', eventKeyCheck)
   })
-};
- const eventKeyCheck = (event) => {
+}
+const eventKeyCheck = (event) => {
   // "Spacebar" for IE11 support
   /*  https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code
       https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code/code_values
       KeyboardEvent: key=' ' | code='Space'
       KeyboardEvent: key='Enter' | code='Enter'  */
-    if (event.key === " " || event.key === "Space" || event.key === "Enter" || event.key === "Spacebar") {
-      // if Space or Enter is keydown then do what click does through toggleAriaExpanded function;
-      event.preventDefault();
-      event.stopPropagation();
-      toggleAriaExpanded(event.target);
-    }
-  };
-  
-  // toggleAriaExpanded updates the "aria-expanded" value
-  // The "aria-expanded" value initially is set in app/views/catalog/_facet_layout.html.erb
-  const toggleAriaExpanded = (element) => {
-    let expanded = element.getAttribute("aria-expanded")
-    element.click();
-    if (expanded == "true") {
-      return element.setAttribute("aria-expanded", "false")
-    } else{
-      return element.setAttribute("aria-expanded", "true")
-    }
-  };
+  if (
+    event.key === ' ' ||
+    event.key === 'Space' ||
+    event.key === 'Enter' ||
+    event.key === 'Spacebar'
+  ) {
+    // if Space or Enter is keydown then do what click does through toggleAriaExpanded function;
+    event.preventDefault()
+    event.stopPropagation()
+    toggleAriaExpanded(event.target)
+  }
+}
 
-export { handleBtnKeyDown };
+// toggleAriaExpanded updates the "aria-expanded" value
+// The "aria-expanded" value initially is set in app/views/catalog/_facet_layout.html.erb
+const toggleAriaExpanded = (element) => {
+  const expanded = element.getAttribute('aria-expanded')
+  element.click()
+  if (expanded == 'true') {
+    return element.setAttribute('aria-expanded', 'false')
+  } else {
+    return element.setAttribute('aria-expanded', 'true')
+  }
+}
+
+export { handleBtnKeyDown }
