df916d25acddd6f2fccb11d120c08db08c676d87
Christina Chortaria <actspatial@gmail.com>
2023-12-04 22:45:41 -0500

    Put back hidden_field_tag. Fixes #3874 (#3876)

--- spec/system/numismatics_form_spec.rb
+++ spec/system/numismatics_form_spec.rb
@@ -2,12 +2,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Numismatics search form' do
+  before do
+    stub_alma_holding_locations
+  end
   context 'when using the view components numismatics form' do
     before { allow(Flipflop).to_receive(:view_components_numismatics?).and_return(true) }
   end
   it 'does not display the basic search form' do
     visit '/numismatics'
     expect(page).not_to have_selector('.search-query-form')
+  end
+  it 'can run a search' do
+    visit '/numismatics'
+    select('shekel', from: 'f_inclusive[issue_denomination_s][]')
+    click_button('advanced-search-submit')
+    expect(page.find(".page_entries").text).to eq('1 entry found')
+    expect(page).to have_content('Coin: 1167')
   end
 
   context 'when using the original numismatics form' do
@@ -16,5 +26,12 @@ RSpec.describe 'Numismatics search form' do
   it 'does not display the basic search form' do
     visit '/numismatics'
     expect(page).not_to have_selector('.search-query-form')
+  end
+  it 'can run a search' do
+    visit '/numismatics'
+    select('shekel', from: 'f_inclusive[issue_denomination_s][]')
+    click_button('advanced-search-submit')
+    expect(page.find(".page_entries").text).to eq('1 entry found')
+    expect(page).to have_content('Coin: 1167')
   end
 end
