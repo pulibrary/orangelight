63ef382d439726aeedb9f4952de1a08fba73b1d8
Jane Sandberg <js7389@princeton.edu>
2024-01-18 15:05:56 -0800

    Refactor multiselect combobox into a view component

---
+++ spec/components/location_code_facet_component_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe LocationCodeFacetComponent, type: :component do
+  before { stub_holding_locations }
+
+  describe '#location_codes_by_lib' do
+    let(:location_items) { [architecture, rcppw, ues, uesla, uesrf] }
+    let(:architecture) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'Architecture Library') }
+    let(:rcppw) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$pw') }
+    let(:ues) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$circ') }
+    let(:uesla) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$la') }
+    let(:uesrf) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$ref') }
+
+    subject(:codes) do
+      component = described_class.new(display_facet: nil, label: 'label', blacklight_config: nil, search_state: nil)
+      component.location_codes_by_lib(location_items)
+    end
+
+    it 'includes library name as key' do
+      expect(codes.key?('Architecture Library')).to be true
+    end
+    describe 'architecture library' do
+      let(:architecture_hash) { subject['Architecture Library'] }
+
+      it 'includes holding location code facet items' do
+        # In Voyager the `architecture_hash['codes']` did not include the ReCAP library (rcppw)
+        # since ReCAP items were assigned to their own library (ReCAP). That is not the case
+        # in Alma and now the ReCAP library (rcppw) is included.
+        expect(architecture_hash['codes']).to match_array([ues, uesla, uesrf, rcppw])
+      end
+      it 'includes recap location code facet items' do
+        expect(architecture_hash['recap_codes']).to include(rcppw)
+      end
+      it 'includes library facet item' do
+        expect(architecture_hash['item']).to eq(architecture)
+      end
+    end
+  end
+end
