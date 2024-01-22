3e4e43f7ca310546a397e292ea2733a4b7f52835
Jane Sandberg <js7389@princeton.edu>
2024-01-22 08:44:19 -0800

    Incorporate feedback from review

--- spec/components/location_code_facet_component_spec.rb
+++ spec/components/location_code_facet_component_spec.rb
@@ -3,15 +3,19 @@
 require "rails_helper"
 
 RSpec.describe LocationCodeFacetComponent, type: :component do
-  before { stub_holding_locations }
+  before { stub_alma_holding_locations }
 
   describe '#location_codes_by_lib' do
-    let(:location_items) { [architecture, rcppw, ues, uesla, uesrf] }
+    let(:location_items) { [architecture, arch_la, recap_arch_pw, arch_newbook, arch_ref, arch_res3hr, arch_resclosed, arch_stacks, arch_unassigned] }
     let(:architecture) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'Architecture Library') }
-    let(:rcppw) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$pw') }
-    let(:ues) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$circ') }
-    let(:uesla) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$la') }
-    let(:uesrf) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$ref') }
+    let(:arch_la) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$la') }
+    let(:arch_newbook) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$newbook') }
+    let(:recap_arch_pw) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$pw') }
+    let(:arch_ref) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$ref') }
+    let(:arch_res3hr) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$res3hr') }
+    let(:arch_resclosed) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$resclosed') }
+    let(:arch_stacks) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$stacks') }
+    let(:arch_unassigned) { Blacklight::Solr::Response::Facets::FacetItem.new(hits: '792', value: 'arch$UNASSIGNED') }
 
     subject(:codes) do
       component = described_class.new(display_facet: nil, label: 'label', blacklight_config: nil, search_state: nil)
@@ -25,13 +29,10 @@ RSpec.describe LocationCodeFacetComponent, type: :component do
       let(:architecture_hash) { subject['Architecture Library'] }
 
       it 'includes holding location code facet items' do
-        # In Voyager the `architecture_hash['codes']` did not include the ReCAP library (rcppw)
-        # since ReCAP items were assigned to their own library (ReCAP). That is not the case
-        # in Alma and now the ReCAP library (rcppw) is included.
-        expect(architecture_hash['codes']).to match_array([ues, uesla, uesrf, rcppw])
+        expect(architecture_hash['codes']).to match_array([recap_arch_pw, arch_la, arch_newbook, arch_ref, arch_res3hr, arch_resclosed, arch_stacks, arch_unassigned])
       end
       it 'includes recap location code facet items' do
-        expect(architecture_hash['recap_codes']).to include(rcppw)
+        expect(architecture_hash['recap_codes']).to include(recap_arch_pw)
       end
       it 'includes library facet item' do
         expect(architecture_hash['item']).to eq(architecture)
