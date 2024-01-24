18fd19cdcdec81515f536c3f536300de0ef9f4bd
Christina Chortaria <actspatial@gmail.com>
2024-01-24 15:12:16 -0500

    Revert "Fix CAS Login for local development" (#3949)
    * Revert "Fix CAS Login for local development"
    
    * Remove cookie sameSite strict
    
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Kevin Reiss <kevinreiss@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Kevin Reiss <kevinreiss@users.noreply.github.com>

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -3,9 +3,3 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.action_dispatch.cookies_serializer = :json
-
-# Strict Same Site Protection protects users from CSRF attacks from non-Princeton
-# domains.  However, when running orangelight on localhost, the CAS login page is
-# on a different domain from orangelight (localhost vs. *.princeton.edu), so
-# we exclude the dev environment from these protections so they can use CAS locally.
-Rails.application.config.action_dispatch.cookies_same_site_protection = :strict unless Rails.env.development?
--- spec/requests/cookies_spec.rb
+++ spec/requests/cookies_spec.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 require 'rails_helper'
 RSpec.describe 'Cookies' do
-  it 'sets SameSite=strict' do
-    get '/'
-    expect(response.headers['Set-Cookie']).to include('SameSite=Strict')
-  end
   it 'sets HttpOnly' do
     get '/'
     expect(response.headers['Set-Cookie']).to include('HttpOnly')
