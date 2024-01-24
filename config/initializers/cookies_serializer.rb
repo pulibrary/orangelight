a6c94e42701778bd663b145463414f04df4a3491
Christina Chortaria <actspatial@gmail.com>
2024-01-24 14:57:23 -0500

    Remove cookie sameSite strict
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Kevin Reiss <kevinreiss@users.noreply.github.com>

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -3,4 +3,3 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.action_dispatch.cookies_serializer = :json
-Rails.application.config.action_dispatch.cookies_same_site_protection = :strict
