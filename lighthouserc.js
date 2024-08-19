34ee72111f496a2a90808146d895ec056b290d25
Jane Sandberg <js7389@princeton.edu>
2024-08-19 12:51:33 -0700

    [#4123] Avoid layout shift and quickly changing colors in the header
    This commit moves the .pul_header container out of the Vue component
    (which is rendered after the rest of the page), and into erb and
    the application CSS, which will block rendering.  It also gives the
    container an explicit height.
    
    This prevents the situation where the page renders with light colored
    content at first, then changes to very dark gray and shifts the layout
    once the Vue component loads, which looks like a distracting flash.
    
    Also, add a threshold for layout shift to our lighthouse check, to
    hopefully guard against regressions.

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,12 +2,14 @@ module.exports = {
   ci: {
     assert: {
       assertions: {
+        'cumulative-layout-shift': ['error', { maxNumericValue: 0.73 }],
         'largest-contentful-paint': ['error', { maxNumericValue: 19000 }],
         'errors-in-console': ['error', { maxLength: 10 }],
       },
     },
     collect: {
       url: [
+        'http://localhost:2999', // The catalog home page
         'http://localhost:2999/catalog/99122304923506421', // A show page
       ],
       startServerCommand: 'bundle exec rails server -p 2999',
@@ -16,4 +18,4 @@ module.exports = {
       target: 'temporary-public-storage',
     },
   },
-}
+};
