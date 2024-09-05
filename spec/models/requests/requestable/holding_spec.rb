bc1d194526d0ede10f690e281a938a24da361e3a
Jane Sandberg <js7389@princeton.edu>
2024-09-05 10:01:30 -0700

    Add the requests tag to more tests

--- spec/models/requests/requestable/holding_spec.rb
+++ spec/models/requests/requestable/holding_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe Requests::Requestable::Holding do
+RSpec.describe Requests::Requestable::Holding, requests: true do
   describe '#thesis?' do
     it 'returns false for a special collections book' do
       holding = described_class.new(JSON.parse('{"22654362360006421":{"location_code":"rare$ga","location":"Graphic Arts Collection","library":"Special Collections","call_number":"2012-0145Q","call_number_browse":"2012-0145Q","items":[{"holding_id":"22654362360006421","id":"23654362330006421","status_at_load":"1","barcode":"32101051826913","copy_number":"1"}]}}'))
