2d1669a597858fc5d9e88bca6fbe7679bd3a8c68
Ryan Laddusaw <rladdusaw@princeton.edu>
2024-09-05 11:07:17 -0400

    Fixed toggle to open facet

--- spec/system/searching_spec.rb
+++ spec/system/searching_spec.rb
@@ -213,7 +213,7 @@ describe 'Searching', type: :system, js: false do
       in_the_library = page.first('.dropdown-item')
       in_the_library.click
       click_on('advanced-search-submit')
-      page.first('.navbar-toggler').click
+      page.first('.collapse-toggle').click
       expect(page).to have_content(/Any of:\nIn the Library/)
     end
 
