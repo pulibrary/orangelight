85f3d1b1d6fe4d5408ccb1e6531e06065b1be6c8
Jane Sandberg <js7389@princeton.edu>
2024-09-10 16:10:33 -0700

    [#4130] Add an AskAQuestionFormSubmission class

---
+++ spec/models/ask_a_question_form_submission_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe AskAQuestionFormSubmission do
+  it 'sends the feedback via Libanswers API' do
+    stub_libanswers_api
+
+    described_class.new(
+        message: 'How can I find books about electricity?',
+        patron_name: 'Miles Morales',
+        patron_email: 'spiderman@example.com',
+        user_agent: 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
+        title: 'Venom : the secrets of nature\'s deadliest weapon', # the title of the record the patron was looking at
+        context: 'https://catalog.princeton.edu/catalog/12345' # the URL of the record the patron was looking at
+      ).send_to_libanswers
+
+    expect(WebMock).to have_requested(
+        :post,
+        'https://faq.library.princeton.edu/api/1.1/ticket/create'
+      ).with(body: 'quid=5678&'\
+      'pquestion=[Catalog] Venom : the secrets of nature\'s deadliest weapon&'\
+      "pdetails=How can I find books about electricity?\n\nSent from https://catalog.princeton.edu/catalog/12345 via LibAnswers API&"\
+      'pname=Miles Morales&'\
+      'pemail=spiderman@example.com&'\
+      'ua=Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
+             headers: { Authorization: 'Bearer abcdef1234567890abcdef1234567890abcdef12' })
+  end
+end
