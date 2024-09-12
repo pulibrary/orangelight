be7d8a19ab8c41d5cb5b3e8027a72de91aa08eb8
Max Kadel <mkadel@princeton.edu>
2024-09-12 15:26:15 -0400

    Pull out AlmaPatron from Patron class

---
+++ spec/models/requests/alma_patron_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Requests::AlmaPatron, requests: true do
+  context 'with a call to Alma' do
+    let(:uid) { 'BC123456789' }
+    let(:patron_with_multiple_barcodes) { fixture('/BC123456789.json') }
+
+    before do
+      stub_request(:get, "https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/#{uid}?expand=fees,requests,loans")
+        .to_return(status: 200, body: patron_with_multiple_barcodes, headers: { "Content-Type" => "application/json" })
+    end
+
+    it 'can be instantiated' do
+      described_class.new(uid:)
+    end
+
+    describe '#hash' do
+      it 'has a hash' do
+        expect(described_class.new(uid:).hash).to be_an_instance_of(HashWithIndifferentAccess)
+      end
+    end
+    context 'with multiple barcodes' do
+      it 'creates an access patron with the active barcode' do
+        patron = described_class.new(uid:)
+        expect(patron.hash[:barcode]).to eq('77777777')
+      end
+    end
+  end
+end
