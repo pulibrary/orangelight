682f503e55e7519a314afbc79cd3b503a0a536c3
Christina Chortaria <actspatial@gmail.com>
2024-09-26 09:22:02 -0400

    yarn lint app/javascript/orangelight/load-resources-by-orangelight-id.js

--- app/javascript/orangelight/load-resources-by-orangelight-id.js
+++ app/javascript/orangelight/load-resources-by-orangelight-id.js
@@ -1,55 +1,55 @@
-import apollo from './graphql-client.js'
-import gql from 'graphql-tag'
+import apollo from './graphql-client.js';
+import gql from 'graphql-tag';
 
 async function loadResourcesByOrangelightId(id) {
-
   const query = gql`
     query GetResourcesByOrangelightId($id: String!) {
       resourcesByOrangelightId(id: $id) {
-         id,
-         thumbnail {
-           iiifServiceUrl,
-           thumbnailUrl
-         },
-         label,
-         url,
-         memberIds,
-         embed {
-           type,
-           content,
-           status
-         },
-         notice {
-           heading,
-           acceptLabel,
-           textHtml
-         },
-         ... on ScannedResource {
-           manifestUrl
-         },
-         ... on ScannedMap {
-           manifestUrl
-         },
-         ... on Coin {
-           manifestUrl
-         }
+        id
+        thumbnail {
+          iiifServiceUrl
+          thumbnailUrl
+        }
+        label
+        url
+        memberIds
+        embed {
+          type
+          content
+          status
+        }
+        notice {
+          heading
+          acceptLabel
+          textHtml
+        }
+        ... on ScannedResource {
+          manifestUrl
+        }
+        ... on ScannedMap {
+          manifestUrl
+        }
+        ... on Coin {
+          manifestUrl
+        }
       }
-    }`
+    }
+  `;
 
   const variables = {
-    id: id
-  }
+    id: id,
+  };
 
   try {
     const response = await apollo.query({
       query,
-      variables
-    })
-    return response.data
+      variables,
+    });
+    return response.data;
   } catch (err) {
-    console.error(err)
-    return null
+    console.error(err);
+    return null;
   }
 }
 
-export default loadResourcesByOrangelightId
+export default loadResourcesByOrangelightId;
