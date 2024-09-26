6accf1891cb6ed733698e275ec6f321df0465b2d
Christina Chortaria <actspatial@gmail.com>
2024-09-26 09:21:23 -0400

    yarn lint app/javascript/orangelight/load-resources-by-figgy-ids.js

--- app/javascript/orangelight/load-resources-by-figgy-ids.js
+++ app/javascript/orangelight/load-resources-by-figgy-ids.js
@@ -1,32 +1,33 @@
-import apollo from './graphql-client.js'
-import gql from 'graphql-tag'
+import apollo from './graphql-client.js';
+import gql from 'graphql-tag';
 
 async function loadResourcesByFiggyIds(ids) {
-
   const query = gql`
     query GetResourcesByFiggyIds($ids: [ID!]!) {
       resourcesByFiggyIds(ids: $ids) {
-         id,
-         thumbnail {
-           iiifServiceUrl,
-           thumbnailUrl
-         }
+        id
+        thumbnail {
+          iiifServiceUrl
+          thumbnailUrl
+        }
       }
-    }`
+    }
+  `;
 
   const variables = {
-    ids: ids
-  }
+    ids: ids,
+  };
 
   try {
     const response = await apollo.query({
-      query, variables
-    })
-    return response.data
-  } catch(err) {
-    console.error(err)
-    return null
+      query,
+      variables,
+    });
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return null;
   }
 }
 
-export default loadResourcesByFiggyIds
+export default loadResourcesByFiggyIds;
