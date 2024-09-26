f1c41815e0b39173b4e6e4a31456c77f3cf2e1df
Christina Chortaria <actspatial@gmail.com>
2024-09-26 09:22:44 -0400

    yarn lint app/javascript/orangelight/load-resources-by-orangelight-ids.js

--- app/javascript/orangelight/load-resources-by-orangelight-ids.js
+++ app/javascript/orangelight/load-resources-by-orangelight-ids.js
@@ -1,47 +1,47 @@
-
-import apollo from './graphql-client.js'
-import gql from 'graphql-tag'
+import apollo from './graphql-client.js';
+import gql from 'graphql-tag';
 
 async function loadResourcesByOrangelightIds(ids) {
-
   const query = gql`
     query GetResourcesByOrangelightIds($ids: [String!]!) {
       resourcesByOrangelightIds(ids: $ids) {
-         id,
-         thumbnail {
-           iiifServiceUrl,
-           thumbnailUrl
-         },
-         url,
-         memberIds,
-         ... on ScannedResource {
-           manifestUrl,
-           orangelightId
-         },
-         ... on ScannedMap {
-           manifestUrl,
-           orangelightId
-         },
-         ... on Coin {
-           manifestUrl,
-           orangelightId
-         }
+        id
+        thumbnail {
+          iiifServiceUrl
+          thumbnailUrl
+        }
+        url
+        memberIds
+        ... on ScannedResource {
+          manifestUrl
+          orangelightId
+        }
+        ... on ScannedMap {
+          manifestUrl
+          orangelightId
+        }
+        ... on Coin {
+          manifestUrl
+          orangelightId
+        }
       }
-    }`
+    }
+  `;
 
   const variables = {
-    ids: ids
-  }
+    ids: ids,
+  };
 
   try {
     const response = await apollo.query({
-      query, variables
-    })
-    return response.data
-  } catch(err) {
-    console.error(err)
-    return null
+      query,
+      variables,
+    });
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return null;
   }
 }
 
-export default loadResourcesByOrangelightIds
+export default loadResourcesByOrangelightIds;
