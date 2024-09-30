48ae24360fe91da2fab09452ba7589cf342da199
Christina Chortaria <actspatial@gmail.com>
2024-09-30 16:21:34 -0400

    [CSP] Set object-src to none to remove vulnerability warning: "object-src" value is not safe See MDN documentation: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/object-src
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
     policy.font_src    :self, :https, :data
     policy.img_src     :self, :https, :data
     policy.media_src   :self, :data
+    policy.object_src  :none
     policy.script_src  :self, :https, :unsafe_eval, :unsafe_inline
     policy.style_src   :self, :https, :unsafe_inline
     policy.frame_src   :self, 'https://figgy.princeton.edu', 'https://*.doubleclick.net'
