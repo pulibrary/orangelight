032e893db44442a24d62f4ceb42122f73373d761
Jane Sandberg <js7389@princeton.edu>
2024-10-02 15:26:57 -0700

    Remove tests that have been skipped for 2 years

--- spec/features/browsing_item_spec.rb
+++ spec/features/browsing_item_spec.rb
@@ -20,38 +20,6 @@ describe 'browsing a catalog item', js: true do
     end
   end
 
-  context 'when an entry has a bib. ID for a resource published in Figgy', skip: true do
-    before do
-      visit 'catalog/9970446223506421'
-    end
-
-    it 'updates the thumbnail and constructs an instance of the Universal Viewer' do
-      using_wait_time 40 do
-        expect(page).to have_selector('iframe')
-        expect(page).to have_selector('.document-thumbnail img[src$="default.jpg"]')
-      end
-    end
-  end
-
-  context 'when an entry has a bib. ID and ARK for a resource published in Figgy', skip: true do
-    before do
-      visit 'catalog/9970446223506421'
-    end
-
-    it 'updates the thumbnail and constructs an instance of the Universal Viewer' do
-      using_wait_time 10 do
-        expect(page).to have_selector('iframe')
-        expect(page).to have_selector('.document-thumbnail img[src$="default.jpg"]')
-      end
-    end
-
-    it 'updates the link to the ARK with a fragment identifier for the UV' do
-      using_wait_time 10 do
-        expect(page).to have_selector('.availability--online a[href$="7044622#view"]', text: 'Table of contents')
-      end
-    end
-  end
-
   context 'when an item does not have a format value' do
     before do
       visit 'catalog/SCSB-7935196'
