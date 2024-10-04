4f9e64239a48aa46eaf75351380490bc8f746bf9
Jane Sandberg <js7389@princeton.edu>
2024-10-04 07:08:30 -0700

    Only run coveralls in CI, to speed up local test runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
-require 'coveralls'
+def in_ci?
+  !ENV['CI'].nil? && ENV['CI'] == 'true'
+end
+
+require 'coveralls' if in_ci?
 require 'factory_bot'
 require 'webmock/rspec'
 
@@ -7,11 +11,13 @@ require 'webmock/rspec'
 WebMock.disable_net_connect!(allow_localhost: true,
                              allow: 'chromedriver.storage.googleapis.com')
 
-Coveralls.wear!('rails') do
-  add_filter '/lib/orangelight/browse_lists.rb'
-  add_filter '/app/models/orangelight.rb'
-  add_filter '/lib/tasks'
-  add_filter 'app/controllers/search_history_controller.rb'
+if in_ci?
+  Coveralls.wear!('rails') do
+    add_filter '/lib/orangelight/browse_lists.rb'
+    add_filter '/app/models/orangelight.rb'
+    add_filter '/lib/tasks'
+    add_filter 'app/controllers/search_history_controller.rb'
+  end
 end
 
 FactoryBot.find_definitions
@@ -19,10 +25,6 @@ FactoryBot.find_definitions
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.example_status_persistence_file_path = "tmp/rspec_examples.txt"
-end
-
-def in_ci?
-  !ENV['CI'].nil? && ENV['CI'] == 'true'
 end
 
 ENV['GRAPHQL_API_URL'] = 'https://figgy.princeton.edu/graphql' unless ENV['GRAPHQL_API_URL']
