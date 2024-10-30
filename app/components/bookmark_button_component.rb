493205a983ed44f74d15b2ea273f019d6bc291fd
Max Kadel <mkadel@princeton.edu>
2024-10-30 11:47:51 -0400

    Try to make Bookmark work on BL7 and BL8
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- app/components/bookmark_button_component.rb
+++ app/components/bookmark_button_component.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 
-class BookmarkButtonComponent < Blacklight::Document::BookmarkComponent; end
+class BookmarkButtonComponent < Blacklight::Document::BookmarkComponent
+  def initialize(**kwargs)
+    if using_blacklight7?
+      super(**kwargs.except(:action))
+    else
+      super
+    end
+  end
+
+  def using_blacklight7?
+    @using_blacklight7 ||= Gem.loaded_specs['blacklight'].version.to_s.start_with? '7'
+  end
+end
