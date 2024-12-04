53c84243526d083edc752010fb69bf90b30af53e
Jane Sandberg <js7389@princeton.edu>
2024-12-04 13:33:05 -0800

    Start replacing our custom stringex with our own class
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>

---
+++ app/models/orangelight/sort_normalize.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Orangelight::SortNormalize
+  def normalize(string)
+    normalize_greek_characters remove_diacritics(string)
+      .gsub(/—/, ' ')
+      .gsub(/[\p{P}\p{S}]/, '')
+      .downcase
+  end
+
+    private
+
+      def remove_diacritics(string)
+        diacritic_combining_characters = [*0x1DC0..0x1DFF, *0x0300..0x036F, *0xFE20..0xFE2F].pack('U*')
+        decomposed_version = string.unicode_normalize(:nfd)
+        decomposed_version.tr(diacritic_combining_characters, '')
+      end
+
+      def normalize_greek_characters(string)
+        string.tr('ς', 'σ')
+      end
+end
---
+++ spec/models/orangelight/sort_normalize_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Orangelight::SortNormalize do
+  it 'removes punctuation and spaces' do
+    normalizer = described_class.new
+    expect(normalizer.normalize('World War, 1939-1945—Occupied territories—Pictorial works')).to eq 'world war 19391945 occupied territories pictorial works'
+    expect(normalizer.normalize('Ζουργός, Ισίδωρος, 1964-')).to eq 'ζουργοσ ισιδωροσ 1964' # Note that this uses the incorrect sigma (if we were displaying, we would use ισίδωρος)
+    expect(normalizer.normalize('دراسات. علوم الادارية.')).to eq 'دراسات علوم الادارية'
+  end
+end
