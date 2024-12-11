d0de6c9380d2801537b441f27d419277d8794066
Jane Sandberg <js7389@princeton.edu>
2024-12-11 08:34:56 -0800

    Move excessive paging out of search builder into controllers
    It doesn't have anything to do with building a search to send to solr, it is more about validating the HTTP requests from the user

---
+++ app/controllers/concerns/orangelight/excessive_paging.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+module Orangelight
+  module ExcessivePaging
+    private
+
+      def deny_excessive_paging
+        page = params[:page].to_i || 0
+        return if page <= 1
+        return if (has_search_parameters? || advanced_search? || bookmarks_page?) && page < 1000
+        render plain: "excessive paging", status: :bad_request
+      end
+
+      def advanced_search?
+        %w[advanced numismatics].include?(params[:advanced_type]) ||
+          params[:search_field] == 'advanced' ||
+          %w[advanced numismatics].include?(action_name)
+      end
+
+      def bookmarks_page?
+        instance_of?(BookmarksController)
+      end
+  end
+end
