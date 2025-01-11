4dae3ad88184f1847016fce3faaa04e535c02aad
Jane Sandberg <js7389@princeton.edu>
2025-01-10 16:09:40 -0800

    Change rails default config to 7.2
    This enables the Ruby YJIT, confirmed on staging with:
    
    1. `cd /opt/orangelight/current`
    2. `bundle exec rails c`
    3. `RubyVM::YJIT.enabled?`
    
    Prior to this commit, this returned false, with this
    commit, it returns true.

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ end
 
 module Orangelight
   class Application < Rails::Application
-    config.load_defaults 7.1
+    config.load_defaults 7.2
     # Before filter for Flipflop dashboard. Replace with a lambda or method name
     # defined in ApplicationController to implement access control.
     config.flipflop.dashboard_access_filter = :verify_admin!
