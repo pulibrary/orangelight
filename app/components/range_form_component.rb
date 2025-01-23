40b244335aa7d20cb7a1f3fb6bd88ab97165a9a7
Christina Chortaria <actspatial@gmail.com>
2025-01-23 15:44:40 -0500

    [#4692] Create range_form_component to override the html and remove the Apply submit button

---
+++ app/components/range_form_component.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# An override of the RangeFormComponent html to remove the Apply submit button
+# from the advanced search and the numismatics form
+class RangeFormComponent < BlacklightRangeLimit::RangeFormComponent
+end
