38db3b17ad2d924bee3e89bed9a483bc05fad37d
Christina Chortaria <actspatial@gmail.com>
2025-03-07 16:32:25 -0500

    Update browse_helpers#vocab_type(vocab) to include the officia; vocabulary name for each vocabulary: https://www.loc.gov/standards/sourcelist/ http://www.getty.edu/research/tools/vocabularies/aat/index.html https://www.loc.gov/standards/sourcelist/subject.html http://homosaurus.org/ https://www.loc.gov/standards/sourcelist/genre-form.html
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>

--- app/helpers/orangelight/browsables_helper.rb
+++ app/helpers/orangelight/browsables_helper.rb
@@ -23,11 +23,18 @@ module Orangelight
     end
 
     def vocab_type(vocab)
-      if vocab == 'Library of Congress genre/form term'
+      if vocab == 'Art & architecture thesaurus'
+        'aat_s'
+      elsif vocab == 'Homosaurus: an international LGBTQ linked data vocabulary'
+        'homoit_genre_s'
+      elsif vocab == 'Library of Congress genre/form terms for library and archival materials'
         'lcgft_s'
+      elsif vocab == 'Locally assigned term'
+        'local_subject_display'
       elsif vocab == 'Rare books genre term'
         'rbgenr_s'
       else
+        # use new field subject_lc
         'subject_facet'
       end
     end
