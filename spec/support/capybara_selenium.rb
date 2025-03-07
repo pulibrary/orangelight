6b3aa9b080c875ee0f32362774c08e4ef53b0ef3
Jane Sandberg <js7389@princeton.edu>
2025-03-07 15:36:27 -0800

    Use a human user agent for selenium tests
    Prior to this commit, our tests used a HeadlessChrome user agent, which the
    updated Voight-Kampff bot list lists as a bot, which I suppose it is.

--- spec/support/capybara_selenium.rb
+++ spec/support/capybara_selenium.rb
@@ -4,12 +4,17 @@ require 'capybara/rails'
 require 'capybara/rspec'
 require "selenium-webdriver"
 
+# The default HeadlessChrome user agent is detected to be a bot by the Voight-Kampff
+# gem, this user agent string is for a human-propelled Microsoft Edge instead
+HUMAN_USER_AGENT = '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59'
+
 Capybara.register_driver(:selenium) do |app|
   browser_options = ::Selenium::WebDriver::Chrome::Options.new
   browser_options.args << "--headless" unless ENV['RUN_IN_BROWSER']
   browser_options.args << "--disable-gpu"
   browser_options.args << "--disable-setuid-sandbox"
   browser_options.args << "--window-size=1920,1200"
+  browser_options.args << "--user-agent=#{HUMAN_USER_AGENT}"
 
   http_client = Selenium::WebDriver::Remote::Http::Default.new
   http_client.read_timeout = 120
@@ -42,6 +47,7 @@ Capybara.register_driver :selenium_chrome_headless do |app|
     opts.add_argument('--headless=new')
     opts.add_argument('--disable-gpu')
     opts.add_argument('--window-size=1920,1200')
+    opts.add_argument("--user-agent=#{HUMAN_USER_AGENT}")
   end
   Capybara::Selenium::Driver.new(app, **{ :browser => :chrome, options_key => browser_options })
 end
