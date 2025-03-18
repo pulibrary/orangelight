3fbd1a4b69c86aa324a069dd58d149d55a27c4d8
Christina Chortaria <actspatial@gmail.com>
2025-03-18 08:14:36 -0400

    lint schema.xml put back check for browselists csv file length.

--- spec/lib/orangelight/browse_lists_spec.rb
+++ spec/lib/orangelight/browse_lists_spec.rb
@@ -12,21 +12,21 @@ RSpec.describe BrowseLists, browse: true do
     FileUtils.rm_f(csv_file_path)
   end
 
-  context 'with an existing CSV file with no data' do
-    before do
-      FileUtils.touch(csv_file_path)
-    end
+  # context 'with an existing CSV file with no data' do
+  #   before do
+  #     FileUtils.touch(csv_file_path)
+  #   end
 
-    # comment out this test temporarily
-    # it 'does not truncate a table if there is no data in the CSV to re-populate it from' do
-    #   sql_command, facet_request, conn = described_class.connection
-    #   expect do
-    #     described_class.load_facet(sql_command, facet_request, conn,
-    #                                'author_s', "#{described_class.table_prefix}_names")
-    #   end.to raise_error(StandardError, 'CSV file too short - 0 lines long. Expected at least 9 lines.')
-    #   expect(described_class).not_to have_received(:system).with(truncate_command)
-    # end
-  end
+  #   comment out this test temporarily
+  #   it 'does not truncate a table if there is no data in the CSV to re-populate it from' do
+  #     sql_command, facet_request, conn = described_class.connection
+  #     expect do
+  #       described_class.load_facet(sql_command, facet_request, conn,
+  #                                  'author_s', "#{described_class.table_prefix}_names")
+  #     end.to raise_error(StandardError, 'CSV file too short - 0 lines long. Expected at least 9 lines.')
+  #     expect(described_class).not_to have_received(:system).with(truncate_command)
+  #   end
+  # end
 
   context 'with a missing CSV file' do
     it 'does not truncate a table if there is no CSV to re-populate it from' do
