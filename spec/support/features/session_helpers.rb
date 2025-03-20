956da56938de4f08d8bc39679b0119d585596098
Jane Sandberg <js7389@princeton.edu>
2025-03-20 15:39:36 -0700

    Remove spec support method that is no longer used

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -4,15 +4,5 @@ module Features
   # Provides methods for login and logout within Feature Tests
   module SessionHelpers
     include Warden::Test::Helpers
-    # Poltergeist-friendly sign-up
-    # Use this in feature tests
-    def sign_up_with(email, password)
-      Capybara.exact = true
-      visit new_user_registration_path
-      fill_in 'Email', with: email
-      fill_in 'Password', with: password
-      fill_in 'Password confirmation', with: password
-      click_button 'Sign up'
-    end
   end
 end
