55386c5286fbb2082c0b9f8d3a6ffb15c3e4a058
Kevin Reiss. <kevin.reiss@gmail.com>
2025-07-01 09:49:43 -0500

    comment

--- spec/system/stackmap_spec.rb
+++ spec/system/stackmap_spec.rb
@@ -10,7 +10,7 @@ describe 'stackmap', type: :system, js: true do
     before do
       allow(Flipflop).to receive(:temporary_where_to_find_it?).and_return(false)
     end
-    xit 'has a link to the stackmap on the record page' do
+    xit 'has a link to the stackmap on the record page' do # Stackmap subscription has lapsed.
       visit '/catalog/99125428126306421'
       click_button('Where to find it', wait: 5)
       expect(page).to have_button('zoom in')
