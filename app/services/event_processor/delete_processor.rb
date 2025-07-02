2cf5c38c96beb6aa5feb9045b42dcf0766f1ecec
Christina Chortaria <actspatial@gmail.com>
2025-07-02 13:35:19 -0400

    rubocop inline - disable Naming/PredicateMethod

--- app/services/event_processor/delete_processor.rb
+++ app/services/event_processor/delete_processor.rb
@@ -2,10 +2,12 @@
 
 class EventProcessor
   class DeleteProcessor < Processor
+    # rubocop:disable Naming/PredicateMethod
     def process
       index.delete_by_query "id:#{RSolr.solr_escape(id)}"
       index.commit unless bulk?
       true
     end
+    # rubocop:enable Naming/PredicateMethod
   end
 end
