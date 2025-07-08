ab4e25194d33590e38b48aeca4170ef9e76440f9
Ryan Laddusaw <rladdusaw@princeton.edu>
2025-07-08 15:59:21 -0400

    Update online options display

--- app/components/holdings/online_holdings_component.rb
+++ app/components/holdings/online_holdings_component.rb
@@ -29,9 +29,8 @@ class Holdings::OnlineHoldingsComponent < ViewComponent::Base
       if electronic_access
         links_hash = JSON.parse(electronic_access)
         links_hash.first(2).each do |url, text|
-          link = link_to(text.first, EzProxyService.ez_proxy_url(url), target: '_blank', rel: 'noopener')
-          link = "#{text[1]}: ".html_safe + link if text[1]
-          urls << content_tag(:div, link, class: 'library-location')
+          description = text[1] ? "#{text[1]}: #{text[0]}" : text[0]
+          urls << { "url" => url, "title" => description }.to_json
         end
       end
       urls
@@ -41,10 +40,6 @@ class Holdings::OnlineHoldingsComponent < ViewComponent::Base
     # @return [Array<String>] array containing the links
     def portfolio_links
       return [] if document.try(:electronic_portfolios).blank?
-      document.electronic_portfolios.map do |portfolio|
-        content_tag(:div, class: 'library-location') do
-          link_to(portfolio["title"], portfolio["url"], target: '_blank', rel: 'noopener')
-        end
-      end
+      document.electronic_portfolios.map(&:to_json)
     end
 end
