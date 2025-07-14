57206792034d2f37611c81094fc235347fc432e5
Christina Chortaria <actspatial@gmail.com>
2025-07-14 12:40:49 -0400

    Revert "Status unavailable 4929"

--- app/services/physical_holdings_markup_builder.rb
+++ app/services/physical_holdings_markup_builder.rb
@@ -73,7 +73,7 @@ class PhysicalHoldingsMarkupBuilder < HoldingRequestsBuilder
   # Holding record with "dspace": false
   def holding_location_unavailable
     children = content_tag(:span,
-                           'Request',
+                           'Unavailable',
                            class: 'availability-icon badge bg-danger')
     content_tag(:td, children.html_safe, class: 'holding-status')
   end
