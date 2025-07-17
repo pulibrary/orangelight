ca3d4e1566d2f849787e6bb7bdb6de743dc96871
Christina Chortaria <actspatial@gmail.com>
2025-07-17 11:29:04 -0400

    Move process_physical_holdings to a view component
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- app/services/physical_holdings_markup_builder.rb
+++ app/services/physical_holdings_markup_builder.rb
@@ -39,37 +39,6 @@ class PhysicalHoldingsMarkupBuilder < HoldingRequestsBuilder
 
   private
 
-    # Generate the markup for a physical holding record
-    # @param holding [Hash] holding information from a Solr Document
-    # @param holding_id [String] the ID for the holding record
-    # @return [String] the markup
-    def process_physical_holding(holding, holding_id)
-      markup = ''
-      doc_id = doc_id(holding)
-      temp_location_code = @adapter.temp_location_code(holding)
-
-      location_rules = @adapter.holding_location_rules(holding)
-      cn_value = @adapter.call_number(holding)
-
-      holding_loc = @adapter.holding_location_label(holding)
-      markup = render_component Holdings::HoldingLocationComponent.new(holding, holding_loc, holding_id, cn_value) if holding_loc.present?
-      markup << render_component(Holdings::CallNumberLinkComponent.new(holding, cn_value))
-      markup << if @adapter.repository_holding?(holding)
-                  holding_location_repository
-                elsif @adapter.scsb_holding?(holding) && !@adapter.empty_holding?(holding)
-                  render_component Holdings::HoldingAvailabilityScsbComponent.new(holding, doc_id, holding_id)
-                elsif @adapter.unavailable_holding?(holding)
-                  holding_location_unavailable
-                else
-                  render_component Holdings::HoldingAvailabilityComponent.new(doc_id, holding_id, location_rules, temp_location_code)
-                end
-      markup << render_component(Holdings::LocationServicesComponent.new(adapter, holding_id, location_rules, holding))
-      markup << render_component(Holdings::HoldingNotesComponent.new(holding, holding_id, @adapter))
-
-      markup = self.class.holding_block(markup) unless markup.empty?
-      markup
-    end
-
     # Generate the markup for physical holdings
     # @return [String] the markup
     def physical_holdings
