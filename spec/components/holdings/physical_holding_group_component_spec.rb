fc625f44dc54cc6bf2868fd56e12f53110db69aa
Jane Sandberg <js7389@princeton.edu>
2025-07-28 09:52:40 -0700

    Show page: open holding group on page load if specified in the open_holdings param
    Helps with #5062

--- spec/components/holdings/physical_holding_group_component_spec.rb
+++ spec/components/holdings/physical_holding_group_component_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe Holdings::PhysicalHoldingGroupComponent, type: :component do
 
     expect(rendered.css('details')).to be_present
   end
+
+  it 'renders an open details element if open: true' do
+    group = Requests::HoldingGroup.new(group_name: 'Firestone Library - Remote Storage (ReCAP)', holdings: [])
+    adapter = instance_double(HoldingRequestsAdapter)
+    rendered = render_inline(described_class.new(group:, adapter:, open: true))
+
+    expect(rendered.css('details[open]')).to be_present
+  end
 end
