76a9d868950c0a873da685e1519606b9076e2e4b
Ryan Laddusaw <rladdusaw@princeton.edu>
2025-10-28 17:12:20 -0400

    Remove more uses of csa_provider

--- app/models/requests/requestable_decorator.rb
+++ app/models/requests/requestable_decorator.rb
@@ -26,17 +26,17 @@ module Requests
     end
 
     def digitize?
-      return false unless patron.cas_provider? # only allow digitization for cas users
+      return false unless patron.cas_patron_group? # only allow digitization for cas users
       eligible_for_library_services? && (item_data? || !circulates?) && (on_shelf_edd? || recap_edd? || marquand_edd?) && !request_status?
     end
 
     def fill_in_digitize?
-      return false unless patron.cas_provider? # only allow fill in digitization for cas users
+      return false unless patron.cas_patron_group? # only allow fill in digitization for cas users
       !item_data? || digitize?
     end
 
     def pick_up?
-      return false if !eligible_for_library_services? || (!patron.cas_provider? && !off_site?)
+      return false if !eligible_for_library_services? || (!patron.cas_patron_group? && !off_site?)
       item_data? && (on_shelf? || recap? || annex?) && circulates? && !holding_library_in_library_only? && !scsb_in_library_use? && !request_status?
     end
 
