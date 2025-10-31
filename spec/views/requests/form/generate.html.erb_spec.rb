890358c3c66328575be4ebaa93e7a993c38ffe13
Jane Sandberg <js7389@princeton.edu>
2025-10-31 09:20:53 -0700

    working on it

--- spec/views/requests/form/generate.html.erb_spec.rb
+++ spec/views/requests/form/generate.html.erb_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'generate.html.erb' do
+RSpec.describe 'generate.html.erb', :requests do
     it 'is good' do
+        controller = Requests::FormController.new
+        first_filtered_requestable = Requestable.new
+        requestable_list = []
+        request = Requests::FormDecorator.new(
+            instance_double(Requests::Form, requestable: requestable_list, first_filtered_requestable:),
+            controller.view_context,
+            Requests::BackToRecordUrl.new(ActionController::Parameters.new)
+        )
          expect(1).to eq(1)
     end
 end
