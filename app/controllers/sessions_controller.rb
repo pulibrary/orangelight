c438afe5403330c61f6c8aadbd06351f23ec8dff
Jane Sandberg <js7389@princeton.edu>
2026-02-03 05:20:04 -0800

    Update respond_to_on_destroy API to match Devise 5 API
    See https://github.com/heartcombo/devise/pull/4878

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,11 @@ class SessionsController < Devise::SessionsController
 
   private
 
-    def respond_to_on_destroy
+    def respond_to_on_destroy(non_navigational_status: :no_content)
       # We actually need to hardcode this as Rails default responder doesn't
       # support returning empty response on GET request
       respond_to do |format|
-        format.all { head :no_content }
+        format.all { head non_navigational_status }
         format.any(*navigational_formats) { redirect_to after_sign_out_path_for(@provider), allow_other_host: true }
       end
     end
