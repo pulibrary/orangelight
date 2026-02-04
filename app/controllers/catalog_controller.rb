dda8862a4eea739820c6e07c9f8d505193ce8752
Ryan Laddusaw <rladdusaw@princeton.edu>
2026-02-04 10:26:57 -0500

    Move method to application controller

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -73,10 +73,6 @@ class CatalogController < ApplicationController
     #   :q => query
     # }
 
-    def render_bookmarks?
-      controller_path == 'bookmarks'
-    end
-
     config.add_show_tools_partial(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
     config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
     config.add_show_tools_partial(:sms, if: :render_sms_action?, callback: :sms_action, validator: :validate_sms_params)
