eaa5380668bf0c4620a1b01b7be2766121bf7c0b
Ryan Laddusaw <rladdusaw@princeton.edu>
2026-02-05 17:02:00 -0500

    resolve reek wanrings

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -6,18 +6,23 @@ module BookmarksHelper
   end
 
   def sort_by_most_recently_bookmarked(documents, user)
+    update_blank_sort_param
+    if params[:sort] == 'score desc' && bookmarks?
+      user.bookmarks.order(updated_at: :desc)
+          .collect { |bookmark| bookmark.document_id.to_s }
+          .filter_map { |id| documents.index_by(&:id)[id] }
+    else
+      documents
+    end
+  end
+
+  private
+
+  def update_blank_sort_param
     if params[:sort].blank?
       # make sure the recently bookmarked sort doesn't get used as the default and
       # force the recently bookmarked sort when viewing bookmarks without a sort param
       params[:sort] = bookmarks? ? 'score desc' : 'score desc, pub_date_start_sort desc, title_sort asc'
-    end
-    if params[:sort] == 'score desc' && bookmarks?
-      solr_docs = documents.index_by(&:id)
-      user.bookmarks.order(updated_at: :desc)
-          .collect { |bookmark| bookmark.document_id.to_s }
-          .filter_map { |id| solr_docs[id] }
-    else
-      documents
     end
   end
 end
