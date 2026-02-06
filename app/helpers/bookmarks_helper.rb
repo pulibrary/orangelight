011075747a4a7090059c03a349a85ac753fafe1b
Ryan Laddusaw <rladdusaw@princeton.edu>
2026-02-06 10:13:12 -0500

    Resolve rubocop warning

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -7,8 +7,8 @@ module BookmarksHelper
 
   def sort_by_most_recently_bookmarked(documents, user)
     update_blank_sort_param
-    if params[:sort] == 'score desc' && bookmarks?
-      user&.bookmarks.order(updated_at: :desc)
+    if params[:sort] == 'score desc' && bookmarks? && user
+      user.bookmarks.order(updated_at: :desc)
           .collect { |bookmark| bookmark.document_id.to_s }
           .filter_map { |id| documents.index_by(&:id)[id] }
     else
