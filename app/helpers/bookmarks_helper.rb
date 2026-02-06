778a74bfff7be489663aa7d4451d2b8085802cd4
Ryan Laddusaw <rladdusaw@princeton.edu>
2026-02-06 09:47:18 -0500

    Safely access user

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -8,7 +8,7 @@ module BookmarksHelper
   def sort_by_most_recently_bookmarked(documents, user)
     update_blank_sort_param
     if params[:sort] == 'score desc' && bookmarks?
-      user.bookmarks.order(updated_at: :desc)
+      user&.bookmarks.order(updated_at: :desc)
           .collect { |bookmark| bookmark.document_id.to_s }
           .filter_map { |id| documents.index_by(&:id)[id] }
     else
