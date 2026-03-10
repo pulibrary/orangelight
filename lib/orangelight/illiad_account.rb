dd1790a83ae34262003d5eac1e16660c0eb5f52f
Jane Sandberg <sandbergja@users.noreply.github.com>
2026-03-10 15:16:50 -0700

    Simplify constructor of IlliadClient (#5670)
    * Several of these ivars are simply not used
    * Others can just as easily be defined as methods and called
      when needed

--- lib/orangelight/illiad_account.rb
+++ lib/orangelight/illiad_account.rb
@@ -3,15 +3,10 @@
 require 'faraday'
 require 'faraday-cookie_jar'
 module Orangelight
+  # This class is responsible for retrieving information about an existing Interlibrary Loan account in ILLiad
   class IlliadAccount
     def initialize(patron)
       @patron = patron
-      @barcode = patron['barcode']
-      @last_name = patron['last_name']
-      @patron_id = patron['patron_id']
-      @netid = patron['netid']
-      @illiad_api_key = Requests.config["illiad_api_key"]
-      @illiad_api_base = Requests.config["illiad_api_base"]
     end
 
     def verify_user?
@@ -23,23 +18,35 @@ module Orangelight
     def illiad_patron_response
       @illiad_patron_response ||= begin
         url = "/ILLiadWebPlatform/Users/#{netid}"
-        Rails.logger.debug { "Illiad Get #{@illiad_api_base}/#{url}" }
+        Rails.logger.debug { "Illiad Get #{illiad_api_base}/#{url}" }
         response = conn.get do |req|
           req.url url
           req.headers['Accept'] = 'application/json'
-          req.headers['ApiKey'] = @illiad_api_key
+          req.headers['ApiKey'] = illiad_api_key
         end
-        Rails.logger.debug { "Illiad Get Response #{@illiad_api_base}/#{url} #{response.status} #{response.body}" }
+        Rails.logger.debug { "Illiad Get Response #{illiad_api_base}/#{url} #{response.status} #{response.body}" }
         response
       end
     rescue Faraday::ConnectionFailed
-      Rails.logger.warn("Unable to Connect to #{@illiad_api_base}")
+      Rails.logger.warn("Unable to Connect to #{illiad_api_base}")
       false
     end
 
     private
 
-      attr_reader :patron, :netid
+      attr_reader :patron
+
+      def netid
+        @netid ||= patron['netid']
+      end
+
+      def illiad_api_key
+        Requests.config["illiad_api_key"]
+      end
+
+      def illiad_api_base
+        Requests.config["illiad_api_base"]
+      end
 
       def conn
         @conn ||= IlliadPatronClient.new(patron).conn
