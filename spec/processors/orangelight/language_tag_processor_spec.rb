16346c8cd8f143d4e8f46a2572f085fb77ce69ba
Jane Sandberg <sandbergja@users.noreply.github.com>
2026-05-07 15:02:07 -0700

    [#2906] Add -Latn subtag in cases where the text has been Romanized (#5856)
    Part of #2906

--- spec/processors/orangelight/language_tag_processor_spec.rb
+++ spec/processors/orangelight/language_tag_processor_spec.rb
@@ -5,35 +5,46 @@ require 'rails_helper'
 RSpec.describe Orangelight::LanguageTagProcessor do
   it 'adds a language tag if configured to do so' do
     processor = described_class.new(
-      ['ABC'],
+      ['نخلة التمر'],
       Blacklight::Configuration::Field.new(language_tag: true),
       SolrDocument.new({ language_iana_s: ['ar'] }),
       {},
       { context: 'show' },
       [Blacklight::Rendering::Terminator]
     )
-    expect(processor.render).to eq(['<span lang="ar">ABC</span>'])
+    expect(processor.render).to eq(['<span lang="ar">نخلة التمر</span>'])
+  end
+  it 'adds a language tag with Script if the field is transliterated' do
+    processor = described_class.new(
+      ['nakhlat al-tamr'],
+      Blacklight::Configuration::Field.new(language_tag: true),
+      SolrDocument.new({ language_iana_s: ['ar'] }),
+      {},
+      { context: 'show' },
+      [Blacklight::Rendering::Terminator]
+    )
+    expect(processor.render).to eq(['<span lang="ar-Latn">nakhlat al-tamr</span>'])
   end
   it 'does not add a language tag if the document does not have one' do
     processor = described_class.new(
-      ['ABC'],
+      ['نخلة التمر'],
       Blacklight::Configuration::Field.new(language_tag: true),
       SolrDocument.new,
       {},
       { context: 'show' },
       [Blacklight::Rendering::Terminator]
     )
-    expect(processor.render).to eq(['ABC'])
+    expect(processor.render).to eq(['نخلة التمر'])
   end
   it 'does not add a language tag if the field is not configured to do so' do
     processor = described_class.new(
-      ['ABC'],
+      ['نخلة التمر'],
       Blacklight::Configuration::Field.new,
       SolrDocument.new({ language_iana_s: ['ar'] }),
       {},
       { context: 'show' },
       [Blacklight::Rendering::Terminator]
     )
-    expect(processor.render).to eq(['ABC'])
+    expect(processor.render).to eq(['نخلة التمر'])
   end
 end
