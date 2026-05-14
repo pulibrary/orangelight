2021607b2b0a0bfb380ed4af6d0ddcb9efca4911
Francis Kayiwa <kayiwa@pobox.com>
2026-05-13 21:22:34 -0400

    rubocop cleanup

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -3,6 +3,5 @@ require 'opentelemetry/exporter/otlp'
 require 'opentelemetry/instrumentation/all'
 
 OpenTelemetry::SDK.configure do |c|
-  c.use_all  # Enables all available instrumentations
+  c.use_all # Enables all available instrumentations
 end
-
