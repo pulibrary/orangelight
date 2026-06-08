dd410f62db113689d1cfd37efbb1a2602191e403
Jane Sandberg <sandbergja@users.noreply.github.com>
2026-06-08 08:42:02 -0700

    Don't require Francis' keys to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,13 +38,6 @@ set :whenever_roles, ->{ [:cron_prod1, :cron_prod2, :cron_prod3, :cron_db] }
 # set :linked_files, %w{config/database.yml}
 #
 
-set :ssh_options, {
-  keys: %w(/Users/kayiwa/.ssh/id_ed25519_pul_desktop_2025-08-26),
-  forward_agent: true,
-  auth_methods: %w(publickey),
-  keys_only: true
-}
-
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
