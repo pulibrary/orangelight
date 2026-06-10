a525c81c0f80d4d477338d5c78a0183c4778547a
Christina Chortaria <actspatial@gmail.com>
2026-06-10 12:45:34 -0400

    Update rubocop (#5948)
    * Fix rubocop errors
    Style/FileOpen: File.open without a block may leak a file descriptor; use the block form
    
    * Fix rubocop error
    Style/SymbolProc: Pass &:read as an argument to open instead of a block.

--- spec/lib/orangelight/browse_lists/call_number_csv_spec.rb
+++ spec/lib/orangelight/browse_lists/call_number_csv_spec.rb
@@ -54,8 +54,8 @@ RSpec.describe BrowseLists::CallNumberCSV, browse: true do
       expect(File.exist?(csv_file)).to be true
       expect(File.read(csv_file).scan(/\n/).count).to eq 496_050
       # Ensure call numbers with spaces in them are stripped for the CSV.
-      space_call_line = File.open(csv_file).readlines.find do |line|
-        line.start_with?("0000053.0000008")
+      space_call_line = File.open(csv_file) do |file|
+        file.readlines.find { |line| line.start_with?("0000053.0000008") }
       end
       expect(space_call_line).to start_with("0000053.0000008,53.8")
     end
