56d5f7b224aa3f69c478d96a370f93f6b4c095ba
Jane Sandberg <sandbergja@users.noreply.github.com>
2026-08-03 10:11:54 -0700

    Remove unused test helper method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,12 +74,3 @@ RSpec.configure do |config|
   config.before(:suite) { Rails.cache.clear }
   config.after { Rails.cache.clear }
 end
-
-def wait_for_ajax
-  counter = 0
-  while page.execute_script('return $.active').to_i > 0
-    counter += 1
-    sleep(0.1)
-    raise 'AJAX request took longer than 5 seconds.' if counter >= 10
-  end
-end
