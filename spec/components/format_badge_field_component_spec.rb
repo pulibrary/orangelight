867edefaaea775fe59f6bf217eb556eb3ee0d21b
Jane Sandberg <js7389@princeton.edu>
2026-08-06 13:46:59 -0700

    Purple online badge when in deduplication mode
    Closes #6016

--- spec/components/format_badge_field_component_spec.rb
+++ spec/components/format_badge_field_component_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe FormatBadgeFieldComponent, type: :component do
   let(:rendered) do
-    Capybara::Node::Simple.new(render_inline(described_class.new(field:)))
+    render_inline(described_class.new(field:))
   end
   let(:document) { SolrDocument.new 'id' => '9912345678906421', 'format' => ['Journal', 'Microform'] }
   let(:field_config) { Blacklight::Configuration::Field.new key: 'format', field: 'format', label: 'Format' }
@@ -16,5 +16,18 @@ RSpec.describe FormatBadgeFieldComponent, type: :component do
   it 'includes all formats' do
     expect(rendered.text).to include('Journal')
     expect(rendered.text).to include('Microform')
+  end
+
+  context 'when the record is electronic access' do
+    let(:document) { SolrDocument.new 'id' => '9912345678906421', 'format' => ['Journal', 'Microform'], electronic_portfolio_s: ['{"title": "Link 1", "url": "http://example.com/1"}'] }
+    it 'has a blue badge when deduplication feature is off' do
+      allow(Flipflop).to receive(:deduplication).and_return(false)
+      expect(rendered.css('lux-badge[color="blue"]')).to be_present
+    end
+
+    it 'has a purple badge when deduplication feature is on' do
+      allow(Flipflop).to receive(:deduplication?).and_return(true)
+      expect(rendered.css('lux-badge[color="purple"]')).to be_present
+    end
   end
 end
