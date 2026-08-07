8cfc7763ee2d6d7ccb65d5b83bd750842f164eec
Jane Sandberg <sandbergja@users.noreply.github.com>
2026-08-07 06:52:48 -0700

    Purple online badge when in deduplication mode (#6087)
    Closes #6016

--- app/components/format_badge_field_component.rb
+++ app/components/format_badge_field_component.rb
@@ -3,4 +3,8 @@
 # This component is responsible for displaying a compact badge
 # with the text and icon of a resource's format
 class FormatBadgeFieldComponent < Blacklight::MetadataFieldComponent
+  delegate :deduplication?, to: Flipflop
+  def online_color
+    deduplication? ? 'purple' : 'blue'
+  end
 end
