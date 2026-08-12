935812dc554d9c60d146e4da94ee4f02af2a8cf0
Jane Sandberg <js7389@princeton.edu>
2026-08-12 15:01:10 -0700

    [Duplicate records with clusters] Display badge in physical holdings on show page

--- spec/components/holdings/physical_holding_group_component_spec.rb
+++ spec/components/holdings/physical_holding_group_component_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 
 RSpec.describe Holdings::PhysicalHoldingGroupComponent, type: :component do
   it 'renders a details element' do
+    allow(Flipflop).to receive(:deduplication?).and_return false
     group = Requests::HoldingGroup.new(group_name: 'Firestone Library - Remote Storage (ReCAP)', holdings: [])
     adapter = instance_double(HoldingRequestsAdapter)
     rendered = render_inline(described_class.new(group:, adapter:))
@@ -12,10 +13,31 @@ RSpec.describe Holdings::PhysicalHoldingGroupComponent, type: :component do
   end
 
   it 'renders an open details element if open: true' do
+    allow(Flipflop).to receive(:deduplication?).and_return false
     group = Requests::HoldingGroup.new(group_name: 'Firestone Library - Remote Storage (ReCAP)', holdings: [])
     adapter = instance_double(HoldingRequestsAdapter)
     rendered = render_inline(described_class.new(group:, adapter:, open: true))
 
     expect(rendered.css('details[open]')).to be_present
+  end
+
+  it 'has table headers' do
+    allow(Flipflop).to receive(:deduplication?).and_return false
+    group = Requests::HoldingGroup.new(group_name: 'Firestone Library - Remote Storage (ReCAP)', holdings: [])
+    adapter = instance_double(HoldingRequestsAdapter)
+    rendered = render_inline(described_class.new(group:, adapter:, open: true))
+    headers = rendered.css('th').map(&:text)
+
+    expect(headers).to eq ['Call Number', 'Status', 'Location Service']
+  end
+
+  it 'includes a format in the headers when using deduplication feature' do
+    allow(Flipflop).to receive(:deduplication?).and_return true
+    group = Requests::HoldingGroup.new(group_name: 'Firestone Library - Remote Storage (ReCAP)', holdings: [])
+    adapter = instance_double(HoldingRequestsAdapter)
+    rendered = render_inline(described_class.new(group:, adapter:, open: true))
+    headers = rendered.css('th').map(&:text)
+
+    expect(headers).to eq ['Format', 'Call Number', 'Status', 'Location Service']
   end
 end
