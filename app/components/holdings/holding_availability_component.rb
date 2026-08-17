71279f90fab8976a0bd0bce8fe23b7826f4ff2e4
Jane Sandberg <sandbergja@users.noreply.github.com>
2026-08-17 12:28:49 -0700

    Use holding source_id (if available) instead of the solr document id for Show page data-record-id attribute (#6124)
    We use this attribute to connect bibdata availability response data
    with the appropriate holding on the show page.
    
    Helps with #6107

--- app/components/holdings/holding_availability_component.rb
+++ app/components/holdings/holding_availability_component.rb
@@ -2,20 +2,26 @@
 # This component is responsible for rendering a holding's availability
 # (which will be provided by Javascript based on the DOM structure of
 # this component)
+# :reek:TooManyInstanceVariables
 class Holdings::HoldingAvailabilityComponent < ViewComponent::Base
-  def initialize(doc_id, holding_id, location_rules, temp_location_code)
+  def initialize(doc_id, holding_id, holding, location_rules, temp_location_code)
     @doc_id = doc_id
     @holding_id = holding_id
+    @holding = holding
     @location_rules = location_rules
     @temp_location_code = temp_location_code
   end
 
     private
 
-      attr_reader :doc_id, :holding_id, :temp_location_code
+      attr_reader :holding_id, :temp_location_code
 
       def aeon_location?
         location_rules[:aeon_location]
+      end
+
+      def doc_id
+        @holding['source_id'] || @doc_id
       end
 
       def location_rules
