pulibrary/orangelight

commit f4c0da7f543cb3a9c2f7439b45fd265b36b4bd03

Add a Semantic Logger filter to remove complex Objects from the payload (#6232)
Before this commit, semantic logger was hitting this error before it
could log most events:

```
Unable to create the configured appenders. The log level has been raised
to WARN and the output directed to STDERR until the problem is fixed. --
Exception: ArgumentError: Could not convert symbol: :json_filter_formatter
to a class in: SemanticLogger::Formatters. Looking for:
SemanticLogger::Formatters::JsonFilterFormatter
```

This commit takes an alternative approach to addressing payloads that
contain infinitely recursive self-referential JSON -- we recursively
remove anything from the payload that isn't a Hash, Array, String, or
Number.

Closes #6231

Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>
Co-authored-by: Francis Kayiwa <kayiwa@users.noreply.github.com>

config/application.rb
@@ -3,6 +3,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require_relative 'log_filter'
 require_relative "lando_env"
 require_relative "../lib/orangelight/browse_lists"
 require_relative "../lib/orangelight/middleware/no_file_uploads"
@@ -83,7 +84,7 @@ module Orangelight
     config.middleware.insert_before Rack::MethodOverride, Orangelight::Middleware::NoFileUploads
 
     config.rails_semantic_logger.appenders do |appenders|
-      appenders.add(file_name: "log/#{Rails.env}.log", formatter: :json_filter_formatter)
+      appenders.add(file_name: "log/#{Rails.env}.log", filter: LogFilter)
     end
   end
 end

config/initializers/json_filter_formatter.rb
@@ -1,14 +1,0 @@
-# frozen_string_literal: true
-
-class JsonFilterFormatter < Logger::Formatter
-  def call(_severity, _timestamp, _progname, msg)
-    log_data = msg.is_a?(Hash) ? msg : { message: msg.to_s }
-
-    if log_data.is_a?(Hash) && log_data.key?(:action_controller)
-      log_data = log_data.dup
-      log_data.delete(:action_controller)
-    end
-
-    "#{JSON.dump(log_data)}\n"
-  end
-end

config/log_filter.rb
@@ -1,0 +1,24 @@
+# frozen_string_literal: true
+# This class is responsible for filtering out payloads that we don't want to write to the logs
+class LogFilter
+  def self.call(event)
+    visit event.payload if event.payload
+
+    # We must return the literal boolean `true` in order to send this event to the logs
+    true
+  end
+
+  SIMPLE_JSON_TYPES = [Numeric, String, Array, Hash].freeze
+
+  def self.visit(node)
+    if node.is_a? Hash
+      node.keep_if { |_key, value| simple_json_type? value }
+      node.each_value { visit it }
+    elsif node.is_a? Array
+      node.keep_if { simple_json_type? it }
+      node.each { visit it }
+    end
+  end
+
+  def self.simple_json_type?(node) = SIMPLE_JSON_TYPES.any? { |type| node.is_a? type }
+end

spec/config/log_filter_spec.rb
@@ -1,0 +1,69 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+MyTestingStruct = Struct.new(:field) do
+  def to_json(*_args) = to_h.to_json
+end
+
+describe LogFilter do
+  it 'does not mutate typical log messages' do
+    event = new_log_event
+    expect { described_class.call(event) }.not_to(change { event })
+  end
+
+  it 'returns true for typical log messages, indicating that we should log the message' do
+    event = new_log_event
+    expect(described_class.call(event)).to be true
+  end
+
+  it 'keeps deeply nested Hashes and Arrays in a payload' do
+    event = new_log_event payload: { data: [
+      1,
+      20.5,
+      { call_me_ishmael: [
+        'some years ago',
+        ['never', 'mind', 'how', 'long', 'precisely']
+      ] }
+    ] }
+
+    expect { described_class.call(event) }.not_to(change { event })
+  end
+
+  it 'removes complex objects from the payload' do
+    event = new_log_event payload: { data: MyTestingStruct.new('hello!') }
+
+    result = described_class.call(event)
+
+    expect(event.payload).to be_empty
+    expect(result).to be true
+  end
+
+  it 'removes deeply complex objects from arrays within the payload' do
+    event = new_log_event payload: { data: { observations: [1, 2, 3, MyTestingStruct.new('hello!')] } }
+
+    result = described_class.call(event)
+
+    expect(event.payload).to eq({ data: { observations: [1, 2, 3] } })
+    expect(result).to be true
+  end
+
+  it 'removes infinite self-recursion from a payload' do
+    a = MyTestingStruct.new
+    b = MyTestingStruct.new(a)
+    a.field = b
+
+    # Confirm that our payload is infinitely recursive
+    expect { a.to_json }.to raise_error(SystemStackError)
+
+    event = new_log_event payload: { my_recursive_data: a, my_good_data: { observations: [1, 2, 3] } }
+    result = described_class.call(event)
+    expect(event.payload).to eq({ my_good_data: { observations: [1, 2, 3] } })
+    expect(result).to be true
+  end
+
+  def new_log_event(level: :warn, message: 'relevant info', payload: { relevant: ['data'] })
+    event = SemanticLogger::Log.new 'my log', level
+    event.assign(message:, payload:)
+    event
+  end
+end

commit 844086047f6681b54e68508101a911cb6712a2ef

Format logs as json
Use the json formatter from Semantic Logger.

This fixes a regression introduced in #6232, which inadvertently
switched to the default semantic logger formatter.  We want the
JSON one instead!

Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>

config/application.rb
@@ -84,7 +84,7 @@ module Orangelight
     config.middleware.insert_before Rack::MethodOverride, Orangelight::Middleware::NoFileUploads
 
     config.rails_semantic_logger.appenders do |appenders|
-      appenders.add(file_name: "log/#{Rails.env}.log", filter: LogFilter)
+      appenders.add(file_name: "log/#{Rails.env}.log", filter: LogFilter, formatter: :json)
     end
   end
 end